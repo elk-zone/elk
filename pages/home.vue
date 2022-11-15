53897fa466645977d44b95ab2267109c51453ec4
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-15 21:50:17 +0800

    chore: update

--- pages/home.vue
+++ pages/home.vue
@@ -5,7 +5,7 @@ if (!token.value)
   router.replace('/public')
 
 const masto = await useMasto()
-const { data: timelines } = await useAsyncData('timelines-home', () => masto.timelines.fetchHome().then(r => r.value))
+const paginator = masto.timelines.getHomeIterable()
 </script>
 
 <template>
@@ -17,7 +17,7 @@ const { data: timelines } = await useAsyncData('timelines-home', () => masto.tim
       <div color-gray i-ri:equalizer-fill mr-1 h-6 />
     </template>
     <slot>
-      <TimelineList :timelines="timelines" />
+      <TimelinePaginator :timelines="paginator" />
     </slot>
   </MainContent>
 </template>

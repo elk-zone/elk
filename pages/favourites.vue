b455c37c104a320ed3d241aecac48598061b531c
patak <matias.capeletto@gmail.com>
2022-11-15 22:21:39 +0100

    feat: favourites (#8)

---
+++ pages/favourites.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+})
+
+const masto = await useMasto()
+const paginator = masto.favourites.getIterator()
+</script>
+
+<template>
+  <MainContent>
+    <template #title>
+      <div i-ri:heart-fill h-6 mr-1 /><span>Favourites</span>
+    </template>
+    <template #actions>
+      <div color-gray i-ri:equalizer-fill mr-1 h-6 />
+    </template>
+    <slot>
+      <TimelinePaginator :paginator="paginator" />
+    </slot>
+  </MainContent>
+</template>

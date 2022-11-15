4d4e8008ea4893866c05e34c2d2e74f2f0b4cfe1
patak <matias.capeletto@gmail.com>
2022-11-15 20:44:33 +0100

    feat: notifications

---
+++ components/notification/NotificationPaginator.client.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Notification, Paginator } from 'masto'
+
+const { paginator } = defineProps<{
+  paginator: Paginator<any, Notification[]>
+}>()
+
+const { items: notifications, isLoading, isDone, endAnchor } = usePaginator(paginator)
+</script>
+
+<template>
+  <template v-for="notification of notifications" :key="notification.id">
+    <NotificationCard :notification="notification" border="t border" pt-4 />
+  </template>
+  <div ref="endAnchor" />
+  <div v-if="isLoading">
+    Loading...
+  </div>
+  <div v-if="isDone">
+    End of list
+  </div>
+</template>
---
+++ pages/notifications.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+})
+
+const masto = await useMasto()
+const paginator = masto.notifications.getIterator()
+</script>
+
+<template>
+  <MainContent>
+    <template #title>
+      <div i-ri:notification-2-fill h-6 mr-1 /><span>Notifications</span>
+    </template>
+    <template #actions>
+      <div color-gray i-ri:equalizer-fill mr-1 h-6 />
+    </template>
+    <slot>
+      <NotificationPaginator :paginator="paginator" />
+    </slot>
+  </MainContent>
+</template>

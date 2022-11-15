c60476a062252e29f917ed5631d56bed74610780
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-16 00:03:12 +0800

    fix: fix gist loading

--- server/shared.ts
+++ server/shared.ts
@@ -4,8 +4,14 @@ import type { AppInfo } from '~/types'
 export const registeredApps: Record<string, AppInfo> = {}
 
 const runtimeConfig = useRuntimeConfig()
-const promise = $fetch(runtimeConfig.registedAppsUrl)
-  .then(r => Object.assign(registeredApps, r))
+const promise = $fetch(runtimeConfig.registedAppsUrl, { responseType: 'json' })
+  .then((r) => {
+    Object.assign(registeredApps, r)
+    // eslint-disable-next-line no-console
+    console.log(`\n${Object.keys(registeredApps).length} registered apps loaded from ${runtimeConfig.registedAppsUrl.split(/\/+/g)[1]}`)
+    // eslint-disable-next-line no-console
+    console.log(`${Object.keys(registeredApps).map(i => ` - ${i}`).join('\n')}\n`)
+  })
   .catch((e) => {
     if (process.dev)
       console.error('Failed to fetch registered apps,\nyou may need to run `nr register-apps` first')

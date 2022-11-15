62de7f543278b238ba7eb81442cb8e47efd4822b
patak <matias.capeletto@gmail.com>
2022-11-15 09:50:58 +0100

    feat: basic account statuses

--- pages/@[user]/index.vue
+++ pages/@[user]/index.vue
@@ -6,10 +6,13 @@ const props = defineProps<{
 const params = useRoute().params
 const masto = await useMasto()
 const { data: account } = await useAsyncData('account', () => masto.accounts.lookup({ acct: params.user as string }))
+const statuses = $(computedAsync(async () => account.value ? await masto.accounts.fetchStatuses(account.value.id).then(r => r.value) : [], []))
 </script>
 
 <template>
   <div>
     <AccountHeader :account="account" />
+    <!-- TODO: Tabbed Posts + Posts and Replies + Media -->
+    <TimelineList :timelines="statuses" />
   </div>
 </template>

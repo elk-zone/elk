787a55bea7589ac815f9d34be1f37bce743b1b22
patak <matias.capeletto@gmail.com>
2022-11-17 14:09:54 +0100

    feat: basic publish (#13)

--- components/publish/PublishWidget.vue
+++ components/publish/PublishWidget.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+const masto = await useMasto()
 
+let draftPost = $ref('')
+let isSending = $ref(false)
+
+async function publish() {
+  try {
+    isSending = true
+    await masto.statuses.create({ status: draftPost })
+    draftPost = ''
+  }
+  finally {
+    isSending = false
+  }
+}
 </script>
 
 <template>
-  <div flex flex-col gap-4>
-    <textarea p2 border-rounded w-full h-40 color-black placeholder="What's on your mind?" />
+  <div flex flex-col gap-4 :class="isSending ? ' pointer-events-none' : ''">
+    <textarea v-model="draftPost" p2 border-rounded w-full h-40 color-black placeholder="What's on your mind?" />
     <div flex justify-end>
-      <button h-9 w-22 bg-primary border-rounded>
+      <button h-9 w-22 bg-primary border-rounded :disabled="draftPost === ''" @click="publish">
         Publish!
       </button>
     </div>

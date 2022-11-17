714b9b2cf78948bb2783143211b4df23a0af1094
patak <matias.capeletto@gmail.com>
2022-11-17 12:25:04 +0100

    feat: basic publish

--- components/account/AccountMe.client.vue
+++ components/account/AccountMe.client.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-const { currentUser } = useAppStore()
+const { currentUser } = $(useAppStore())
+
+const account = $computed(() => currentUser?.account)
 </script>
 
 <template>
   <div flex flex-col gap-4 p4>
     <!-- TODO: multiple account switcher -->
-    <AccountInfo v-if="currentUser?.account" :account="currentUser.account" />
+    <template v-if="account">
+      <AccountInfo :account="account" />
+      <PublishWidget />
+    </template>
     <!-- TODO: dialog for select server -->
     <a v-else href="/api/mas.to/login" px2 py1 bg-teal6 text-white m2 rounded>Login</a>
-    <PublishWidget />
   </div>
 </template>

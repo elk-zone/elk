b6210aeb2bbbd0d6330cda7aac978613e126c9fb
patak <matias.capeletto@gmail.com>
2022-11-22 10:40:37 +0100

    feat: follow actions on profile

--- components/account/AccountCard.vue
+++ components/account/AccountCard.vue
@@ -27,7 +27,6 @@ function follow() {
   <div flex justify-between>
     <AccountInfo :account="account" p3 />
     <div h-full p5>
-      <!-- TODO is following logic and actions -->
       <div v-if="isFollowing === true" color-purple hover:color-gray hover:cursor-pointer i-ri:user-unfollow-fill @click="unfollow" />
       <div v-else-if="isFollowing === false" color-gray hover:color-purple hover:cursor-pointer i-ri:user-follow-fill @click="follow" />
     </div>

eea9c7c509ee0121aeca0fedfa47d43cfef5d74f
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-23 21:15:33 +0800

    chore: fix signin button

--- components/user/UserSignInEntry.vue
+++ components/user/UserSignInEntry.vue
@@ -3,7 +3,7 @@
     <div text-sm op50>
       Sign in to follow profiles or hashtags, favourite, share and reply to posts, or interact from your account on a different server.
     </div>
-    <button class="btn-solid text-center" @click="openUserSwitcher">
+    <button class="btn-solid text-center" @click="openSigninDialog()">
       Sign in
     </button>
   </div>

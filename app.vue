3adab283370f6a0196958ccbbb69d6a985660f7b
Daniel Roe <daniel@roe.dev>
2022-11-24 09:59:07 +0000

    fix: use elk colors for loading indicator

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@ isDark.value
 </script>
 
 <template>
-  <NuxtLoadingIndicator />
+  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,var(--c-primary) 0%,var(--c-primary-active) 100%)" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>

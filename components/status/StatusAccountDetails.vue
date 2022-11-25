64a6c7f7ea80ff97b70559181b2e23f84fb1ddcd
三咲智子 <sxzz@sxzz.moe>
2022-11-25 15:49:43 +0800

    fix: status card on mobile device
    closes #79

--- components/status/StatusAccountDetails.vue
+++ components/status/StatusAccountDetails.vue
@@ -10,7 +10,7 @@ const accountHandle = $(useAccountHandle(account))
 </script>
 
 <template>
-  <NuxtLink :to="link ? getAccountPath(account) : undefined" flex gap-2 items-center>
+  <NuxtLink :to="link ? getAccountPath(account) : undefined" flex="~ row wrap" gap-x-2 items-center>
     <ContentRichSetup font-bold :content="getDisplayName(account)" :emojis="account.emojis" />
     <p op35 text-sm>
       {{ accountHandle }}

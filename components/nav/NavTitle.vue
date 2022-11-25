fed9849016cdc549d8fb82bb66b688c387979767
Shinigami <chrissi92@hotmail.de>
2022-11-25 12:24:40 +0100

    feat: jump to top after clicking logo (#86)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- components/nav/NavTitle.vue
+++ components/nav/NavTitle.vue
@@ -1,5 +1,6 @@
 <template>
-  <NuxtLink flex items-center text-2xl gap-2 to="/">
+  <!-- Use external to force refresh page and jump to top of timeline -->
+  <NuxtLink flex items-center text-2xl gap-2 to="/" external>
     <img aria-label="Elk Logo" src="/logo.svg" w-10 h-10>
     <div>
       Elk <sup text-sm italic op50 mt-1>alpha</sup>

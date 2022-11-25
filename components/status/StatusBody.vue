727bef6066d5ca1a225c7f69c5a9aeef7fc815c7
三咲智子 <sxzz@sxzz.moe>
2022-11-25 16:43:47 +0800

    fix(status): add word wrap
    closes #63

--- components/status/StatusBody.vue
+++ components/status/StatusBody.vue
@@ -8,7 +8,7 @@ const { translation } = useTranslation(status)
 </script>
 
 <template>
-  <div class="status-body">
+  <div class="status-body" whitespace-pre-wrap break-words>
     <ContentRichSetup :content="translation.visible ? translation.text : status.content" :emojis="status.emojis" />
   </div>
 </template>

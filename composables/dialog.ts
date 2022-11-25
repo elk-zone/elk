9df9c06f64fd88525cb1ec2606e31a498bd2a019
Lucie <25330882+lihbr@users.noreply.github.com>
2022-11-25 13:13:44 -0500

    feat: image previews (#104)
    Co-authored-by: lihbr <lihbr@users.noreply.github.com>

--- composables/dialog.ts
+++ composables/dialog.ts
@@ -1,6 +1,7 @@
 import type { Draft } from './statusDrafts'
 import { STORAGE_KEY_FIRST_VISIT, STORAGE_KEY_ZEN_MODE } from '~/constants'
 
+export const imagePreview = ref({ src: '', alt: '' })
 export const isFirstVisit = useLocalStorage(STORAGE_KEY_FIRST_VISIT, true)
 export const isZenMode = useLocalStorage(STORAGE_KEY_ZEN_MODE, false)
 export const toggleZenMode = useToggle(isZenMode)
@@ -8,6 +9,7 @@ export const toggleZenMode = useToggle(isZenMode)
 export const isUserSwitcherOpen = ref(false)
 export const isSigninDialogOpen = ref(false)
 export const isPublishDialogOpen = ref(false)
+export const isImagePreviewDialogOpen = ref(false)
 export const isPreviewHelpOpen = ref(isFirstVisit.value)
 
 export function openUserSwitcher() {
@@ -17,10 +19,6 @@ export function openUserSwitcher() {
 export function openSigninDialog() {
   isSigninDialogOpen.value = true
   isUserSwitcherOpen.value = false
-}
-
-export function openPreviewHelp() {
-  isPreviewHelpOpen.value = true
 }
 
 export function openPublishDialog(draft?: Draft) {
@@ -33,4 +31,13 @@ if (isPreviewHelpOpen.value) {
   watch(isPreviewHelpOpen, () => {
     isFirstVisit.value = false
   })
+}
+
+export function openImagePreviewDialog(image: { src: string; alt: string }) {
+  imagePreview.value = image
+  isImagePreviewDialogOpen.value = true
+}
+
+export function openPreviewHelp() {
+  isPreviewHelpOpen.value = true
 }

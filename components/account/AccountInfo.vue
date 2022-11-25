b7713503c8426e145715b69459a4d6916cf3d2a5
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-26 00:33:41 +0800

    chore: typo

--- components/account/AccountInfo.vue
+++ components/account/AccountInfo.vue
@@ -14,10 +14,10 @@ const accountHandle = $(useAccountHandle(account, fullServer))
 <!-- TODO: Make this work for both buttons and links -->
 <!-- This is sometimes (like in the sidebar) used directly as a button, and sometimes, like in follow notifications, as a link. I think this component may need a second refactor that either lets an implementation pass in a link or an action and adapt to what's passed in, or the implementations need to be updated to wrap in the action they want to take and this be just the layout for these items -->
 <template>
-  <button gap-3 cursor-default class="account-switcher" :account="currentUser.account">
+  <button gap-3 cursor-default class="account-switcher" :account="currentUser?.account">
     <AccountAvatar :account="account" :hover="hover" class="account-icon" />
     <h3>
-      <ContentRichSetup font-bold hover:underline :content="getDisplayName(account)" :emojis="account.emojis" />
+      <ContentRich font-bold hover:underline :content="getDisplayName(account)" :emojis="account.emojis" />
     </h3>
     <p text-sm>
       {{ accountHandle }}

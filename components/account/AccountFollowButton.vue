191f784e1de1c15f0207230fb995ebbde3218e8a
patak <matias.capeletto@gmail.com>
2022-11-25 23:09:07 +0100

    chore: only show button for following

--- components/account/AccountFollowButton.vue
+++ components/account/AccountFollowButton.vue
@@ -21,7 +21,10 @@ async function toggleFollow() {
       flex group
       @click="toggleFollow"
     >
-      <div rounded-l w-28 p2 h-10 :group-hover="relationship?.following ? 'bg-red/75' : 'bg-orange/40'" :class="!relationship?.following ? relationship?.followedBy ? 'bg-orange/20' : 'bg-white/20' : relationship?.followedBy ? ' bg-orange/70' : 'bg-orange/50'">
+     <div v-if="!relationship?.following" rounded-l p2 w-11 h-10 flex justify-center items-center :group-hover="relationship?.following ? 'bg-white/10' : 'bg-white/10'" :class="!relationship?.following ? relationship?.followedBy ? 'bg-white/10' : 'bg-white/10' : relationship?.followedBy ? ' bg-white/10' : 'bg-white/10'">
+        <div i-ri:user-follow-fill group-hover="color-orange/85"/>
+      </div>
+      <div w-28 p2 h-10 :group-hover="relationship?.following ? 'bg-red/75' : 'bg-orange/40'" :class="!relationship?.following ? ('rounded-r ' + (relationship?.followedBy ? 'bg-orange/20' : 'bg-white/20')) : ('rounded ' + (relationship?.followedBy ? 'bg-orange/70' : 'bg-orange/50'))">
         <template v-if="relationship?.following">
           <span group-hover="hidden">{{ relationship?.followedBy ? 'Mutuals' : 'Following' }}</span>
           <span hidden group-hover="inline">Unfollow</span>
@@ -33,14 +36,6 @@ async function toggleFollow() {
         <template v-else>
           <span group-hover="hidden">Unrelated</span>
           <span hidden group-hover="inline">Follow</span>
-        </template>
-      </div>
-      <div rounded-r p2 w-11 h-10 flex justify-center items-center :group-hover="relationship?.following ? 'bg-white/10' : 'bg-white/10'" :class="!relationship?.following ? relationship?.followedBy ? 'bg-white/10' : 'bg-white/10' : relationship?.followedBy ? ' bg-white/10' : 'bg-white/10'">
-        <template v-if="relationship?.following" >
-          <div i-ri:user-unfollow-fill class="color-orange/60" group-hover="color-red/85" />
-        </template>
-        <template v-else>
-          <div i-ri:user-follow-fill group-hover="color-orange/85"/>
         </template>
       </div>
     </button>

0ce28872e20818c11703df40cc110eb1223b6f29
Shinigami92 <chrissi92@hotmail.de>
2022-11-25 11:59:41 +0100

    Apply review suggestion
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- components/nav/NavTitle.vue
+++ components/nav/NavTitle.vue
@@ -1,9 +1,9 @@
 <template>
-  <!-- Use a[href] here to force refresh page and jump to top of timeline -->
-  <a flex items-center text-2xl gap-2 href="/home">
+  <!-- Use external to force refresh page and jump to top of timeline -->
+  <NuxtLink flex items-center text-2xl gap-2 to="/" external>
     <img aria-label="Elk Logo" src="/logo.svg" w-10 h-10>
     <div>
       Elk <sup text-sm italic op50 mt-1>alpha</sup>
     </div>
-  </a>
+  </NuxtLink>
 </template>

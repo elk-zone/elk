7c7cd91a7b1836b4acd3bdd24e18b50c4e8881c7
Sam Richard <snugug@google.com>
2022-11-25 11:18:37 -0500

    Update AccountInfo to improve semantics

--- components/account/AccountInfo.vue
+++ components/account/AccountInfo.vue
@@ -11,20 +11,30 @@ const { account, link = true, fullServer = false } = defineProps<{
 const accountHandle = $(useAccountHandle(account, fullServer))
 </script>
 
+<!-- TODO: Make this work for both buttons and links -->
+<!-- This is sometimes (like in the sidebar) used directly as a button, and sometimes, like in follow notifications, as a link. I think this component may need a second refactor that either lets an implementation pass in a link or an action and adapt to what's passed in, or the implementations need to be updated to wrap in the action they want to take and this be just the layout for these items -->
 <template>
-  <div flex gap-3 cursor-default>
-    <div>
-      <NuxtLink :to="link ? getAccountPath(account) : null">
-        <AccountAvatar :account="account" :hover="hover" w-12 h-12 />
-      </NuxtLink>
-    </div>
-    <NuxtLink flex flex-col :to="link ? getAccountPath(account) : null">
+  <button gap-3 cursor-default class="account-switcher" :account="currentUser.account">
+    <AccountAvatar :account="account" :hover="hover" class="account-icon" />
+    <h3>
       <ContentRichSetup font-bold hover:underline :content="getDisplayName(account)" :emojis="account.emojis" />
-      <p op35 text-sm>
-        {{ accountHandle }}
-      </p>
-      <slot name="bottom" />
-    </NuxtLink>
-    <slot />
-  </div>
+    </h3>
+    <p text-sm>
+      {{ accountHandle }}
+    </p>
+    <slot name="bottom" />
+  </button>
 </template>
+
+<style>
+  .account-switcher {
+    display: grid;
+    grid-template-columns: 3rem auto;
+    row-gap: 0;
+    text-align: left
+  }
+
+  .account-icon {
+    grid-row: span 2;
+  }
+</style>

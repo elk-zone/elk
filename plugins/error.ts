1643c4d15815d024f92a9061ea9e3000174e4a39
userquin <userquin@gmail.com>
2022-11-26 16:49:26 +0100

    chore: remove error plugin

--- plugins/error.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.config.errorHandler = () => {
-    // TODO: client.ts module has a top level await, and so the app will not work
-    // TODO: once client.ts module fixed we can remove this
-    // console.error(err, vm, info)
-  }
-})

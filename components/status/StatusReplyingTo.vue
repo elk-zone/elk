a05880d922baa338366663824e6f693ef4236018
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-27 04:48:06 +0800

    ui: adjust

--- components/status/StatusReplyingTo.vue
+++ components/status/StatusReplyingTo.vue
@@ -15,7 +15,7 @@ const account = asyncComputed(() => fetchAccount(status.inReplyToAccountId!))
     :to="getStatusInReplyToPath(status)"
     :title="account ? `Replying to ${getDisplayName(account)}` : 'Replying to someone'"
   >
-    <div i-ri:reply-fill rotate-180 text-secondary class="mr-1.5" />
+    <div i-ri:reply-fill rotate-180 text-secondary-light class="mr-1.5" />
     <AccountInlineInfo v-if="account" :account="account" :link="false" />
     <span v-else>Someone</span>
     's post

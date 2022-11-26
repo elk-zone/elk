2cd85e2d5f74ab449225be1678efb1028c0f6223
userquin <userquin@gmail.com>
2022-11-26 16:02:49 +0100

    chore: allow app work offline

--- composables/client.ts
+++ composables/client.ts
@@ -6,4 +6,5 @@ import { DEFAULT_SERVER } from '~/constants'
 export const masto = await login({
   url: `https://${currentUser.value?.server || DEFAULT_SERVER}`,
   accessToken: currentUser.value?.token || undefined,
-})
+}).catch(() => Promise.resolve(null))
+// TODO: allow work offline, the catch will return a null instance and we can catch it on the global error handler plugin

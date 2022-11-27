d835a886d961540a9e0e94a1cfb49cd1e4827a75
userquin <userquin@gmail.com>
2022-11-27 12:40:47 +0100

    chore: add error handle hint

--- pages/signin/callback.vue
+++ pages/signin/callback.vue
@@ -7,6 +7,8 @@ const router = useRouter()
 const { query } = useRoute()
 
 onMounted(async () => {
+  // don't handle error here: this page has 'none' layout
+  // it will be handled by the error page
   await loginTo(query as any)
   router.push('/')
 })

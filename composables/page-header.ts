f2deb1297f18e6e1455f9cbe4fdb58fb9752fc57
Daniel Roe <daniel@roe.dev>
2022-11-27 16:30:36 +0000

    refactor: slightly simplify

--- composables/page-header.ts
+++ composables/page-header.ts
@@ -1,17 +1,11 @@
 import { APP_NAME } from '~/constants'
 
 export function usePageHeader() {
-  const head = useHead({
-    titleTemplate: title => `${title ? `${title} | ` : ''}${APP_NAME}${import.meta.env.DEV ? ' (dev)' : ''}`,
-    link: [
-      {
-        rel: 'icon', type: 'image/svg+png', href: '/favicon.png',
-      },
-    ],
+  useHead({
+    titleTemplate: title => `${title ? `${title} | ` : ''}${APP_NAME}${process.dev ? ' (dev)' : ''}`,
+    link: [{ rel: 'icon', type: 'image/svg+png', href: '/favicon.png' }],
   })
 
   // eslint-disable-next-line no-unused-expressions
   isDark.value
-
-  return head
 }

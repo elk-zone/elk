4be395274b4a2c3d8af792bc85794415da4cb077
LittleSound <464388324@qq.com>
2022-11-27 22:41:05 +0800

    docs: navside i18n

--- components/nav/NavSide.vue
+++ components/nav/NavSide.vue
@@ -1,18 +1,24 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
 <template>
   <nav px3 py4 flex="~ col gap2" text-lg>
     <template v-if="currentUser">
-      <NavSideItem text="Home" to="/home" icon="i-ri:home-5-line" />
-      <NavSideItem text="Notifications" to="/notifications" icon="i-ri:notification-4-line" />
+      <NavSideItem :text="t('nav_side.home')" to="/home" icon="i-ri:home-5-line" />
+      <NavSideItem :text="t('nav_side.notifications')" to="/notifications" icon="i-ri:notification-4-line" />
     </template>
-    <NavSideItem text="Explore" to="/explore" icon="i-ri:hashtag" />
-    <NavSideItem text="Local" to="/public/local" icon="i-ri:group-2-line " />
-    <NavSideItem text="Federated" to="/public" icon="i-ri:earth-line" />
+    <NavSideItem :text="t('nav_side.explore')" to="/explore" icon="i-ri:hashtag" />
+    <NavSideItem :text="t('nav_side.local')" to="/public/local" icon="i-ri:group-2-line " />
+    <NavSideItem :text="t('nav_side.federated')" to="/public" icon="i-ri:earth-line" />
     <template v-if="currentUser">
-      <NavSideItem text="Conversations" to="/conversations" icon="i-ri:at-line" />
-      <NavSideItem text="Favourites" to="/favourites" icon="i-ri:heart-3-line" />
-      <NavSideItem text="Bookmarks" to="/bookmarks" icon="i-ri:bookmark-line " />
+      <NavSideItem :text="t('nav_side.conversations')" to="/conversations" icon="i-ri:at-line" />
+      <NavSideItem :text="t('nav_side.favourites')" to="/favourites" icon="i-ri:heart-3-line" />
+      <NavSideItem :text="t('nav_side.bookmarks')" to="/bookmarks" icon="i-ri:bookmark-line " />
       <NavSideItem
-        :text="currentUser.account.displayName || 'Profile'"
+        :text="currentUser.account.displayName || t('nav_side.profile')"
         :to="getAccountPath(currentUser.account)" icon="i-ri:list-check-2-line"
       >
         <template #icon>

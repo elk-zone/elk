0843878a42c8aa1b949d81615835d4b4006b9e8a
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-28 17:51:15 +0800

    ui: hover card ui

--- components/account/AccountHeader.vue
+++ components/account/AccountHeader.vue
@@ -115,17 +115,7 @@ watchEffect(() => {
           <ContentRich text-sm filter-saturate-0 :content="field.value" />
         </div>
       </div>
-      <div flex gap-5>
-        <NuxtLink :to="getAccountPath(account)" exact-active-class="text-primary">
-          <span font-bold>{{ formattedNumber(account.statusesCount) }}</span> <span text-secondary>Posts</span>
-        </NuxtLink>
-        <NuxtLink :to="`${getAccountPath(account)}/following`" exact-active-class="text-primary">
-          <span font-bold>{{ humanReadableNumber(account.followingCount) }}</span> <span text-secondary>Following</span>
-        </NuxtLink>
-        <NuxtLink :to="`${getAccountPath(account)}/followers`" exact-active-class="text-primary">
-          <span font-bold>{{ humanReadableNumber(account.followersCount) }}</span> <span text-secondary>Followers</span>
-        </NuxtLink>
-      </div>
+      <AccountPostsFollowers :account="account" />
     </div>
   </div>
 </template>
---
+++ components/account/AccountPostsFollowers.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Account } from 'masto'
+
+defineProps<{
+  account: Account
+}>()
+</script>
+
+<template>
+  <div flex gap-5>
+    <NuxtLink :to="getAccountPath(account)" exact-active-class="text-primary">
+      <span font-bold>{{ formattedNumber(account.statusesCount) }}</span> <span text-secondary>Posts</span>
+    </NuxtLink>
+    <NuxtLink :to="`${getAccountPath(account)}/following`" exact-active-class="text-primary">
+      <span font-bold>{{ humanReadableNumber(account.followingCount) }}</span> <span text-secondary>Following</span>
+    </NuxtLink>
+    <NuxtLink :to="`${getAccountPath(account)}/followers`" exact-active-class="text-primary">
+      <span font-bold>{{ humanReadableNumber(account.followersCount) }}</span> <span text-secondary>Followers</span>
+    </NuxtLink>
+  </div>
+</template>

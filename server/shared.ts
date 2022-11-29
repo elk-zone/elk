c85d9bb9136870adfbb086b4c61a732715ae5822
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-30 06:45:07 +0800

    feat: use different app name for dev

--- server/shared.ts
+++ server/shared.ts
@@ -7,6 +7,7 @@ import { parseURL } from 'ufo'
 import { $fetch } from 'ohmyfetch'
 import type { Storage } from 'unstorage'
 
+import { isCI } from 'std-env'
 import cached from './cache-driver'
 
 import type { AppInfo } from '~/types'
@@ -41,7 +42,7 @@ async function fetchAppInfo(server: string) {
   const app: AppInfo = await $fetch(`https://${server}/api/v1/apps`, {
     method: 'POST',
     body: {
-      client_name: APP_NAME,
+      client_name: APP_NAME + (isCI ? '' : ' (dev)'),
       redirect_uris,
       scopes: 'read write follow push',
     },

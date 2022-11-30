9c83e8bbb668962cbf3a28c7a46b0b6b4f53ec4a
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-30 10:16:15 +0800

    i18n: improve

--- components/account/AccountFollowButton.vue
+++ components/account/AccountFollowButton.vue
@@ -21,16 +21,14 @@ async function toggleFollow() {
   }
 }
 
+const { t } = useI18n()
+
 useCommand({
   scope: 'Actions',
-
   order: -2,
-
   visible: () => command && enable,
-
-  name: () => `${relationship?.following ? 'Unfollow' : 'Follow'} ${getShortHandle(account)}`,
+  name: () => `${relationship?.following ? t('account.unfollow') : t('account.follow')} ${getShortHandle(account)}`,
   icon: 'i-ri:star-line',
-
   onActivate: () => toggleFollow(),
 })
 </script>
@@ -44,7 +42,7 @@ useCommand({
     rounded-full flex="~ gap2 center" font-500 w-30 h-fit py1 :class="relationship?.following ? 'text-base border-text-base' : 'text-inverted bg-primary border-primary'" :hover="relationship?.following ? 'border-red text-red' : 'bg-base border-primary text-primary'" @click="toggleFollow"
   >
     <template v-if="relationship?.following">
-      <span group-hover="hidden">{{ relationship?.followedBy ? 'Mutuals' : 'Following' }}</span>
+      <span group-hover="hidden">{{ relationship?.followedBy ? $t('account.mutuals') : $t('account.following') }}</span>
       <span hidden group-hover="inline">{{ $t('account.unfollow') }}</span>
     </template>
     <template v-else-if="relationship?.requested">

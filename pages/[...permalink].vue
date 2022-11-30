24bbe9135bf8a66ffc5b1f17578f5ad8995908e1
Daniel Roe <daniel@roe.dev>
2022-11-30 13:31:33 +0000

    fix: early return on invalid permalinks
    resolves #255

--- pages/[...permalink].vue
+++ pages/[...permalink].vue
@@ -3,32 +3,39 @@ import { parseURL } from 'ufo'
 
 definePageMeta({
   middleware: async (to) => {
-    let permalink = Array.isArray(to.params.permalink)
-      ? to.params.permalink.join('/')
-      : to.params.permalink
+    const HANDLED_MASTO_URL = /^(https?:\/\/)?(\w+\.)+\w+\/(@[@\w\d\.]+)(\/\d+)?$/
+    try {
+      let permalink = Array.isArray(to.params.permalink)
+        ? to.params.permalink.join('/')
+        : to.params.permalink
 
-    if (!permalink.startsWith('http'))
-      permalink = `https://${permalink}`
+      if (!HANDLED_MASTO_URL.test(permalink))
+        return '/home'
 
-    if (!currentUser.value) {
-      const { host, pathname } = parseURL(permalink)
-      await loginTo({ server: host! })
-      return pathname
-    }
+      if (!permalink.startsWith('http'))
+        permalink = `https://${permalink}`
 
-    const { value } = await useMasto().search({ q: permalink, resolve: true, limit: 1 }).next()
-
-    const { accounts, statuses } = value
-    if (statuses[0]) {
-      return {
-        path: getStatusPath(statuses[0]),
-        state: {
-          status: statuses[0] as any,
-        },
+      if (!currentUser.value) {
+        const { host, pathname } = parseURL(permalink)
+        await loginTo({ server: host! })
+        return pathname
       }
+
+      const { value } = await useMasto().search({ q: permalink, resolve: true, limit: 1 }).next()
+
+      const { accounts, statuses } = value
+      if (statuses[0]) {
+        return {
+          path: getStatusPath(statuses[0]),
+          state: {
+            status: statuses[0] as any,
+          },
+        }
+      }
+      if (accounts[0])
+        return getAccountPath(accounts[0])
     }
-    if (accounts[0])
-      return getAccountPath(accounts[0])
+    catch {}
 
     return '/home'
   },

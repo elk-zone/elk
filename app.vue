b997feb6b37dc347104673455552a558d31f8acf
Daniel Roe <daniel@roe.dev>
2022-11-30 09:41:14 +0000

    fix: add user id to page key (#241)

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ setupPageHeader()
 await setupI18n()
 
 // We want to trigger rerendering the page when account changes
-const key = computed(() => useMasto().instances.config.url || 'default')
+const key = computed(() => currentServer.value + ':' + (currentUser.value?.account.id || ''))
 </script>
 
 <template>

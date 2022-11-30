b150d6d65ef2c05639ffc17befe71f8139b04881
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-30 15:15:26 +0800

    fix: shiki don't load all lang

--- composables/shiki.ts
+++ composables/shiki.ts
@@ -15,7 +15,9 @@ export function highlightCode(code: string, lang: Lang) {
             'vitesse-dark',
             'vitesse-light',
           ],
-          langs: [],
+          langs: [
+            'html',
+          ],
         })
       })
   }

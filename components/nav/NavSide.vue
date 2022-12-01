447ed6d9418a46567d1dfceab5a7d7888c0a63e3
三咲智子 <sxzz@sxzz.moe>
2022-12-01 21:22:03 +0800

    refactor: simplify

--- components/nav/NavSide.vue
+++ components/nav/NavSide.vue
@@ -1,20 +1,16 @@
-<script lang="ts" setup>
-const { t } = useI18n()
-</script>
-
 <template>
   <nav px3 py4 flex="~ col gap2" text-lg>
     <template v-if="currentUser">
-      <NavSideItem :text="t('nav_side.home')" to="/home" icon="i-ri:home-5-line" />
-      <NavSideItem :text="t('nav_side.notifications')" to="/notifications" icon="i-ri:notification-4-line" />
+      <NavSideItem :text="$t('nav_side.home')" to="/home" icon="i-ri:home-5-line" />
+      <NavSideItem :text="$t('nav_side.notifications')" to="/notifications" icon="i-ri:notification-4-line" />
     </template>
-    <NavSideItem :text="t('nav_side.explore')" to="/explore" icon="i-ri:hashtag" />
-    <NavSideItem :text="t('nav_side.local')" to="/public/local" icon="i-ri:group-2-line " />
-    <NavSideItem :text="t('nav_side.federated')" to="/public" icon="i-ri:earth-line" />
+    <NavSideItem :text="$t('nav_side.explore')" to="/explore" icon="i-ri:hashtag" />
+    <NavSideItem :text="$t('nav_side.local')" to="/public/local" icon="i-ri:group-2-line " />
+    <NavSideItem :text="$t('nav_side.federated')" to="/public" icon="i-ri:earth-line" />
     <template v-if="currentUser">
-      <NavSideItem :text="t('nav_side.conversations')" to="/conversations" icon="i-ri:at-line" />
-      <NavSideItem :text="t('nav_side.favourites')" to="/favourites" icon="i-ri:heart-3-line" />
-      <NavSideItem :text="t('nav_side.bookmarks')" to="/bookmarks" icon="i-ri:bookmark-line " />
+      <NavSideItem :text="$t('nav_side.conversations')" to="/conversations" icon="i-ri:at-line" />
+      <NavSideItem :text="$t('nav_side.favourites')" to="/favourites" icon="i-ri:heart-3-line" />
+      <NavSideItem :text="$t('nav_side.bookmarks')" to="/bookmarks" icon="i-ri:bookmark-line " />
       <NavSideItem
         v-if="isMediumScreen"
         :text="currentUser.account.displayName"
@@ -25,7 +21,7 @@ const { t } = useI18n()
           <AccountAvatar :account="currentUser.account" h="1.2em" />
         </template>
         <ContentRich
-          :content="getDisplayName(currentUser.account, { rich: true }) || t('nav_side.profile')"
+          :content="getDisplayName(currentUser.account, { rich: true }) || $t('nav_side.profile')"
           :emojis="currentUser.account.emojis"
         />
       </NavSideItem>
--- components/publish/PublishButton.vue
+++ components/publish/PublishButton.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-function openDialog() {
-  openPublishDialog()
-}
-</script>
-
 <template>
-  <button btn-outline rounded-full font-bold py4 flex="~ gap2 center" @click="openDialog">
+  <button btn-outline rounded-full font-bold py4 flex="~ gap2 center" @click="openPublishDialog()">
     <div i-ri:quill-pen-line />
     {{ $t('action.compose') }}
   </button>

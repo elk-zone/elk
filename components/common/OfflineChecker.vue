5f2c21e898cfe6772d4d3cbbd73e1fca3d722bd6
Anthony Fu <anthonyfu117@hotmail.com>
2022-12-01 11:06:30 +0800

    chore: color

--- components/common/OfflineChecker.vue
+++ components/common/OfflineChecker.vue
@@ -8,7 +8,7 @@ const online = useOnline()
 <template>
   <div
     v-if="!online"
-    w-full min-h-30px px4 py3 text-yellow-5 bg-base
+    w-full min-h-30px px4 py3 text-primary bg-base
     border="t base" flex="~ gap-2 center"
   >
     <div i-ri:wifi-off-line />

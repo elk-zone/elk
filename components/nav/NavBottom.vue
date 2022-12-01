08467a6b951eb063e4de17be590f0ae3e69800f0
LittleSound <464388324@qq.com>
2022-12-01 13:50:37 +0800

    fix: active status error for component `NavBottom`

--- components/nav/NavBottom.vue
+++ components/nav/NavBottom.vue
@@ -6,24 +6,24 @@
   <nav h-14 border="t base" flex flex-row>
     <template v-if="currentUser">
       <NuxtLink to="/home" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-        <div i-ri:home-5-line />
+        <div text="[inherit]" i-ri:home-5-line />
       </NuxtLink>
       <NuxtLink to="/notifications" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-        <div i-ri:notification-4-line />
+        <div text="[inherit]" i-ri:notification-4-line />
       </NuxtLink>
     </template>
     <NuxtLink to="/explore" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-      <div i-ri:hashtag />
+      <div text="[inherit]" i-ri:hashtag />
     </NuxtLink>
     <NuxtLink group to="/public/local" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-      <div i-ri:group-2-line />
+      <div text="[inherit]" i-ri:group-2-line />
     </NuxtLink>
     <NuxtLink to="/public" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-      <div i-ri:earth-line />
+      <div text="[inherit]" i-ri:earth-line />
     </NuxtLink>
     <template v-if="currentUser">
       <NuxtLink to="/conversations" active-class="text-primary" flex flex-row items-center place-content-center h-full flex-1 @click="$scrollToTop">
-        <div i-ri:at-line />
+        <div text="[inherit]" i-ri:at-line />
       </NuxtLink>
     </template>
   </nav>

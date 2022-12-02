2a84bbb3d64f201fc4e72cea48531fb117488678
Alex <49969959+alexzhang1030@users.noreply.github.com>
2022-12-03 00:48:44 +0800

    fix(ui): use correct url for timeline image srcset (#296)

--- components/status/StatusAttachment.vue
+++ components/status/StatusAttachment.vue
@@ -9,7 +9,7 @@ const { attachment } = defineProps<{
 
 const src = $computed(() => attachment.previewUrl || attachment.url || attachment.remoteUrl!)
 const srcset = $computed(() => [
-  [src, attachment.meta?.original?.width],
+  [attachment.url, attachment.meta?.original?.width],
   [attachment.previewUrl, attachment.meta?.small?.width],
 ].filter(([url]) => url).map(([url, size]) => `${url} ${size}w`).join(', '))
 

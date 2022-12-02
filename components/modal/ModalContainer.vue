8d688dff975061b2a7e594f2d2e883643c46cd2a
LittleSound <464388324@qq.com>
2022-12-02 10:10:43 +0800

    style: support user sign in

--- components/modal/ModalContainer.vue
+++ components/modal/ModalContainer.vue
@@ -9,8 +9,8 @@ import {
 </script>
 
 <template>
-  <ModalNewModalDialog v-model="isSigninDialogOpen">
-    <UserSignIn m6 />
+  <ModalNewModalDialog v-model="isSigninDialogOpen" py-6 px-3 sm-px-6>
+    <UserSignIn />
   </ModalNewModalDialog>
   <ModalNewModalDialog v-model="isPreviewHelpOpen">
     <HelpPreview @close="closePreviewHelp()" />
@@ -18,7 +18,7 @@ import {
   <ModalNewModalDialog v-model="isPublishDialogOpen" max-w-180>
     <PublishWidget :draft-key="dialogDraftKey" expanded />
   </ModalNewModalDialog>
-  <ModalDialog v-model="isMediaPreviewOpen" close-button>
+  <ModalDialog v-model="isMediaPreviewOpen" close-button h-screen w-screen>
     <ModalMediaPreview v-if="isMediaPreviewOpen" @close="closeMediaPreview()" />
   </ModalDialog>
   <ModalNewModalDialog v-model="isEditHistoryDialogOpen">

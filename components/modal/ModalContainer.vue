feb8872f5fced992147dce240a46168dcf591a0d
Ayaka Rizumu <464388324@qq.com>
2022-12-02 15:02:44 +0800

    refactor: modal dialog (#277)

--- components/modal/ModalContainer.vue
+++ components/modal/ModalContainer.vue
@@ -9,16 +9,16 @@ import {
 </script>
 
 <template>
-  <ModalDialog v-model="isSigninDialogOpen">
-    <UserSignIn m6 />
+  <ModalDialog v-model="isSigninDialogOpen" py-6 px-3 sm-px-6>
+    <UserSignIn />
   </ModalDialog>
-  <ModalDialog v-model="isPreviewHelpOpen" :type="isSmallScreen ? 'bottom' : 'dialog'">
+  <ModalDialog v-model="isPreviewHelpOpen">
     <HelpPreview @close="closePreviewHelp()" />
   </ModalDialog>
-  <ModalDialog v-model="isPublishDialogOpen">
-    <PublishWidget :draft-key="dialogDraftKey" expanded min-w-180 />
+  <ModalDialog v-model="isPublishDialogOpen" max-w-180>
+    <PublishWidget :draft-key="dialogDraftKey" expanded />
   </ModalDialog>
-  <ModalDialog v-model="isMediaPreviewOpen" close-button>
+  <ModalDialog v-model="isMediaPreviewOpen" w-full max-w-full h-full max-h-full bg-transparent border-0 pointer-events-none>
     <ModalMediaPreview v-if="isMediaPreviewOpen" @close="closeMediaPreview()" />
   </ModalDialog>
   <ModalDialog v-model="isEditHistoryDialogOpen">

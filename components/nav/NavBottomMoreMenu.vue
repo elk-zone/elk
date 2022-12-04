e63120cb758a8d39646ab3ce84075e3335f411a1
LittleSound <464388324@qq.com>
2022-12-04 14:25:52 +0800

    style: beautiful

--- components/nav/NavBottomMoreMenu.vue
+++ components/nav/NavBottomMoreMenu.vue
@@ -45,60 +45,65 @@ onBeforeUnmount(() => {
 
     <!-- Drawer -->
     <Transition
-      enter-active-class="transition duration-250 ease-out"
-      enter-from-class="transform translate-y-full opacity-0"
-      enter-to-class="transform translate-y-0 opacity-100"
-      leave-active-class="transition duration-250 ease-in"
-      leave-from-class="transform translate-y-0 opacity-100"
-      leave-to-class="transform translate-y-full opacity-0"
+      enter-active-class="transition duration-250 ease-out children:(transition duration-250 ease-out)"
+      enter-from-class="opacity-0 children:(transform translate-y-full)"
+      enter-to-class="opacity-100 children:(transform translate-y-0)"
+      leave-active-class="transition duration-250 ease-in  children:(transition duration-250 ease-in)"
+      leave-from-class="opacity-100 children:(transform translate-y-0)"
+      leave-to-class="opacity-0 children:(transform translate-y-full)"
       persisted
     >
       <div
         v-show="show"
-        class="
-          absolute inset-x-0 top-auto bottom-full z-20
-          min-w-48 py-2
+        absolute inset-x-0 top-auto bottom-full z-20 h-100vh
+        flex items-end of-hidden
+        bg="black/50"
+      >
+        <div
+          class="
+          flex-1 min-w-48 py-2
           overflow-y-auto overscroll-none scrollbar-hide max-h-[calc(100vh-200px)]
-          rounded-t-lg shadow-none bg-base
+          rounded-t-lg  bg-white/85 dark:bg-neutral-900/85 backdrop-filter backdrop-blur-md
           border-y-1 border-base
         "
-      >
-        <ClientOnly>
-          <!-- Nav -->
-          <NavSide />
+        >
+          <ClientOnly>
+            <!-- Nav -->
+            <NavSide />
 
-          <!-- Divider line -->
-          <div class="*border-lv2" border="t-1" m="x-3 y-2" />
+            <!-- Divider line -->
+            <div border="neutral-300 dark:neutral-700 t-1" m="x-3 y-2" />
 
-          <!-- Function menu -->
-          <button
-            class="
+            <!-- Function menu -->
+            <button
+              class="
               flex flex-row items-center
               block px-4 py-3 focus-blue w-full
               text-sm text-base capitalize text-left whitespace-nowrap
               transition-colors duration-200 transform
             "
-            hover="bg-gray-100 dark:(bg-gray-700 text-white)"
-            @click="toggleDark()"
-          >
-            <span class="i-ri:sun-line dark:i-ri:moon-line flex-shrink-0 text-xl mr-4 !align-middle" />
-            {{ !isDark ? t('button.toggle_dark') : t('button.toggle_light') }}
-          </button>
-          <!-- 切换语言 -->
-          <button
-            class="
+              hover="bg-gray-100 dark:(bg-gray-700 text-white)"
+              @click="toggleDark()"
+            >
+              <span class="i-ri:sun-line dark:i-ri:moon-line flex-shrink-0 text-xl mr-4 !align-middle" />
+              {{ !isDark ? t('button.toggle_dark') : t('button.toggle_light') }}
+            </button>
+            <!-- 切换语言 -->
+            <button
+              class="
               flex flex-row items-center
               block px-4 py-3 focus-blue w-full
               text-sm text-base capitalize text-left whitespace-nowrap
               transition-colors duration-200 transform
             "
-            hover="bg-gray-100 dark:(bg-gray-700 text-white)"
-            @click.stop="toggleLocales"
-          >
-            <span class="i-carbon:language flex-shrink-0 text-xl mr-4 !align-middle" />
-            {{ t('button.toggle_langs') }}
-          </button>
-        </ClientOnly>
+              hover="bg-gray-100 dark:(bg-gray-700 text-white)"
+              @click.stop="toggleLocales"
+            >
+              <span class="i-carbon:language flex-shrink-0 text-xl mr-4 !align-middle" />
+              {{ t('button.toggle_langs') }}
+            </button>
+          </ClientOnly>
+        </div>
       </div>
     </Transition>
   </div>

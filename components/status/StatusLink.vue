783f5bbe440a9d1256030d7178f0f46d1c33810e
Ayo <ramon.aycojr@gmail.com>
2022-12-06 16:20:15 +0100

    style: image bottom border

---
+++ components/status/StatusLink.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { Card } from 'masto'
+
+const prop = defineProps<{
+  card: Card
+}>()
+const alt = $computed(() => `${prop.card.title} - ${prop.card.title}`)
+
+// TODO: handle card.type: CardType
+</script>
+
+<template>
+  <div
+    v-if="card"
+    border="~ base"
+    rounded-lg
+    object-cover
+    h-full
+    w-full
+  >
+    <NuxtLink :to="card.url">
+      <CommonBlurhash
+        v-if="card.image"
+        :blurhash="card.blurhash"
+        class="status-attachment-image"
+        :src="card.image"
+        :width="card.width"
+        :height="card.height"
+        :alt="alt"
+        border-b="~ base"
+        rounded-lg
+        rounded-b-none
+        object-cover
+        h-full
+        w-full
+      />
+      <div p2 max-h-2xl>
+        <p v-if="card.providerName" text-secondary line-clamp-1 text-ellipsis>
+          {{ card.providerName }}
+        </p>
+        <strong v-if="card.title" line-clamp-1 text-ellipsis>{{ card.title }}</strong>
+        <p v-if="card.description" text-secondary line-clamp-2 text-ellipsis>
+          {{ card.description }}
+        </p>
+      </div>
+    </NuxtLink>
+  </div>
+</template>

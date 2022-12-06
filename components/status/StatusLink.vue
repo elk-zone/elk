c04337252a9d773b821717d3837d9a8cc470983f
Ayo <ramon.aycojr@gmail.com>
2022-12-06 16:08:56 +0100

    style: line-clamp and overflow

--- components/status/StatusLink.vue
+++ components/status/StatusLink.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { Card } from 'masto'
 
-// TODO: needs masto.js PR: https://github.com/neet/masto.js/pull/745
 const prop = defineProps<{
   card: Card
 }>()
@@ -9,19 +8,38 @@ const alt = $computed(() => `${prop.card.title} - ${prop.card.title}`)
 </script>
 
 <template>
-  <div v-if="card">
-    <CommonBlurhash
-      :blurhash="card.blurhash"
-      class="status-attachment-image"
-      :src="card.image"
-      :width="card.width"
-      :height="card.height"
-      :alt="alt"
-      border="~ base"
-      rounded-lg
-      h-full
-      w-full
-      object-cover
-    />
+  <div
+    v-if="card"
+    border="~ base"
+    rounded-lg
+    object-cover
+    h-full
+    w-full
+  >
+    <NuxtLink :href="card.url">
+      <CommonBlurhash
+        v-if="card.image"
+        :blurhash="card.blurhash"
+        class="status-attachment-image"
+        :src="card.image"
+        :width="card.width"
+        :height="card.height"
+        :alt="alt"
+        rounded-lg
+        rounded-b-none
+        object-cover
+        h-full
+        w-full
+      />
+      <div p2 max-h-2l>
+        <p v-if="card.providerName" text-secondary line-clamp-1 text-ellipsis>
+          {{ card.providerName }}
+        </p>
+        <strong v-if="card.title" line-clamp-1 text-ellipsis>{{ card.title }}</strong>
+        <p v-if="card.description" text-secondary line-clamp-2 text-ellipsis>
+          {{ card.description }}
+        </p>
+      </div>
+    </NuxtLink>
   </div>
 </template>

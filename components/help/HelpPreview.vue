68d94b47a937d434457aedae48aee89c307c10cc
Daniel Roe <daniel@roe.dev>
2022-12-06 23:07:31 +0000

    perf: use transform on team images

--- components/help/HelpPreview.vue
+++ components/help/HelpPreview.vue
@@ -59,7 +59,7 @@ const teams: Team[] = [
     <p flex="~ gap-2 wrap" mxa>
       <template v-for="team of teams" :key="team.github">
         <a :href="`https://github.com/sponsors/${team.github}`" target="_blank" rounded-full>
-          <img :src="`https://github.com/${team.github}.png`" :alt="team.display" rounded-full w-15 h-15>
+          <img :src="`https://res.cloudinary.com/dchoja2nb/image/twitter_name/h_120,w_120/f_auto/${team.twitter}.jpg`" :alt="team.display" rounded-full w-15 h-15 height="60" width="60">
         </a>
       </template>
     </p>

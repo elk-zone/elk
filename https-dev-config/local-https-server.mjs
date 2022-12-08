429efe8e498d02fc7724c9f29c4afd3a687ab2cf
userquin <userquin@gmail.com>
2022-12-08 14:33:46 +0100

    chore: use import.meta.utl to load ssl stuff

--- https-dev-config/local-https-server.mjs
+++ https-dev-config/local-https-server.mjs
@@ -1,7 +1,8 @@
 import { readFileSync } from 'fs'
+import { fileURLToPath } from 'node:url'
 
-process.env.NITRO_SSL_CERT = readFileSync('./localhost.crt', 'utf8')
-process.env.NITRO_SSL_KEY = readFileSync('./localhost.key', 'utf8')
+process.env.NITRO_SSL_CERT = readFileSync(fileURLToPath(new URL('./localhost.crt', import.meta.url)), 'utf8')
+process.env.NITRO_SSL_KEY = readFileSync(fileURLToPath(new URL('./localhost.key', import.meta.url)), 'utf8')
 
 async function run() {
   await import('../.output/server/index.mjs')

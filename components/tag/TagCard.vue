22c3ce90c4320489611441f8e706675fe50bc8a5
LittleSound <464388324@qq.com>
2022-12-10 16:46:56 +0800

    feat: WIP tags page

---
+++ components/tag/TagCard.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { Tag } from 'masto'
+
+const {
+  tag,
+} = $defineProps<{
+  tag: Tag
+}>()
+
+const to = $computed(() => new URL(tag.url).pathname)
+</script>
+
+<template>
+  <NuxtLink :to="to" block p4>
+    <h4 font-medium line-clamp-1 break-all ws-pre-wrap>
+      <spn>#</spn>
+      <span hover:underline>{{ tag.name }}</span>
+    </h4>
+    <p text-secondary-light>
+      {{ tag.history?.[0].uses }}
+    </p>
+  </NuxtLink>
+</template>
--- pages/[[server]]/explore/tags.vue
+++ pages/[[server]]/explore/tags.vue
@@ -1,13 +1,36 @@
 <script lang="ts" setup>
+import type { Tag } from 'masto'
+import { STORAGE_KEY_HIDE_TAGS_TIPS } from '~~/constants'
+const { data, pending, error } = useLazyAsyncData(() => useMasto().trends.fetchTags(), { immediate: true })
+
+const hideTagsTips = useLocalStorage(STORAGE_KEY_HIDE_TAGS_TIPS, false)
+
+function getTagUrl(tag: Tag) {
+  return new URL(tag.url).pathname
+}
 </script>
 
 <template>
-  <div mt-10 space-y-3 text-center>
-    <h1 text-5xl>
-      🚧
-    </h1>
-    <p text-xl>
-      This feature is under development
-    </p>
+  <CommonAlert v-if="!hideTagsTips && data && data.length" @close="hideTagsTips = true">
+    <p>{{ $t('tooltip.explore_tags_intro') }}</p>
+  </CommonAlert>
+
+  <div v-if="data && data.length">
+    <TagCard v-for="item of data" :key="item.name" :tag="item" border="b base">
+      {{ item.name }}
+      {{ item.following }}
+      {{ item.url }}
+    </TagCard>
+  </div>
+  <div v-else-if="pending">
+    <StatusCardSkeleton border="b base" />
+    <StatusCardSkeleton border="b base" op50 />
+    <StatusCardSkeleton border="b base" op25 />
+  </div>
+  <div v-else-if="error" p5 text-center text-red italic>
+    {{ $t('common.error') }}: {{ error }}
+  </div>
+  <div v-else p5 text-center text-secondary italic>
+    {{ $t('error.explore-list-empty') }}
   </div>
 </template>

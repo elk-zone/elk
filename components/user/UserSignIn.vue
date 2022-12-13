b69fad405f5b15ca39419ca2cb6745c3f40e2acd
userquin <userquin@gmail.com>
2022-12-13 23:26:18 +0100

    feat: enhance login and error handling

--- components/user/UserSignIn.vue
+++ components/user/UserSignIn.vue
@@ -3,16 +3,43 @@ import { DEFAULT_SERVER } from '~/constants'
 
 const input = $ref<HTMLInputElement>()
 let server = $ref<string>('')
+let busy = $ref(false)
+let error = $ref(false)
+let showError = $ref(false)
 
 async function oauth() {
+  if (busy)
+    return
+
+  busy = true
+  error = false
+  showError = false
+
   if (server)
     server = server.split('/')[0]
-  location.href = `/api/${server || DEFAULT_SERVER}/login`
+
+  try {
+    location.href = await $fetch(`/api/${server || DEFAULT_SERVER}/login`)
+  }
+  catch {
+    showError = true
+    error = true
+    await nextTick()
+    input?.focus()
+    await nextTick()
+    setTimeout(() => {
+      busy = false
+      error = false
+    }, 512)
+  }
 }
 
 async function handleInput() {
   if (server.startsWith('https://'))
     server = server.replace('https://', '')
+
+  if (server.length > 0)
+    showError = false
 }
 
 onMounted(() => {
@@ -28,10 +55,31 @@ onMounted(() => {
         {{ $t('action.sign_in') }}
       </div>
     </div>
-    <div>{{ $t('user.server_address_label') }}</div>
-    <div flex bg-gray:10 px4 py2 mxa rounded border="~ base" items-center font-mono focus:outline-none focus:ring="2 primary inset">
-      <span text-secondary-light mr1>https://</span>
-      <input ref="input" v-model="server" outline-none bg-transparent w-full max-w-50 @input="handleInput">
+    <div>
+      {{ $t('user.server_address_label') }}
+    </div>
+    <div :class="error ? 'animate animate-shake-x animate-delay-100' : null">
+      <div
+        flex bg-gray:10 px4 py2 mxa rounded
+        border="~ base" items-center font-mono
+        focus:outline-none focus:ring="2 primary inset"
+        :class="showError ? 'border-red-600 dark:border-red-400' : null"
+      >
+        <span text-secondary-light mr1>https://</span>
+        <input
+          ref="input"
+          v-model="server"
+          outline-none bg-transparent w-full max-w-50
+          @input="handleInput"
+        >
+      </div>
+      <div min-h-4>
+        <Transition css enter-active-class="animate animate-fade-in">
+          <p v-if="showError" role="alert" p-0 m-0 text-xs text-red-600 dark:text-red-400>
+            {{ $t('error.sign_in_error') }}
+          </p>
+        </Transition>
+      </div>
     </div>
     <div text-secondary text-sm flex>
       <div i-ri:lightbulb-line mr-1 />
@@ -41,7 +89,8 @@ onMounted(() => {
         </i18n-t>
       </span>
     </div>
-    <button btn-solid mt2 :disabled="!server">
+    <button flex="~ row" gap-x-2 items-center btn-solid mt2 :disabled="!server || busy">
+      <span aria-hidden="true" inline-block :class="busy ? 'i-ri:loader-2-fill animate animate-spin' : 'i-ri:login-circle-line'" />
       {{ $t('action.sign_in') }}
     </button>
   </form>

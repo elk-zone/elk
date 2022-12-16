87266e7f2c1e0d50d8978e0c4560b80947706136
userquin <userquin@gmail.com>
2022-12-16 15:11:06 +0100

    chore: add locale change announce

--- components/aria/AriaAnnouncer.vue
+++ components/aria/AriaAnnouncer.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import type { ComputedRef } from 'vue'
 import type { AriaAnnounceType, AriaLive } from '~/composables/aria/types'
 import { useAriaAnnouncer } from '~/composables/aria'
+import type { LocaleObject } from '#i18n'
 
 const router = useRouter()
-const { t } = useI18n()
+const { t, locale, locales } = useI18n()
 const { ariaAnnouncer, announce } = useAriaAnnouncer()
 
 let ariaLive = $ref<AriaLive>('polite')
@@ -17,6 +19,18 @@ const onMessage = (event: AriaAnnounceType, message?: string) => {
   else
     ariaLive = 'polite'
 }
+
+watch(locale, (ol, l) => {
+  if (ol) {
+    const ls = locales as ComputedRef<LocaleObject[]>
+    const localName = ls.value.find(x => x.code === ol)?.name
+    const toLocalName = ls.value.find(x => x.code === ol)?.name
+    announce(t('a11y.locale_changing', [localName ?? ol]))
+    setTimeout(() => {
+      announce(t('a11y.locale_changed', [toLocalName ?? l]))
+    }, 1000)
+  }
+}, { immediate: true })
 
 onMounted(() => {
   ariaAnnouncer.on(onMessage)

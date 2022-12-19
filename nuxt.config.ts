8fb35e61b27387c9c0d41c21c76a4cf8d555c86d
userquin <userquin@gmail.com>
2022-12-19 22:59:10 +0100

    cleanup: remove unused icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -112,11 +112,6 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        // { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        // { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' },
-        // { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
-        // { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
-        // { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ffffff' },
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
       ],
     },

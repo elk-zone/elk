ee9627108cd06725898c07d99a7f7d5625046666
Daniel Roe <daniel@roe.dev>
2022-12-21 13:58:44 +0000

    fix: revert 92475fdbf4fa5bf7c4cf1564823f7bf75c1d39f6

--- middleware/link.global.ts
+++
@@ -1,6 +0,0 @@
-const reloadRoutes = new Set(['/invite'])
-
-export default defineNuxtRouteMiddleware((to) => {
-  if (process.client && reloadRoutes.has(to.fullPath))
-    window.location.pathname = to.path
-})

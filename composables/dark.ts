447624af80fcb52ab682b067862f24b34e9d4f11
userquin <userquin@gmail.com>
2022-12-22 12:21:06 +0100

    feat: dark theme on samsung internet browser

--- composables/dark.ts
+++ composables/dark.ts
@@ -1,2 +1,13 @@
-export const isDark = useDark()
+const dark = useDark()
+
+const samsungBrowser = isSamsungBrowser()
+
+export const isDark = computed(() => dark.value && !samsungBrowser)
 export const toggleDark = useToggle(isDark)
+
+function isSamsungBrowser() {
+  if (typeof window === 'undefined')
+    return false
+
+  return navigator.userAgent.match(/SamsungBrowser/i)
+}

db4cfe0a9b1b41876aacbf1147f37de74b90d9c6
patak <matias.capeletto@gmail.com>
2022-12-23 06:06:41 +0100

    feat: re-apply timelines loading optimization

---
+++ components/timeline/TimelineBlocks.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const paginator = useMasto().blocks.iterate()
+</script>
+
+<template>
+  <AccountPaginator :paginator="paginator" />
+</template>
---
+++ components/timeline/TimelinePinned.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const paginator = useMasto().accounts.iterateStatuses(currentUser.value!.account.id, { pinned: true })
+</script>
+
+<template>
+  <TimelinePaginator :paginator="paginator" />
+</template>

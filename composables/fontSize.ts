6c9908d030d9744d140207dbbec165ca31464b2f
Anthony Fu <anthonyfu117@hotmail.com>
2022-12-24 00:22:57 +0100

    chore: clean up

--- composables/fontSize.ts
+++ composables/fontSize.ts
@@ -1,18 +1,16 @@
 import type { InjectionKey, Ref } from 'vue'
 import { STORAGE_KEY_FONT_SIZE } from '~/constants'
 
-export type FontSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-export const InjectionKeyFontSize = Symbol('fontSize') as InjectionKey<Ref<FontSize>>
+const InjectionKeyFontSize = Symbol('fontSize') as InjectionKey<Ref<FontSize>>
+const DEFAULT = 'md'
 
-export function setFontSize(size: FontSize) {
-  inject(InjectionKeyFontSize)!.value = size
-}
+export type FontSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 
 export function getFontSize() {
   return inject(InjectionKeyFontSize)!
 }
 
-export const fontSizeMap = {
+const fontSizeMap = {
   xs: '13px',
   sm: '14px',
   md: '15px',
@@ -21,19 +19,19 @@ export const fontSizeMap = {
 }
 
 export async function setupFontSize() {
-  const fontSize = useCookie<FontSize>(STORAGE_KEY_FONT_SIZE, { default: () => 'md' })
+  const fontSize = useCookie<FontSize>(STORAGE_KEY_FONT_SIZE, { default: () => DEFAULT })
   getCurrentInstance()?.appContext.app.provide(InjectionKeyFontSize, fontSize)
 
   if (!process.server) {
     watchEffect(() => {
-      document.documentElement.style.setProperty('--font-size', fontSizeMap[fontSize.value || 'md'])
+      document.documentElement.style.setProperty('--font-size', fontSizeMap[fontSize.value || DEFAULT])
     })
   }
   else {
     useHead({
       style: [
         {
-          innerHTML: `:root { --font-size: ${fontSizeMap[fontSize.value || 'md']}; }`,
+          innerHTML: `:root { --font-size: ${fontSizeMap[fontSize.value || DEFAULT]}; }`,
         },
       ],
     })

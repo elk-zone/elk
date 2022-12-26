95c7ef103d5fff0ee21aa6ccbf9c07643531f958
userquin <userquin@gmail.com>
2022-12-26 14:14:27 +0100

    chore: add `ar` locale and `dir=rtl` to html

--- config/i18n.ts
+++ config/i18n.ts
@@ -2,41 +2,55 @@ import type { NuxtI18nOptions } from '@nuxtjs/i18n'
 import type { DateTimeFormats } from '@intlify/core-base'
 import type { LocaleObject } from '#i18n'
 
+// @ts-expect-error dir is there, ts complaining
 const locales: LocaleObject[] = [
   {
     code: 'en-US',
     file: 'en-US.json',
     name: 'English',
+    dir: 'ltr',
   },
   {
     code: 'de-DE',
     file: 'de-DE.json',
     name: 'Deutsch',
+    dir: 'ltr',
   },
   {
     code: 'zh-CN',
     file: 'zh-CN.json',
     name: '简体中文',
+    dir: 'ltr',
   },
   {
     code: 'ja-JP',
     file: 'ja-JP.json',
     name: '日本語',
+    dir: 'ltr',
   },
   {
     code: 'es-ES',
     file: 'es-ES.json',
     name: 'Español',
+    dir: 'ltr',
   },
   {
     code: 'fr-FR',
     file: 'fr-FR.json',
     name: 'Français',
+    dir: 'ltr',
   },
   {
     code: 'cs-CZ',
     file: 'cs-CZ.json',
     name: 'Česky',
+    dir: 'ltr',
+  },
+  {
+    code: 'ar',
+    file: 'ar-EG.json',
+    name: 'عربي',
+    dir: 'rtl',
   },
 ].sort((a, b) => a.code.localeCompare(b.code))
 

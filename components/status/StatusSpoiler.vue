ae92ffb928b51c96da69a428a232fb4b698cde5e
Ayo <ramon.aycojr@gmail.com>
2022-12-26 18:24:18 +0100

    fix: small "show more" button, a11y issue

--- components/status/StatusSpoiler.vue
+++ components/status/StatusSpoiler.vue
@@ -11,11 +11,11 @@ watchEffect(() => {
 
 <template>
   <div v-if="enabled" flex flex-col items-start>
-    <div class="content-rich" px-4 pb-2 text-center text-secondary text-sm w-full border="~ base" border-0 border-b-dotted border-b-3 mt-2>
+    <div class="content-rich" px-4 pb-2 text-center text-secondary w-full border="~ base" border-0 border-b-dotted border-b-3 mt-2>
       <slot name="spoiler" />
     </div>
     <div flex="~ gap-1 center" w-full mt="-3.5">
-      <button btn-text px-2 py-1 text-3 bg-base flex="~ center gap-2" :class="showContent ? '' : 'filter-saturate-0 hover:filter-saturate-100'" @click="toggleContent()">
+      <button btn-text px-2 py-1 bg-base flex="~ center gap-2" :class="showContent ? '' : 'filter-saturate-0 hover:filter-saturate-100'" @click="toggleContent()">
         <div v-if="showContent" i-ri:eye-line />
         <div v-else i-ri:eye-close-line />
         {{ showContent ? $t('status.spoiler_show_less') : $t(filter ? 'status.filter_show_anyway' : 'status.spoiler_show_more') }}

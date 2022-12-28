363cca318f5b64c06b9f4901756782820004b446
Jacob Hands <jacob@gogit.io>
2022-12-28 15:18:04 -0600

    fix: Public timelines not starting websockets
    Loading Public or Public Local timelines directly resulted in streaming
    websockets not being opened. This is because it was not waiting for
    isMastoInitialised before loading the stream, which meant that
    useMasto().stream was undefined.

---
+++ components/timeline/TimelinePublic.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const paginator = useMasto().timelines.iteratePublic()
+const stream = useMasto().stream.streamPublicTimeline()
+onBeforeUnmount(() => stream.then(s => s.disconnect()))
+</script>
+
+<template>
+  <div>
+    <TimelinePaginator v-bind="{ paginator, stream }" context="public" />
+  </div>
+</template>
---
+++ components/timeline/TimelinePublicLocal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const paginator = useMasto().timelines.iteratePublic({ local: true })
+const stream = useMasto().stream.streamCommunityTimeline()
+onBeforeUnmount(() => stream.then(s => s.disconnect()))
+</script>
+
+<template>
+  <div>
+    <TimelinePaginator v-bind="{ paginator, stream }" context="public" />
+  </div>
+</template>

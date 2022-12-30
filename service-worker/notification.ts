d92394e73518b54d3058f2db9c142a1957154703
userquin <userquin@gmail.com>
2022-12-30 15:35:37 +0100

    chore: update sw notification logic

--- service-worker/notification.ts
+++ service-worker/notification.ts
@@ -6,16 +6,16 @@ export const findNotification = async (access_token: string, notificationId: str
   if (!users)
     return undefined
 
-  const user = users.filter(user => user.token === access_token)
-  if (!user || user.length === 0)
+  const filteredUsers = users.filter(user => user.token === access_token)
+  if (!filteredUsers || filteredUsers.length === 0)
     return undefined
 
-  for (const u of users) {
+  for (const user of filteredUsers) {
     try {
-      const response = await fetch(`https://${u.server}/api/v1/notifications/${notificationId}`, {
+      const response = await fetch(`https://${user.server}/api/v1/notifications/${notificationId}`, {
         method: 'get',
         headers: {
-          'Authorization': `Bearer ${u.token}`,
+          'Authorization': `Bearer ${user.token}`,
           'Content-Type': 'application/json',
         },
       })
@@ -24,8 +24,8 @@ export const findNotification = async (access_token: string, notificationId: str
 
       const notification = await response.json()
 
-      if (u.server === notification.status?.server)
-        return { user: u, notification }
+      if (user.server === notification.status?.server)
+        return { user, notification }
     }
     catch {
       // just ignore

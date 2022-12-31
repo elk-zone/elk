943963a05f7ffa759cddd191e28e5f204af89459
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-12-31 10:44:41 +0200

    fix: always display account handle in LTR

--- components/account/AccountHandle.vue
+++ components/account/AccountHandle.vue
@@ -10,8 +10,10 @@ const serverName = $computed(() => getServerName(account))
 
 <template>
   <p line-clamp-1 whitespace-pre-wrap break-all text-secondary-light>
-    <!-- fix: #274 only line-clamp-1 can be used here, using text-ellipsis is not valid -->
-    <span text-secondary>{{ getShortHandle(account) }}</span>
-    <span v-if="serverName" text-secondary-light>@{{ serverName }}</span>
+    <span dir="ltr">
+      <!-- fix: #274 only line-clamp-1 can be used here, using text-ellipsis is not valid -->
+      <span text-secondary>{{ getShortHandle(account) }}</span>
+      <span v-if="serverName" text-secondary-light>@{{ serverName }}</span>
+    </span>
   </p>
 </template>

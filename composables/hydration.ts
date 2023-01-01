0187763c2947b71a31d059b1e32cb8074b473d01
Daniel Roe <daniel@roe.dev>
2023-01-02 00:18:05 +0100

    refactor: set hydrated value in nuxt plugin

--- composables/hydration.ts
+++ composables/hydration.ts
@@ -1,8 +1,1 @@
 export const isHydrated = ref(false)
-
-if (!process.server) {
-  const nuxtApp = useNuxtApp()
-  nuxtApp.hooks.hookOnce('app:suspense:resolve', () => {
-    isHydrated.value = true
-  })
-}

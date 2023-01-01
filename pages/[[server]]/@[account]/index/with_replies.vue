532d798734a02270bd143ade14ee387e6f9f5116
Daniel Roe <daniel@roe.dev>
2023-01-02 00:55:38 +0100

    fix: remove history state fallback + asyncdata

--- pages/[[server]]/@[account]/index/with_replies.vue
+++ pages/[[server]]/@[account]/index/with_replies.vue
@@ -7,16 +7,13 @@ const { t } = useI18n()
 const params = useRoute().params
 const handle = $(computedEager(() => params.account as string))
 
-const { data: account } = await useAsyncData(`account:${handle}`, async () => (
-  window.history.state?.account as Account | undefined)
-    ?? await fetchAccountByHandle(handle),
-)
+const account = await fetchAccountByHandle(handle)
 
-const paginator = useMasto().accounts.iterateStatuses(account.value!.id, { excludeReplies: false })
+const paginator = useMasto().accounts.iterateStatuses(account.id, { excludeReplies: false })
 
 if (account) {
   useHeadFixed({
-    title: () => `${t('tab.posts_with_replies')} | ${getDisplayName(account.value!)} (@${account.value!.acct})`,
+    title: () => `${t('tab.posts_with_replies')} | ${getDisplayName(account)} (@${account.acct})`,
   })
 }
 </script>

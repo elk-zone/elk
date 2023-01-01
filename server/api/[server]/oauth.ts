94db42ae9ef8142270d9d8c84b87e88a4e11548c
三咲智子 <sxzz@sxzz.moe>
2023-01-02 02:09:22 +0800

    feat: get origin from client

--- server/api/[server]/oauth.ts
+++ server/api/[server]/oauth.ts
@@ -3,9 +3,10 @@ import { createError, defineEventHandler, getQuery, getRouterParams, sendRedirec
 import { getApp, getRedirectURI } from '~/server/shared'
 
 export default defineEventHandler(async (event) => {
+  const { origin } = getQuery(event) as { origin: string }
   let { server } = getRouterParams(event)
   server = server.toLocaleLowerCase().trim()
-  const app = await getApp(server)
+  const app = await getApp(origin, server)
 
   if (!app) {
     throw createError({
@@ -27,7 +28,7 @@ export default defineEventHandler(async (event) => {
     body: {
       client_id: app.client_id,
       client_secret: app.client_secret,
-      redirect_uri: getRedirectURI(server),
+      redirect_uri: getRedirectURI(origin, server),
       grant_type: 'authorization_code',
       code,
       scope: 'read write follow push',

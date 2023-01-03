11876dc1e63f2af2e248d384a88adf2446889834
Joel Drake <joel.drake@ica.se>
2023-01-03 17:49:07 +0100

    simplification

--- composables/screen.ts
+++ composables/screen.ts
@@ -2,6 +2,5 @@ import { breakpointsTailwind } from '@vueuse/core'
 
 export const breakpoints = useBreakpoints(breakpointsTailwind)
 
-export const isMobileScreen = breakpoints.smallerOrEqual('sm')
 export const isSmallScreen = breakpoints.smallerOrEqual('md')
 export const isMediumScreen = breakpoints.smallerOrEqual('lg')

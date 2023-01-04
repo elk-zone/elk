1e4bfe8e802956bdb38f51637f1af2da2ac57671
三咲智子 <sxzz@sxzz.moe>
2023-01-04 21:04:23 +0800

    chore: add comments

--- config/env.ts
+++ config/env.ts
@@ -2,7 +2,21 @@ import Git from 'simple-git'
 import { isDevelopment } from 'std-env'
 
 export { version } from '../package.json'
+
+/**
+ * Environment variable `PULL_REQUEST` provided by Netlify.
+ * @see {@link https://docs.netlify.com/configure-builds/environment-variables/#git-metadata}
+ *
+ * Whether triggered by a GitHub PR
+ */
 export const isPR = process.env.PULL_REQUEST === 'true'
+
+/**
+ * Environment variable `CONTEXT` provided by Netlify.
+ * @see {@link https://docs.netlify.com/configure-builds/environment-variables/#build-metadata}
+ *
+ * Whether triggered by PR, `deploy-preview` or `dev`.
+ */
 export const isPreview = isPR || process.env.CONTEXT === 'deploy-preview' || process.env.CONTEXT === 'dev'
 
 const git = Git()

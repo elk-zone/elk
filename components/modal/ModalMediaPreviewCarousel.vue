121a9f3c548f63a29b3c2d03cb023b7a8cfb4934
wheatjs <jacobrclevenger@gmail.com>
2023-01-04 01:15:56 -0500

    feat: add image preview swipe

---
+++ components/modal/ModalMediaPreviewCarousel.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { SwipeDirection } from '@vueuse/core'
+import { useReducedMotion } from '@vueuse/motion'
+import type { Attachment } from 'masto'
+
+const props = withDefaults(defineProps<{ media: Attachment[]; threshold: number; modelValue: number }>(), {
+  media: [] as any,
+  threshold: 20,
+  modelValue: 0,
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', v: boolean): void
+  (event: 'close'): void
+}>()
+
+const target = ref()
+const index = useVModel(props, 'modelValue', emit)
+
+const animateTimeout = useTimeout(10)
+const reduceMotion = useReducedMotion()
+
+const canAnimate = computed(() => !reduceMotion.value && animateTimeout.value)
+
+const { width, height } = useElementSize(target)
+const { isSwiping, lengthX, lengthY, direction } = useSwipe(target, {
+  threshold: 5,
+  passive: false,
+  onSwipeEnd, // eslint-disable-line @typescript-eslint/no-use-before-define,
+})
+
+const distanceX = computed(() => {
+  if (width.value === 0)
+    return 0
+
+  if (!isSwiping.value || (direction.value !== SwipeDirection.LEFT && direction.value !== SwipeDirection.RIGHT))
+    return index.value * 100 * -1
+
+  return (lengthX.value / width.value) * 100 * -1 + (index.value * 100) * -1
+})
+
+const distanceY = computed(() => {
+  if (height.value === 0 || !isSwiping.value || direction.value !== SwipeDirection.UP)
+    return 0
+
+  return (lengthY.value / height.value) * 100 * -1
+})
+
+const onSwipeEnd = (e: TouchEvent, direction: SwipeDirection) => {
+  if (direction === SwipeDirection.RIGHT && Math.abs(distanceX.value) > props.threshold)
+    index.value = Math.max(0, index.value - 1)
+
+  if (direction === SwipeDirection.LEFT && Math.abs(distanceX.value) > props.threshold)
+    index.value = Math.min(props.media.length - 1, index.value + 1)
+
+  if (direction === SwipeDirection.UP && Math.abs(distanceY.value) > props.threshold)
+    emit('close')
+}
+</script>
+
+<template>
+  <div ref="target" w-full overflow-hidden flex flex-row items-center class="test">
+    <div w-full flex flex-row :style="{ transform: `translateX(${distanceX}%) translateY(${distanceY}%)`, transition: isSwiping ? 'none' : canAnimate ? 'all 0.5s ease' : 'none' }">
+      <div v-for="item in media" :key="item.id" p4 select-none w-full flex-shrink-0 text-center text-7xl flex flex-col place-content-center place-items-center>
+        <img :draggable="false" select-none :src="item.url || item.previewUrl" :alt="item.description || ''">
+      </div>
+    </div>
+  </div>
+</template>

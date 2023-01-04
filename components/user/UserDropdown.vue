53dcd8b28f949361f399dc14f3722a063c6ac311
三咲智子 <sxzz@sxzz.moe>
2023-01-04 22:15:24 +0800

    fix: resolve conflicts

--- components/user/UserDropdown.vue
+++ components/user/UserDropdown.vue
@@ -3,7 +3,7 @@
     <button btn-action-icon :aria-label="$t('action.switch_account')">
       <!-- TODO -->
       <div :class="{ 'hidden xl:block': !isGuest }" i-ri:more-2-line />
-      <AccountAvatar v-if="!isGuest" xl:hidden :account="account" w-9 h-9 />
+      <AccountAvatar v-if="!isGuest" xl:hidden :account="currentUser.account" w-9 h-9 />
       <span v-else>TODO: Guest</span>
     </button>
     <template #popper="{ hide }">

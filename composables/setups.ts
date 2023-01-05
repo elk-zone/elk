7fb563c8cc455fa25118f8e7954ec26e50075500
Daniel Roe <daniel@roe.dev>
2023-01-05 22:01:31 +0000

    refactor: simplify head config slightly

--- composables/setups.ts
+++ composables/setups.ts
@@ -1,20 +1,9 @@
-import type { Link } from '@unhead/schema'
 import type { Directions } from 'vue-i18n-routing'
 import { buildInfo } from 'virtual:build-info'
 import type { LocaleObject } from '#i18n'
 
 export function setupPageHeader() {
   const { locale, locales, t } = useI18n()
-
-  const link = process.client && useRuntimeConfig().public.pwaEnabled
-    ? computed<Link[]>(() => {
-      return [{
-        key: `webmanifest-${locale.value}`,
-        rel: 'manifest',
-        href: `/manifest-${locale.value}.webmanifest`,
-      }]
-    })
-    : []
 
   const localeMap = (locales.value as LocaleObject[]).reduce((acc, l) => {
     acc[l.code!] = l.dir ?? 'auto'
@@ -33,6 +22,12 @@ export function setupPageHeader() {
         titleTemplate += ` (${buildInfo.env})`
       return titleTemplate
     },
-    link,
+    link: process.client && useRuntimeConfig().public.pwaEnabled
+      ? () => [{
+          key: `webmanifest-${locale.value}`,
+          rel: 'manifest',
+          href: `/manifest-${locale.value}.webmanifest`,
+        }]
+      : [],
   })
 }

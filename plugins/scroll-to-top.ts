8e19574ea17755f01a4458077279efd34cd40cba
userquin <userquin@gmail.com>
2023-01-07 19:11:10 +0100

    chore: cleanup checks

--- plugins/scroll-to-top.ts
+++ plugins/scroll-to-top.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxt) => {
       scrollToTop: (evt?: MouseEvent | KeyboardEvent) => {
         const path = evt?.composedPath?.() as HTMLElement[]
         const el = path?.find(el => el.tagName?.toUpperCase() === 'A') as HTMLAnchorElement
-        if (el && el.href) {
+        if (el?.href) {
           if (nuxt.$preventScrollToTop(new URL(el.href, import.meta.url).pathname))
             return
         }

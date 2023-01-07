c5f84ce98a36c5b6c31b3333bbb9ef417f1312e3
Daniel Roe <daniel@roe.dev>
2023-01-07 09:35:19 +0000

    docs: add documentation scaffold

---
+++ docs/app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Docus',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'nuxt_themes',
+      github: 'nuxt-themes/docus',
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'IconNuxtLabs',
+        },
+      ],
+    },
+  },
+})

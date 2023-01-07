2a30510127aaef5b92dcee39906090a373ca53df
userquin <userquin@gmail.com>
2023-01-07 18:29:36 +0100

    chore: prevent scroll on nav links

--- plugins/scroll-to-top.ts
+++ plugins/scroll-to-top.ts
@@ -1,7 +1,14 @@
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin((nuxt) => {
   return {
     provide: {
-      scrollToTop: () => {
+      scrollToTop: (evt?: MouseEvent | KeyboardEvent) => {
+        const path = evt?.composedPath?.() as HTMLElement[]
+        const el = path?.find(el => el.tagName?.toUpperCase() === 'A') as HTMLAnchorElement
+        if (el && el.href) {
+          if (nuxt.$preventScrollToTop(new URL(el.href, import.meta.url).pathname))
+            return
+        }
+
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
       },
     },

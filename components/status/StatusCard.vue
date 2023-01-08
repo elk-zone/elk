3d252431665ceb3398574b85b1922763d42cb6fb
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-08 09:27:21 +0100

    feat(ui): new replying status design (#846)

--- components/status/StatusCard.vue
+++ components/status/StatusCard.vue
@@ -15,6 +15,7 @@ const props = withDefaults(
     // Manual overrides
     hasOlder?: boolean
     hasNewer?: boolean
+
     // When looking into a detailed view of a post, we can simplify the replying badges
     // to the main expanded post
     main?: mastodon.v1.Status
@@ -32,8 +33,12 @@ const status = $computed(() => {
 const directReply = $computed(() => props.hasNewer || (!!status.inReplyToId && (status.inReplyToId === props.newer?.id || status.inReplyToId === props.newer?.reblog?.id)))
 // Use reblogged status, connect it to further replies
 const connectReply = $computed(() => props.hasOlder || status.id === props.older?.inReplyToId || status.id === props.older?.reblog?.inReplyToId)
+// Open a detailed status, the replies directly to it
+const replyToMain = $computed(() => props.main && props.main.id === status.inReplyToId)
 
 const rebloggedBy = $computed(() => props.status.reblog ? props.status.account : null)
+
+const statusRoute = $computed(() => getStatusRoute(status))
 
 const el = ref<HTMLElement>()
 const router = useRouter()
@@ -47,13 +52,12 @@ function onclick(evt: MouseEvent | KeyboardEvent) {
 }
 
 function go(evt: MouseEvent | KeyboardEvent) {
-  const route = getStatusRoute(status)
   if (evt.metaKey || evt.ctrlKey) {
-    window.open(route.href)
+    window.open(statusRoute.href)
   }
   else {
     cacheStatus(status)
-    router.push(route)
+    router.push(statusRoute)
   }
 }
 
@@ -70,17 +74,12 @@ const filter = $computed(() => filterResult?.filter as mastodon.v2.Filter)
 const filterPhrase = $computed(() => filter?.phrase || (filter as any)?.title)
 const isFiltered = $computed(() => filterPhrase && (props.context ? filter?.context.includes(props.context) : false))
 
+const isSelfReply = $computed(() => status.inReplyToAccountId === status.account.id)
 const collapseRebloggedBy = $computed(() => rebloggedBy?.id === status.account.id)
-
-// Collapse ReplyingTo badge if it is a self-reply (thread)
-const collapseReplyingTo = $computed(() => (!rebloggedBy || collapseRebloggedBy) && status.inReplyToAccountId === status.account.id)
-
-// Only show avatar in ReplyingTo badge if it was reblogged by the same account or if it is against the main post
-const simplifyReplyingTo = $computed(() =>
-  (props.main && props.main.account.id === status.inReplyToAccountId) || (rebloggedBy && rebloggedBy.id === status.inReplyToAccountId),
-)
-
 const isDM = $computed(() => status.visibility === 'direct')
+
+const showUpperBorder = $computed(() => props.newer && !directReply)
+const showReplyTo = $computed(() => !replyToMain && !directReply)
 </script>
 
 <template>
@@ -88,8 +87,7 @@ const isDM = $computed(() => status.visibility === 'direct')
     v-if="filter?.filterAction !== 'hide'"
     :id="`status-${status.id}`"
     ref="el"
-    relative flex flex-col gap-1 pl-3 pr-4 pt-1
-    class="pb-1.5"
+    relative flex="~ col gap1" p="l-3 r-4 b-2"
     :class="{ 'hover:bg-active': hover }"
     tabindex="0"
     focus:outline-none focus-visible:ring="2 primary"
@@ -97,10 +95,37 @@ const isDM = $computed(() => status.visibility === 'direct')
     @click="onclick"
     @keydown.enter="onclick"
   >
-    <div v-if="newer && !directReply" w-auto h-1px bg-border />
-    <div flex justify-between>
+    <!-- Upper border -->
+    <div :h="showUpperBorder ? '1px' : '0'" w-auto bg-border mb-1 />
+
+    <!-- Line connecting to previous status -->
+    <template v-if="status.inReplyToAccountId">
+      <StatusReplyingTo
+        v-if="showReplyTo"
+        ml-6 pt-1 pl-5
+        :status="status"
+        :is-self-reply="isSelfReply"
+        :class="faded ? 'text-secondary-light' : ''"
+      />
+      <div flex="~ col gap-1" items-center pos="absolute top-0 left-0" w="20.5" z--1>
+        <template v-if="showReplyTo">
+          <div w="1px" h="0.5" border="x base" mt-3 />
+          <div w="1px" h="0.5" border="x base" />
+          <div w="1px" h="0.5" border="x base" />
+        </template>
+        <div w="1px" h-10 border="x base" />
+      </div>
+    </template>
+
+    <!-- Reblog status & Meta -->
+    <div flex="~ col" justify-between>
       <slot name="meta">
-        <div v-if="rebloggedBy && !collapseRebloggedBy" relative text-secondary ws-nowrap flex="~" items-center p="t-1 b-0.5 x-1px" bg-base>
+        <div
+          v-if="rebloggedBy && !collapseRebloggedBy"
+          flex="~" items-center
+          p="t-1 b-0.5 x-1px"
+          relative text-secondary ws-nowrap
+        >
           <div i-ri:repeat-fill me-46px text-primary w-16px h-16px />
           <div absolute top-1 ms-24px w-32px h-32px rounded-full>
             <AccountHoverWrapper :account="rebloggedBy">
@@ -111,11 +136,11 @@ const isDM = $computed(() => status.visibility === 'direct')
           </div>
           <AccountInlineInfo font-bold :account="rebloggedBy" :avatar="false" text-sm />
         </div>
-        <div v-else />
       </slot>
-      <StatusReplyingTo v-if="!directReply && !collapseReplyingTo" :status="status" :simplified="!!simplifyReplyingTo" :class="faded ? 'text-secondary-light' : ''" pt1 />
     </div>
+
     <div flex gap-3 :class="{ 'text-secondary': faded }">
+      <!-- Avatar -->
       <div relative>
         <div v-if="collapseRebloggedBy" absolute flex items-center justify-center top--6px px-2px py-3px rounded-full bg-base>
           <div i-ri:repeat-fill text-primary w-16px h-16px />
@@ -125,24 +150,25 @@ const isDM = $computed(() => status.visibility === 'direct')
             <AccountBigAvatar :account="status.account" />
           </NuxtLink>
         </AccountHoverWrapper>
-        <div v-if="connectReply" w-full h-full flex justify-center>
-          <div class="w-2.5px" bg-primary-light />
+
+        <div v-if="connectReply" w-full h-full flex mt--3px justify-center>
+          <div w-1px border="x base" />
         </div>
       </div>
+
+      <!-- Main -->
       <div flex="~ col 1" min-w-0>
+        <!-- Account Info -->
         <div flex items-center space-x-1>
           <AccountHoverWrapper :account="status.account">
             <StatusAccountDetails :account="status.account" />
           </AccountHoverWrapper>
-          <div v-if="!directReply && collapseReplyingTo" flex="~" ps-1 items-center justify-center>
-            <StatusReplyingTo :collapsed="true" :status="status" :class="faded ? 'text-secondary-light' : ''" />
-          </div>
           <div flex-auto />
           <div v-show="!userSettings.zenMode" text-sm text-secondary flex="~ row nowrap" hover:underline>
             <AccountBotIndicator v-if="status.account.bot" me-2 />
             <div flex>
               <CommonTooltip :content="createdAt">
-                <a :title="status.createdAt" :href="getStatusRoute(status).href" @click.prevent="go($event)">
+                <a :title="status.createdAt" :href="statusRoute.href" @click.prevent="go($event)">
                   <time text-sm ws-nowrap hover:underline :datetime="status.createdAt">
                     {{ timeago }}
                   </time>
@@ -153,6 +179,8 @@ const isDM = $computed(() => status.visibility === 'direct')
           </div>
           <StatusActionsMore v-if="actions !== false" :status="status" me--2 />
         </div>
+
+        <!-- Content -->
         <StatusContent :status="status" :context="context" mb2 :class="{ 'mt-2 mb1': isDM }" />
         <StatusActions v-if="actions !== false" v-show="!userSettings.zenMode" :status="status" />
       </div>

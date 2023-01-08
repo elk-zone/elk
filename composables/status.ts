a2a6b84ba7f1bc1870ba6ab11cb083f00bb3af6d
userquin <userquin@gmail.com>
2023-01-08 09:39:11 +0100

    chore: include remember position

--- composables/status.ts
+++ composables/status.ts
@@ -1,3 +1,6 @@
 import type { mastodon } from 'masto'
 
-export const navigateToStatus = ({ status, focusReply = false }: { status: mastodon.v1.Status; focusReply?: boolean }) => navigateTo({ path: getStatusRoute(status).href, state: { focusReply } })
+export const navigateToStatus = ({ status, focusReply = false }: { status: mastodon.v1.Status; focusReply?: boolean }) => {
+  useNuxtApp().$rememberStatusPosition(status)
+  navigateTo({ path: getStatusRoute(status).href, state: { focusReply } })
+}

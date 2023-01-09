33faa509bad828f84e6a4990ed4ec848c41240fe
Daniel Roe <daniel@roe.dev>
2023-01-09 21:33:00 +0000

    perf: use `tiny-decode` for parsing html entities

--- composables/content-render.ts
+++ composables/content-render.ts
@@ -3,8 +3,9 @@ import type { Node } from 'ultrahtml'
 import { Fragment, h, isVNode } from 'vue'
 import type { VNode } from 'vue'
 import { RouterLink } from 'vue-router'
+import { decode } from 'tiny-decode'
 import type { ContentParseOptions } from './content-parse'
-import { decodeHtml, parseMastodonHTML } from './content-parse'
+import { parseMastodonHTML } from './content-parse'
 import ContentCode from '~/components/content/ContentCode.vue'
 import AccountHoverWrapper from '~/components/account/AccountHoverWrapper.vue'
 
@@ -47,7 +48,7 @@ function treeToVNode(
   input: Node,
 ): VNode | string | null {
   if (input.type === TEXT_NODE)
-    return decodeHtml(input.value)
+    return decode(input.value)
 
   if ('children' in input) {
     const node = handleNode(input)

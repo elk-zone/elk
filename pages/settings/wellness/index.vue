e70c2743453fdd73d677462477d9a7624e1f0c76
wheatjs <jacobrclevenger@gmail.com>
2023-01-09 18:30:18 -0500

    feat: wellness settings

---
+++ pages/settings/wellness/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+useHeadFixed({
+  title: () => `${t('settings.preferences.label')} | ${t('nav.settings')}`,
+})
+</script>
+
+<template>
+  <MainContent back-on-small-screen>
+    <template #title>
+      <div text-lg font-bold flex items-center gap-2 @click="$scrollToTop">
+        <span>{{ $t('settings.preferences.label') }}</span>
+      </div>
+    </template>
+    <h3 px6 py4 mt2 font-bold text-xl flex="~ gap-1" items-center>
+      <div i-ri-leaf-line />
+      {{ $t('settings.wellness.label') }}
+    </h3>
+    <SettingsToggleItem
+      :checked="getWellnessSetting('hideBoostCount')"
+      @click="toggleWellnessSetting('hideBoostCount')"
+    >
+      {{ $t('settings.wellness.feature.hide_boost_count') }}
+    </SettingsToggleItem>
+    <SettingsToggleItem
+      :checked="getWellnessSetting('hideFavoriteCount')"
+      @click="toggleWellnessSetting('hideFavoriteCount')"
+    >
+      {{ $t('settings.wellness.feature.hide_favorite_count') }}
+    </SettingsToggleItem>
+    <SettingsToggleItem
+      :checked="getWellnessSetting('hideFollowerCount')"
+      @click="toggleWellnessSetting('hideFollowerCount')"
+    >
+      {{ $t('settings.wellness.feature.hide_follower_count') }}
+    </SettingsToggleItem>
+  </MainContent>
+</template>

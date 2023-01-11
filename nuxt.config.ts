3add7a5f406339289bf411f833576e7d2532048f
Daniel Roe <daniel@roe.dev>
2023-01-11 22:51:23 +0000

    chore: test open graph on edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -120,8 +120,9 @@ export default defineNuxtConfig({
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
         // open graph social image
         { property: 'og:title', content: 'Elk' },
+        { property: 'og:description', content: 'A nimble Mastodon web client' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:image', content: 'https://elk.zone/elk-og.png' },
+        { property: 'og:image', content: 'https://main.elk.zone/elk-og.png' },
         { property: 'og:image:width', content: '3800' },
         { property: 'og:image:height', content: '1900' },
         { property: 'og:site_name', content: 'Elk' },

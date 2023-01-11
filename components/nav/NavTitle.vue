af85a5ea8d74e956a3f81969be3e27fb30f76b25
Piotrek Tomczewski <piotr.tomczewski@noaignite.com>
2023-01-11 14:39:20 +0100

    fix(hashtag): fixed hashtag follow button on larger screens (#862)

--- components/nav/NavTitle.vue
+++ components/nav/NavTitle.vue
@@ -26,7 +26,10 @@ router.afterEach(() => {
         {{ $t('app_name') }} <sup text-sm italic text-secondary mt-1>{{ env === 'release' ? 'alpha' : env }}</sup>
       </div>
     </NuxtLink>
-    <div hidden xl:flex items-center me-8 mt-2 :class="{ 'pointer-events-none op40': !back || back === '/' }">
+    <div
+      hidden xl:flex items-center me-8 mt-2
+      :class="{ 'pointer-events-none op40': !back || back === '/', 'xl:flex': $route.name !== 'tag' }"
+    >
       <NuxtLink
         :aria-label="$t('nav.back')"
         @click="$router.go(-1)"

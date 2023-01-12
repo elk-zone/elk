74886d188397cdfac26220314fd9ba74b8c52db8
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-12 22:14:48 +0100

    feat: hide in zen

--- components/content/ContentMentionGroup.vue
+++ components/content/ContentMentionGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <p flex="~ gap-1" items-center text-sm>
+  <p flex="~ gap-1" items-center text-sm class="zen-hide">
     <span i-ri-arrow-right-line ml--1 text-secondary-light /><slot />
   </p>
 </template>

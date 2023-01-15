6fcaa1192714c8861631443c309fb0e582495119
Michel Edighoffer <edimitchel@gmail.com>
2023-01-16 00:36:34 +0100

    feat: add translation keys

--- pages/settings/language/index.vue
+++ pages/settings/language/index.vue
@@ -15,7 +15,7 @@ const userSettings = useUserSettings()
         <span>{{ $t('settings.language.label') }}</span>
       </div>
     </template>
-    <div p6>
+    <div p6 flex="~ col gap6">
       <label space-y-2>
         <p font-medium>{{ $t('settings.language.display_language') }}</p>
         <SettingsLanguage v-model:language="userSettings.language" select-settings />

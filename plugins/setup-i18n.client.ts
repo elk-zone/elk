6b901317832fc77c1470bc5886f1a9b80be97a84
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-01-15 23:15:03 +0800

    fix: i18n hydration on dev

--- plugins/setup-i18n.client.ts
+++ plugins/setup-i18n.client.ts
@@ -4,21 +4,15 @@ import type { LocaleObject } from 'vue-i18n-routing'
 export default defineNuxtPlugin(async (nuxt) => {
   const i18n = nuxt.vueApp.config.globalProperties.$i18n as VueI18n
   const { setLocale, locales } = i18n
-  const supportLanguages = (locales as LocaleObject[]).map(locale => locale.code)
   const userSettings = useUserSettings()
-  const lang = userSettings.value.language
+  const lang = $computed(() => userSettings.value.language)
 
-  if (process.client && !supportLanguages.includes(lang))
+  const supportLanguages = (locales as LocaleObject[]).map(locale => locale.code)
+  if (!supportLanguages.includes(lang))
     userSettings.value.language = getDefaultLanguage(locales as string[])
 
-  if (process.server) {
-    if (lang !== i18n.locale)
-      await setLocale(lang)
-    return
-  }
-
-  watch(() => userSettings.value.language, (lang) => {
-    if (lang !== i18n.locale)
+  watch([$$(lang), isHydrated], () => {
+    if (isHydrated.value && lang !== i18n.locale)
       setLocale(lang)
   }, { immediate: true })
 })

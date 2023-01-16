bb2e3ab44f5b36802a5555fb6346e3ab74756f49
patak <matias.capeletto@gmail.com>
2023-01-16 06:10:33 +0100

    fix: publish widget mentions (#1198)

--- components/content/ContentMentionGroup.vue
+++ components/content/ContentMentionGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <p flex="~ gap-1" items-center text-sm class="zen-none">
+  <p flex="~ gap-1 wrap" items-center text-sm class="zen-none">
     <span i-ri-arrow-right-line ml--1 text-secondary-light /><slot />
   </p>
 </template>

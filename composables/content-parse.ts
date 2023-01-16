750da83108c346246ad084ec4b063ec8d6eaf7a6
Joachim Viide <jviide@iki.fi>
2023-01-16 17:28:21 +0000

    feat: support all HTML elements allowed by Glitch

--- composables/content-parse.ts
+++ composables/content-parse.ts
@@ -35,12 +35,35 @@ const sanitizer = sanitize({
   code: {
     class: filterClasses(/^language-\w+$/),
   },
-  // other elements supported in glitch
-  h1: {},
-  ol: {},
-  ul: {},
-  li: {},
+  // Other elements supported in glitch, as seen in
+  // https://github.com/glitch-soc/mastodon/blob/13227e1dafd308dfe1a3effc3379b766274809b3/lib/sanitize_ext/sanitize_config.rb#L75
+  abbr: {
+    title: keep,
+  },
+  del: {},
+  blockquote: {
+    cite: filterHref(),
+  },
+  b: {},
+  strong: {},
+  u: {},
+  sub: {},
+  sup: {},
+  i: {},
   em: {},
+  h1: {},
+  h2: {},
+  h3: {},
+  h4: {},
+  h5: {},
+  ul: {},
+  ol: {
+    start: keep,
+    reversed: keep,
+  },
+  li: {
+    value: keep,
+  },
 })
 
 /**
@@ -253,6 +276,10 @@ function filterClasses(allowed: RegExp) {
 
     return c.split(/\s/g).filter(cls => allowed.test(cls)).join(' ')
   }
+}
+
+function keep(value: string | undefined) {
+  return value
 }
 
 function set(value: string) {

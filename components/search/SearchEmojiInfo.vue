e847f8ef1ded6c818bd147e2398f6a3cfc494985
Yuexun Jiang <yuexunjiang@gmail.com>
2023-01-16 18:22:26 +0800

    feat(tiptap): add discord-style emoji suggestion support (#1066)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

---
+++ components/search/SearchEmojiInfo.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Emoji } from 'emoji-mart'
+
+export interface SearchEmoji {
+  title: string
+  src: string
+}
+
+defineProps<{
+  emoji: SearchEmoji
+}>()
+</script>
+
+<template>
+  <div flex gap-2 items-center>
+    <img
+      :key="emoji.title"
+      width="30"
+      height="30"
+      :src="emoji.src"
+      loading="lazy"
+    >
+    <div flex="~ col gap1" pt-1 pl-2 shrink h-full overflow-hidden leading-none>
+      <div flex="~" gap-2>
+        <span text-lg>:{{ emoji.title }}:</span>
+      </div>
+    </div>
+  </div>
+</template>

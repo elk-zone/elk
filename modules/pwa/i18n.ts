3ca58a7c62c23f6b14165f9add148ddcef053f80
userquin <userquin@gmail.com>
2023-01-21 19:17:50 +0100

    chore: handle pwa web manifests

--- modules/pwa/i18n.ts
+++ modules/pwa/i18n.ts
@@ -41,9 +41,10 @@ export const createI18n = async (): Promise<LocalizedWebManifest> => {
     pwaLocales
       .filter(l => l.code !== 'en-US')
       .map(async ({ code, dir = 'ltr', file, files }) => {
-        // read locale file
-        // TODO: check also files entry
-        const { pwa, app_name, app_desc_short } = await readI18nFile(file ?? files![0])
+        // read locale file or files
+        const { pwa, app_name, app_desc_short } = file
+          ? await readI18nFile(file)
+          : await findBestWebManifestData(files, env)
         const entry: WebManifestEntry = pwa?.webmanifest?.[env] ?? {}
         if (!entry.name && app_name)
           entry.name = dir === 'rtl' ? `${envName} ${app_name}` : `${app_name} ${envName}`
@@ -157,4 +158,56 @@ async function readI18nFile(file: string) {
   return JSON.parse(Buffer.from(
     await readFile(resolve(`./locales/${file}`), 'utf-8'),
   ).toString())
+}
+
+interface PWAEntry {
+  webmanifest?: Record<string, {
+    name?: string
+    short_name?: string
+    description?: string
+  }>
+}
+
+interface JsonEntry {
+  pwa?: PWAEntry
+  app_name?: string
+  app_desc_short?: string
+}
+
+async function findBestWebManifestData(locales: string[], env: string) {
+  const entries: JsonEntry[] = await Promise.all(locales.map(async (locale) => {
+    const { pwa, app_name, app_desc_short } = await readI18nFile(`${locale}.json`)
+    return { pwa, app_name, app_desc_short }
+  }))
+
+  let pwa: PWAEntry | undefined
+  let app_name: string | undefined
+  let app_desc_short: string | undefined
+
+  for (const entry of entries) {
+    const webmanifest = entry?.pwa?.webmanifest?.[env]
+    if (webmanifest) {
+      if (pwa) {
+        if (webmanifest.name)
+          pwa.webmanifest![env].name = webmanifest.name
+
+        if (webmanifest.short_name)
+          pwa.webmanifest![env].short_name = webmanifest.short_name
+
+        if (webmanifest.description)
+          pwa.webmanifest![env].description = webmanifest.description
+      }
+      else {
+        pwa = entry.pwa
+      }
+    }
+
+    if (entry.app_name)
+      app_name = entry.app_name
+
+    if (entry.app_desc_short)
+      app_desc_short = entry.app_desc_short
+  }
+
+  return { pwa, app_name, app_desc_short }
 }

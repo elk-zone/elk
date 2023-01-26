d0232184501aa3e3621a8ff763ee7a1e5294bde9
Evan Boehs <evan@boehs.org>
2023-01-26 17:53:49 -0500

    feat: userList dropdown

---
+++ components/list/UserList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { listId } = defineProps<{
+  listId: string
+}>()
+
+const paginator = useMastoClient().v1.lists.listAccounts(listId)
+</script>
+
+<template>
+  <CommonPaginator :paginator="paginator">
+    <template #default="{ item }">
+      <AccountCard
+        :account="item"
+        hover-card
+        border="b base" py2 px4
+      />
+    </template>
+  </CommonPaginator>
+</template>
--- pages/[[server]]/list/[list].vue
+++ pages/[[server]]/list/[list].vue
@@ -23,12 +23,25 @@ onReactivated(() => {
   // The user will see the previous content first, and any changes will be updated to the UI when the request is completed
   refresh()
 })
+
+const mask = useMask()
 </script>
 
 <template>
   <MainContent back>
     <template #title>
       <span text-lg font-bold>{{ listInfo ? listInfo.title : $t('nav.list') }}</span>
+    </template>
+    <template #actions>
+      <VDropdown :distance="0" placement="bottom" @apply-show="mask.show()" @apply-hide="mask.hide()">
+        <!-- Todo: Why doesn't this look good -->
+        <button rounded-full p2 group-hover="bg-orange/10" group-focus-visible="bg-orange/10" group-focus-visible:ring="2 current">
+          <div i-ri:user-line />
+        </button>
+        <template #popper>
+          <ListUserList :list-id="listId" />
+        </template>
+      </VDropdown>
     </template>
     <TimelinePaginator v-bind="{ paginator, stream }" :preprocess="reorderedTimeline" context="home" />
   </MainContent>

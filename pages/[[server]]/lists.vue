26faa48aeeedf3fd20decafd93bc7bc0b59479cc
Evan Boehs <evan@boehs.org>
2023-01-26 14:30:01 -0500

    feat: add lists page

---
+++ pages/[[server]]/lists.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const { t } = useI18n()
+
+const { client } = $(useMasto())
+const paginator = client.v1.lists.list()
+
+useHeadFixed({
+  title: () => t('nav.lists'),
+})
+</script>
+
+<template>
+  <MainContent back>
+    <template #title>
+      <span text-lg font-bold>{{ t('nav.lists') }}</span>
+    </template>
+    <slot>
+      <CommonPaginator :paginator="paginator">
+        <template #default="{ item }">
+          <NuxtLink :to="`list/${item.id}`" block p4 hover:bg-active flex justify-between>
+            {{ item.title }}
+          </NuxtLink>
+        </template>
+      </CommonPaginator>
+    </slot>
+  </MainContent>
+</template>

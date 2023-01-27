c19616b60f29476dd860c3e984e574f8c630e4d0
Daniel Roe <daniel@roe.dev>
2023-01-27 09:26:32 +0100

    feat: add privacy policy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,6 +93,7 @@ export default defineNuxtConfig({
       apiToken: '',
     },
     public: {
+      privacyPolicyUrl: '',
       env: '', // set in build-env module
       buildInfo: {} as BuildInfo, // set in build-env module
       pwaEnabled: !isDevelopment || process.env.VITE_DEV_PWA === 'true',

746a09d205841dbccc5e35072758e7c9757eccc4
victor del <victor.delvalle@thecolvinco.com>
2023-01-27 12:29:54 +0100

    separated file

---
+++ components/publish/PublishEditorTools.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const { editor } = defineProps<{
+  editor: object
+}>()
+</script>
+
+<template>
+  <CommonTooltip placement="top" :content="$t('tooltip.open_editor_tools')">
+    <VDropdown v-if="editor" placement="top">
+      <button
+        btn-action-icon
+      >
+        <div i-ri:font-size-2 />
+      </button>
+      <template #popper>
+        <div flex gap-1>
+          <CommonTooltip placement="top" :content="$t('tooltip.toggle_code_block')">
+            <button
+              btn-action-icon
+              :aria-label="$t('tooltip.toggle_code_block')"
+              :class="editor.isActive('codeBlock') ? 'text-primary' : ''"
+              @click="editor?.chain().focus().toggleCodeBlock().run()"
+            >
+              <div i-ri:code-s-slash-line />
+            </button>
+          </CommonTooltip>
+          <CommonTooltip placement="top" :content="$t('tooltip.toggle_bold')">
+            <button
+              btn-action-icon
+              :aria-label="$t('tooltip.toggle_bold')"
+              :class="editor.isActive('bold') ? 'text-primary' : ''"
+              @click="editor?.chain().focus().toggleBold().run()"
+            >
+              <div i-ri:bold />
+            </button>
+          </CommonTooltip>
+          <CommonTooltip placement="top" :content="$t('tooltip.toggle_italic')">
+            <button
+              btn-action-icon
+              :aria-label="$t('tooltip.toggle_italic')"
+              :class="editor.isActive('italic') ? 'text-primary' : ''"
+              @click="editor?.chain().focus().toggleItalic().run()"
+            >
+              <div i-ri:italic />
+            </button>
+          </CommonTooltip>
+        </div>
+      </template>
+    </VDropdown>
+  </CommonTooltip>
+</template>

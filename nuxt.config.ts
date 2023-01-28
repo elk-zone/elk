a3025e4d06f32999fb7675b632355027a791b17d
Roni Laukkarinen <roni@dude.fi>
2023-01-28 16:21:55 +0200

    Accessibility: Do not prevent arbitrary zooming on mobile devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -130,8 +130,7 @@ export default defineNuxtConfig({
   app: {
     keepalive: true,
     head: {
-      // Prevent arbitrary zooming on mobile devices
-      viewport: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover',
+      viewport: 'width=device-width,initial-scale=1',
       bodyAttrs: {
         class: 'overflow-x-hidden',
       },

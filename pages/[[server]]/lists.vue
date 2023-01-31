0546efafd4deeab9b307d9177b17bf4e3aec4446
Evan Boehs <evan@boehs.org>
2023-01-30 21:25:57 -0500

    feat: add edit button skeleton

--- pages/[[server]]/lists.vue
+++ pages/[[server]]/lists.vue
@@ -3,7 +3,7 @@ const { t } = useI18n()
 
 const client = useMastoClient()
 
-let paginator = client.v1.lists.list()
+const paginator = client.v1.lists.list()
 
 useHeadFixed({
   title: () => t('nav.lists'),
@@ -14,9 +14,9 @@ async function removeList(listId: string) {
     title: t('confirm.delete_list.title'),
     confirm: t('confirm.delete_list.confirm'),
     cancel: t('confirm.delete_list.cancel'),
+  // eslint-disable-next-line curly
   }) === 'confirm') {
     client.v1.lists.remove(listId)
-    paginator = (await paginator).filter(item => item.id !== listId)
   }
 }
 </script>
@@ -30,19 +30,28 @@ async function removeList(listId: string) {
       </NuxtLink>
     </template>
     <slot>
-      <CommonPaginator :paginator="paginator">
+      <CommonPaginator :paginator="paginator" no-end-message>
         <template #default="{ item }">
           <div hover:bg-active flex justify-between items-center>
             <NuxtLink :to="`list/${item.id}`" block grow p4>
               {{ item.title }}
             </NuxtLink>
-            <button
-              rounded-full text-sm p2 border-1 transition-colors
-              border-base hover:text-primary mr4
-              @click="() => removeList(item.id)"
-            >
-              <span i-ri:delete-bin-2-line block text-current />
-            </button>
+            <div mr4 flex gap2>
+              <button
+                rounded-full text-sm p2 border-1 transition-colors
+                border-base hover:text-primary
+                @click="() => removeList(item.id)"
+              >
+                <span i-ri:edit-2-line block text-current />
+              </button>
+              <button
+                rounded-full text-sm p2 border-1 transition-colors
+                border-base hover:text-primary
+                @click="() => removeList(item.id)"
+              >
+                <span i-ri:delete-bin-2-line block text-current />
+              </button>
+            </div>
           </div>
         </template>
       </CommonPaginator>

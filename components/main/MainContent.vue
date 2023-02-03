50c50a7512d072d592a8e72066187c38680d841a
Michel EDIGHOFFER <edimitchel@gmail.com>
2023-02-03 17:32:46 +0100

    fix: drag on every position in background

--- components/main/MainContent.vue
+++ components/main/MainContent.vue
@@ -20,7 +20,7 @@ const wideLayout = computed(() => route.meta.wideLayout ?? false)
       border="b base" bg="[rgba(var(--rgb-bg-base),0.7)]"
       class="native:lg:w-[calc(100vw-5rem)] native:xl:w-[calc(135%+(100vw-1200px)/2)]"
     >
-      <div flex justify-between px5 py2 :class="{ 'xl:hidden': $route.name !== 'tag' }" data-tauri-drag-region class="native:xl:flex">
+      <div flex justify-between px5 py2 :class="{ 'xl:hidden': $route.name !== 'tag' }" class="native:xl:flex">
         <div flex gap-3 items-center :overflow-hidden="!noOverflowHidden ? '' : false" py2 w-full class="native-mac:pl-14 native-mac:sm:pl-0">
           <NuxtLink
             v-if="backOnSmallScreen || back" flex="~ gap1" items-center btn-text p-0 xl:hidden

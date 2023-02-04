e92d1c6adf30634cde7580dec513f1d4e11404da
Tuur Martens <tuurmartens4@gmail.com>
2023-02-04 18:02:05 +0100

    feat: add preference to hide emojis in usernames (#1612)

--- components/account/AccountDisplayName.vue
+++ components/account/AccountDisplayName.vue
@@ -4,12 +4,15 @@ import type { mastodon } from 'masto'
 defineProps<{
   account: mastodon.v1.Account
 }>()
+
+const userSettings = useUserSettings()
 </script>
 
 <template>
   <ContentRich
     :content="getDisplayName(account, { rich: true })"
     :emojis="account.emojis"
+    :show-emojis="!getPreferences(userSettings, 'hideUsernameEmojis')"
     :markdown="false"
   />
 </template>

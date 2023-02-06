8306c58f1affc58c9c37899394f542478b57b6de
userquin <userquin@gmail.com>
2023-02-06 19:13:59 +0100

    chore: cache database

--- utils/elk-idb.ts
+++ utils/elk-idb.ts
@@ -1,33 +1,44 @@
 import { del as delIdb, get as getIdb, promisifyRequest, set as setIdb, update as updateIdb } from 'idb-keyval'
 import type { UseStore } from 'idb-keyval'
 
-const databases: IDBDatabase[] = []
+const databases: IDBOpenDBRequest[] = []
 
 function createStore(): UseStore {
   const storeName = 'keyval'
   const request = indexedDB.open('keyval-store')
-  databases.push(request.result)
+  databases.push(request)
   request.onupgradeneeded = () => request.result.createObjectStore(storeName)
   const dbp = promisifyRequest(request)
   return (txMode, callback) => dbp.then(db => callback(db.transaction(storeName, txMode).objectStore(storeName)))
 }
 
+let defaultGetStoreFunc: UseStore
+function defaultGetStore() {
+  if (!defaultGetStoreFunc)
+    defaultGetStoreFunc = createStore()
+
+  return defaultGetStoreFunc
+}
+
 export function get<T = any>(key: IDBValidKey) {
-  return getIdb<T>(key, createStore())
+  return getIdb<T>(key, defaultGetStore())
 }
 
 export function set(key: IDBValidKey, value: any) {
-  return setIdb(key, value, createStore())
+  return setIdb(key, value, defaultGetStore())
 }
 
 export function update<T = any>(key: IDBValidKey, updater: (oldValue: T | undefined) => T) {
-  return updateIdb(key, updater, createStore())
+  return updateIdb(key, updater, defaultGetStore())
 }
 
 export function del(key: IDBValidKey) {
-  return delIdb(key, createStore())
+  return delIdb(key, defaultGetStore())
 }
 
 export function closeDatabases() {
-  databases.forEach(db => db.close())
+  databases.forEach((db) => {
+    if (db.result)
+      db.result.close()
+  })
 }

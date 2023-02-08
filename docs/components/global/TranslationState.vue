cb2564ffdc7ec8482e0c3f5cb6f3fe3356394523
Michel EDIGHOFFER <edimitchel@gmail.com>
2023-02-08 16:42:16 +0100

    feat(docs): add translation status table

---
+++ docs/components/global/TranslationState.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+import ISO639 from 'iso-639-1'
+import flatteredLocales from 'virtual:elk-locales'
+
+type LocaleCode = string
+
+interface LocaleStatus {
+  // Translated keys
+  translated?: string[]
+  // Translated keys
+  missing?: string[]
+  outdated?: string[]
+  total: number
+}
+
+const flatteredLocalesTyped: Record<LocaleCode, LocaleStatus> = flatteredLocales
+
+const getLocaleName = (locale: string) => {
+  const [localeCode, variant] = locale.split('-')
+  const localeName = ISO639.getName(localeCode)
+
+  return `${localeName} ${variant ? `(${variant})` : ''}`
+}
+</script>
+
+<script setup lang="ts">
+const localesStatuses = flatteredLocalesTyped
+</script>
+
+<template>
+  <table class="w-full">
+    <thead>
+      <tr>
+        <th>Language</th>
+        <th>Translated</th>
+        <th>Missing</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="({ translated, missing, total }, key) in localesStatuses" :key="key">
+        <td>{{ getLocaleName(key) }}</td>
+        <td>{{ `${translated?.length ?? 0} (${(100 * (translated?.length ?? 0) / total).toFixed(1)}%)` }}</td>
+        <td>{{ `${missing?.length ?? 0} (${(100 * (missing?.length ?? 0) / total).toFixed(1)}%)` }}</td>
+        <td><button>Raise a PR</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  width: 100%
+}
+
+th {
+  text-align: left;
+}
+
+button {
+
+}
+</style>
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,28 @@
+import virtual from 'vite-plugin-virtual/dist'
+
 export default defineNuxtConfig({
   app: {
     baseURL: '/docs',
   },
   extends: '@nuxt-themes/docus',
+  vite: {
+    plugins: [
+      virtual({
+        'virtual:elk-locales': {
+          en: {
+            translated: [
+              'a11y.mykey',
+            ],
+            missing: [
+              'a11y.myOtherKey',
+            ],
+            outdated: [
+              'a11y.notExistFromSource',
+            ],
+            total: 2,
+          },
+        },
+      }),
+    ],
+  },
 })

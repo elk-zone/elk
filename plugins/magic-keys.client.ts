8a44ca80d7052285903c536c67ff26046c295ea2
hartmut <10864443+hartmut-co-uk@users.noreply.github.com>
2023-03-05 16:06:31 +0100

    feat(#39): cleanup unused code from initial j/k nav attempt

--- plugins/magic-keys.client.ts
+++ plugins/magic-keys.client.ts
@@ -39,72 +39,21 @@ export default defineNuxtPlugin(({ $scrollToTop }) => {
   whenever(logicAnd(notUsingInput, useMagicSequence(['g', 'h'])), () => navigateTo('/home'))
   whenever(logicAnd(isAuthenticated, notUsingInput, useMagicSequence(['g', 'n'])), () => navigateTo('/notifications'))
 
-  const activeStatus: HTMLElement | null = $ref(null)
-
-  // const initActiveStatus = () => {
-  //   activeStatus = document.querySelector<HTMLElement>('[aria-roledescription=status-details]')
-  //     || document.querySelector<HTMLElement>('[aria-roledescription=status-card]')
-  //   activeStatus?.focus()
-  // }
-  //
-  // const validAriaRoledescriptionsToNavigatePrevNextInTimeline = ['status-details', 'status-card']
-
-  // // work with or without vue-virtual-scroller wrapped StatusCards
-  // // TODO: find a solution that is less coupled, complex and better maintainable...
-  // const virtualScrollerWrap = (el: HTMLElement | null) => el?.closest('.vue-recycle-scroller__item-view') as HTMLElement | null || el
-  // const virtualScrollerUnwrap = (el: HTMLElement | null) => validAriaRoledescriptionsToNavigatePrevNextInTimeline.includes(el?.getAttribute('aria-roledescription') || '') ? el : el?.querySelector<HTMLElement>('[aria-roledescription=status-card]') || el
-  //
-  // const previousElementSiblingFn = (el: HTMLElement | null) => el?.previousElementSibling as HTMLElement | null
-  // const nextElementSiblingFn = (el: HTMLElement | null) => el?.nextElementSibling as HTMLElement | null
-
-  // const timelineMoveFocus = (xElementSiblingFn: (el: HTMLElement | null) => (HTMLElement | null)) => {
-  //   if (!activeStatus || !activeStatus.isConnected) {
-  //     initActiveStatus()
-  //   }
-  //   else {
-  //     let prevEl = virtualScrollerUnwrap(xElementSiblingFn(virtualScrollerWrap(activeStatus)))
-  //     // in detail view, 'jump over' (reply) publish widget
-  //     while (prevEl) {
-  //       if (prevEl && prevEl.hasAttribute('aria-roledescription') && validAriaRoledescriptionsToNavigatePrevNextInTimeline.includes(`${prevEl.getAttribute('aria-roledescription')}`)) {
-  //         activeStatus = prevEl
-  //         activeStatus.focus()
-  //         break
-  //       }
-  //       prevEl = virtualScrollerUnwrap(xElementSiblingFn(virtualScrollerWrap(prevEl)))
-  //     }
-  //   }
-  // }
-
-  // whenever(logicAnd(notUsingInput, keys.j), () => timelineMoveFocus(nextElementSiblingFn))
-  // whenever(logicAnd(notUsingInput, keys.k), () => timelineMoveFocus(previousElementSiblingFn))
-
   const toggleFavouriteActiveStatus = () => {
-    let el: HTMLElement | null
-    if (activeStatus && activeStatus.isConnected) {
-      el = activeStatus
-    }
-    else {
-      // status detail page 'default' (if no other reply status is onfocus/active)
-      el = document.querySelector<HTMLElement>('[aria-roledescription=status-details]')
-    }
     // TODO: find a better solution than clicking buttons...
-    const button = el?.querySelector<HTMLElement>('button[aria-label=Favourite]')
-    button?.click()
+    document
+      .querySelector<HTMLElement>('[aria-roledescription=status-details]')
+      ?.querySelector<HTMLElement>('button[aria-label=Favourite]')
+      ?.click()
   }
   whenever(logicAnd(isAuthenticated, notUsingInput, keys.f), toggleFavouriteActiveStatus)
 
   const toggleBoostActiveStatus = () => {
-    let el: HTMLElement | null
-    if (activeStatus && activeStatus.isConnected) {
-      el = activeStatus
-    }
-    else {
-      // status detail page 'default' (if no other reply status is onfocus/active)
-      el = document.querySelector<HTMLElement>('[aria-roledescription=status-details]')
-    }
     // TODO: find a better solution than clicking buttons...
-    const button = el?.querySelector<HTMLElement>('button[aria-label=Boost]')
-    button?.click()
+    document
+      .querySelector<HTMLElement>('[aria-roledescription=status-details]')
+      ?.querySelector<HTMLElement>('button[aria-label=Boost]')
+      ?.click()
   }
   whenever(logicAnd(isAuthenticated, notUsingInput, keys.b), toggleBoostActiveStatus)
 })

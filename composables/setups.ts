d34a5e6e9638b1d39bb77b9a8f7732908363bcf4
patak <matias.capeletto@gmail.com>
2023-04-16 22:35:25 +0200

    chore: fix titleTemplate param type

--- composables/setups.ts
+++ composables/setups.ts
@@ -22,7 +22,7 @@ export function setupPageHeader() {
       name: 'viewport',
       content: () => `width=device-width,initial-scale=1${enablePinchToZoom.value ? '' : ',maximum-scale=1,user-scalable=0'},viewport-fit=cover`,
     }],
-    titleTemplate: (title) => {
+    titleTemplate: (title?: string) => {
       let titleTemplate = title ?? ''
 
       if (titleTemplate.match(/&[a-z0-9#]+;/gi)) {

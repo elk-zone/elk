f07d32375a8fb1ac07749af410e296a02d939d02
Joaquín Sánchez <userquin@gmail.com>
2023-04-16 21:39:33 +0200

    fix: saving profile data breaks cache (#1937)

--- pages/settings/profile/appearance.vue
+++ pages/settings/profile/appearance.vue
@@ -71,6 +71,12 @@ const { submit, submitting } = submitter(async ({ dirtyFields }) => {
     return
   }
 
+  const server = currentUser.value!.server
+
+  if (!res.account.acct.includes('@'))
+    res.account.acct = `${res.account.acct}@${server}`
+
+  cacheAccount(res.account, server, true)
   currentUser.value!.account = res.account
   reset()
 })

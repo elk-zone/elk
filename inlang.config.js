45c52cf773ecb89370134742b493ab9faffd9751
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-04-24 11:09:31 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -5,19 +5,19 @@
  */
 export async function defineConfig(env) {
   const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js',
   );
 
   const { standardLintRules } = await env.$import(
-    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
-  );
+    'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js',
+  )
 
   const pluginConfig = {
-    pathPattern: "./locales/{language}.json",
-  };
+    pathPattern: './locales/{language}.json',
+  }
 
   return {
-    referenceLanguage: "en",
+    referenceLanguage: 'en',
     languages: await plugin.getLanguages({ ...env, pluginConfig }),
     readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
     writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),

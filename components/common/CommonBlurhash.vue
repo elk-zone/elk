9106285370f83824c7022e90c9737a1f0ebbbe27
Shinigami92 <chrissi92@hotmail.de>
2023-04-25 19:37:15 +0200

    use @unlazy/nuxt

--- components/common/CommonBlurhash.vue
+++ components/common/CommonBlurhash.vue
@@ -1,52 +1,15 @@
 <script setup lang="ts">
-import { decode } from 'blurhash'
-
-const { blurhash, src, srcset, shouldLoadImage = true } = defineProps<{
-  blurhash?: string | null | undefined
+const { blurhash = '', src, srcset } = defineProps<{
+  blurhash?: string
   src: string
   srcset?: string
-  shouldLoadImage?: boolean
 }>()
 
 defineOptions({
   inheritAttrs: false,
 })
-
-const isLoaded = ref(false)
-const placeholderSrc = $computed(() => {
-  if (!blurhash)
-    return ''
-  const pixels = decode(blurhash, 32, 32)
-  return getDataUrlFromArr(pixels, 32, 32)
-})
-
-const target = ref<HTMLImageElement | null>(null)
-const targetIsVisible = ref(false)
-
-function loadImage() {
-  isLoaded.value = true
-}
-
-const { stop } = useIntersectionObserver(target, ([{ isIntersecting }]) => {
-  targetIsVisible.value = isIntersecting
-
-  if (isIntersecting) {
-    if (shouldLoadImage)
-      loadImage()
-
-    // Once the image is loaded, we don't need to observe it anymore
-    stop()
-  }
-}, { threshold: 0.2 })
-
-// If the loading is requested lazy from outside, we need to load the image
-watch(() => shouldLoadImage, () => {
-  if (shouldLoadImage)
-    loadImage()
-})
 </script>
 
 <template>
-  <img v-if="isLoaded || !placeholderSrc" v-bind="$attrs" ref="target" loading="lazy" :src="src" :srcset="srcset">
-  <img v-else v-bind="$attrs" ref="target" loading="lazy" :src="placeholderSrc">
+  <UnLazyImage v-bind="$attrs" :blurhash="blurhash" :src="src" :src-set="srcset" auto-sizes />
 </template>

b7b7220108dc0b67fe42f707b42971cebc145947
kytta <me@kytta.dev>
2023-05-02 15:20:01 +0200

    Actually return link to Mastodon reports

--- composables/masto/routes.ts
+++ composables/masto/routes.ts
@@ -30,7 +30,7 @@ export function getAccountFollowersRoute(account: mastodon.v1.Account) {
 }
 
 export function getReportRoute(id: number) {
-  // TODO: return link to Mastodon (NOT Elk)
+  return `https://${currentUser.value?.server}/admin/reports/${id}`
 }
 
 export function getStatusRoute(status: mastodon.v1.Status) {

92189928cbf25866846ed79627ec84bd60d0f7b5
userquin <userquin@gmail.com>
2023-05-20 15:44:14 +0200

    chore: exclude public and _nuxt assets from auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,7 +4,18 @@ export default defineNuxtRouteMiddleware((to) => {
   if (process.server)
     return
 
-  if (to.path === '/signin/callback')
+  if (
+    to.path === '/signin/callback'
+      || to.path.startsWith('/_nuxt/')
+      || to.path.startsWith('/avatars/')
+      || to.path.startsWith('/emojis/')
+      || to.path.startsWith('/fonts/')
+      || to.path.startsWith('/screenshots/')
+      || to.path.startsWith('/shiki/')
+      || to.path.startsWith('/manifest-')
+      || to.path.startsWith('/sw.js')
+      || to.path.match(/^\/(apple-touch-icon|elk-og|favicon|logo|maskable-icon|pwa-192x192|pwa-512x512|robots)\.(png|ico|svg|txt)$/)
+  )
     return
 
   if (isHydrated.value)

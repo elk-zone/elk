9869332b4c41f85e64dabea66a8c7e218a7ff634
Balint Horvath <horvbalint99@gmail.com>
2023-05-28 23:37:12 +0200

    double tap + desktop support

--- components/modal/ModalMediaPreviewCarousel.vue
+++ components/modal/ModalMediaPreviewCarousel.vue
@@ -3,9 +3,8 @@ import { useGesture } from '@vueuse/gesture'
 import { useReducedMotion } from '@vueuse/motion'
 import type { mastodon } from 'masto'
 
-const { media = [], threshold = 70 } = defineProps<{
+const { media = [] } = defineProps<{
   media?: mastodon.v1.MediaAttachment[]
-  threshold?: number
 }>()
 
 const emit = defineEmits<{
@@ -21,9 +20,7 @@ const slider = ref()
 const slide = ref()
 const image = ref()
 
-const animateTimeout = useTimeout(10)
 const reduceMotion = process.server ? ref(false) : useReducedMotion()
-const canAnimate = computed(() => !reduceMotion.value && animateTimeout.value)
 
 const scale = ref(1)
 const x = ref(0)
@@ -43,51 +40,16 @@ function scrollToFocusedSlide() {
 onMounted(() => scrollToFocusedSlide())
 watch(modelValue, scrollToFocusedSlide)
 
-useSwipe(view, {
-  threshold,
-  passive: false,
-  onSwipeEnd(event, direction) {
-    if (isZoomed.value || isPinching.value)
-      return
-
-    isDragging.value = false
-
-    if (direction === 'right')
-      modelValue.value = Math.max(0, modelValue.value - 1)
-    if (direction === 'left')
-      modelValue.value = Math.min(media.length - 1, modelValue.value + 1)
-    if (direction === 'up')
-      emit('close')
-  },
-})
-
-function restrictToInsideSlide() {
-  const focusedImage = image.value[modelValue.value]
-  const focusedSlide = slide.value[modelValue.value]
-
-  const gap = 20
-  const scaledImageWidth = focusedImage.offsetWidth * scale.value
-  const horizontalOverflow = (scaledImageWidth / 2 - view.value.clientWidth / 2 + gap) / scale.value
-  const scaledImageHeight = focusedImage.offsetHeight * scale.value
-  const verticalOverflow = (scaledImageHeight / 2 - view.value.clientHeight / 2 + gap) / scale.value
-
-  const restrictions = {
-    left: focusedSlide.offsetLeft - horizontalOverflow,
-    right: focusedSlide.offsetLeft + horizontalOverflow,
-    top: focusedSlide.offsetTop - verticalOverflow,
-    bottom: focusedSlide.offsetTop + verticalOverflow,
-  }
-
-  x.value = Math.min(restrictions.right, Math.max(restrictions.left, x.value))
-  y.value = Math.min(restrictions.bottom, Math.max(restrictions.top, y.value))
-}
-
+let lastGestureDistance = 0
 useGesture({
   onPinch({ offset: [distance, _angle] }) {
     isPinching.value = true
 
-    scale.value = Math.max(1, 1 + distance / 200)
-    // restrictToInsideSlide()
+    const currDistance = distance - lastGestureDistance
+    lastGestureDistance = distance
+
+    scale.value = Math.max(1, scale.value + currDistance / 200)
+    restrictDragToInsideSlide()
   },
   onPinchEnd() {
     isPinching.value = false
@@ -95,19 +57,18 @@ useGesture({
     if (!isZoomed.value)
       scrollToFocusedSlide()
   },
-  onDrag({ delta: [deltaX, deltaY], pinching }) {
-    isDragging.value = true
+  onDrag({ delta, pinching, tap, first, swipe, event }) {
+    event.preventDefault()
 
-    if (pinching)
+    if (first || pinching)
       return
 
-    x.value -= deltaX / scale.value
-    y.value -= deltaY / scale.value
-
-    if (!isZoomed.value)
-      y.value = Math.max(0, y.value)
+    if (tap)
+      handleTap()
+    else if (swipe[0] || swipe[1])
+      handleSwipe(swipe)
     else
-      restrictToInsideSlide()
+      handleDrag(delta)
   },
   onDragEnd() {
     isDragging.value = false
@@ -118,9 +79,74 @@ useGesture({
 }, {
   domTarget: view,
   eventOptions: {
-    passive: true,
+    passive: false,
   },
 })
+
+const dragRestrictions = computed(() => {
+  const focusedImage = image.value[modelValue.value]
+  const focusedSlide = slide.value[modelValue.value]
+
+  const gap = 20
+
+  const scaledImageWidth = focusedImage.offsetWidth * scale.value
+  const scaledHorizontalOverflow = scaledImageWidth / 2 - view.value.clientWidth / 2 + gap
+  const horizontalOverflow = Math.max(0, scaledHorizontalOverflow / scale.value)
+
+  const scaledImageHeight = focusedImage.offsetHeight * scale.value
+  const scaledVerticalOverflow = scaledImageHeight / 2 - view.value.clientHeight / 2 + gap
+  const verticalOverflow = Math.max(0, scaledVerticalOverflow / scale.value)
+
+  return {
+    left: focusedSlide.offsetLeft - horizontalOverflow,
+    right: focusedSlide.offsetLeft + horizontalOverflow,
+    top: focusedSlide.offsetTop - verticalOverflow,
+    bottom: focusedSlide.offsetTop + verticalOverflow,
+  }
+})
+
+let lastTapAt = 0
+function handleTap() {
+  const now = Date.now()
+  const isDoubleTap = now - lastTapAt < 250
+  lastTapAt = now
+  if (!isDoubleTap)
+    return
+
+  if (isZoomed.value)
+    scale.value = 1
+  else
+    scale.value = 1.5
+}
+
+function handleSwipe([horiz, vert]: [number, number]) {
+  if (isZoomed.value || isPinching.value)
+    return
+
+  if (horiz === 1) // left
+    modelValue.value = Math.max(0, modelValue.value - 1)
+  if (horiz === -1) // right
+    modelValue.value = Math.min(media.length - 1, modelValue.value + 1)
+  if (vert === -1) // top
+    emit('close')
+}
+
+function handleDrag([deltaX, deltaY]: [number, number]) {
+  isDragging.value = true
+
+  x.value -= deltaX / scale.value
+  y.value -= deltaY / scale.value
+
+  if (!isZoomed.value)
+    y.value = Math.max(0, y.value)
+  else
+    restrictDragToInsideSlide()
+}
+
+function restrictDragToInsideSlide() {
+  x.value = Math.min(dragRestrictions.value.right, Math.max(dragRestrictions.value.left, x.value))
+  y.value = Math.min(dragRestrictions.value.bottom, Math.max(dragRestrictions.value.top, y.value))
+}
 
 const sliderStyle = computed(() => {
   const style = {
@@ -128,8 +154,8 @@ const sliderStyle = computed(() => {
     transition: 'none',
   }
 
-  if (canAnimate.value && !isDragging.value)
-    style.transition = 'all 0.5s ease'
+  if (!reduceMotion.value && !isDragging.value && !isPinching.value)
+    style.transition = 'all 0.3s ease'
 
   return style
 })

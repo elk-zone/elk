01bed82d4cd42390e18a7232d5f64dfb6025e121
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-06-15 12:09:01 +0000

    fix: linting in config js

--- inlang.config.js
+++ inlang.config.js
@@ -1,16 +1,16 @@
 export async function defineConfig(env) {
-	const { default: i18next } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js"
-	);
+  const { default: i18next } = await env.$import(
+    'https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js',
+  )
 
-	const { default: standardLintRules } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js"
-	);
+  const { default: standardLintRules } = await env.$import(
+    'https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js',
+  )
 
-	return {
-		referenceLanguage: 'en',
-		plugins: [i18next({ 
-			pathPattern: './locales/{language}.json'
-		}), standardLintRules()]
-	};
+  return {
+    referenceLanguage: 'en',
+    plugins: [i18next({
+      pathPattern: './locales/{language}.json',
+    }), standardLintRules()],
+  }
 }

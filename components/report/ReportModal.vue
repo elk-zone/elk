37b14789d4d0bbafceb632eef3835a634a6b6e59
Ashwin Agarwal <me@ashwinagarwal.com>
2023-06-20 16:52:27 -0400

    Add localization for Report strings

--- components/report/ReportModal.vue
+++ components/report/ReportModal.vue
@@ -12,6 +12,7 @@ const emit = defineEmits<{
 }>()
 
 const { client } = useMasto()
+const { t } = useI18n()
 
 const step = ref('selectCategory')
 const serverRules = ref((await client.value.v2.instance.fetch()).rules || [])
@@ -93,7 +94,9 @@ function resetModal() {
 <template>
   <div my-8 px-3 sm:px-8 flex="~ col gap-4" relative>
     <h2 mxa text-xl>
-      {{ reportReason === 'dontlike' ? "Limiting" : "Reporting" }} <b text-primary>@{{ account.acct }}</b>
+      <i18n-t :keypath="reportReason === 'dontlike' ? 'report.limiting' : 'report.reporting'">
+        <b text-primary>@{{ account.acct }}</b>
+      </i18n-t>
     </h2>
     <button ref="dismissButton" btn-action-icon absolute top--8 right-0 m1 aria-label="Close" @click="emit('close')">
       <div i-ri:close-line />
@@ -101,33 +104,33 @@ function resetModal() {
 
     <template v-if="step === 'selectCategory'">
       <h1 mxa text-4xl mb4>
-        Tell us what's wrong with this {{ status ? "post" : "account" }}
+        {{ status ? $t('report.whats_wrong_post') : $t('report.whats_wrong_account') }}
       </h1>
       <p text-xl>
-        Choose the best match:
+        {{ $t('report.select_one') }}
       </p>
 
       <div>
         <input id="dontlike" v-model="reportReason" type="radio" value="dontlike">
-        <label pl-2 for="dontlike" font-bold>I don't like it</label>
+        <label pl-2 for="dontlike" font-bold>{{ $t('report.dontlike') }}</label>
         <p pl-6>
-          It is not something you want to see
+          {{ $t('report.dontlike_desc') }}
         </p>
       </div>
 
       <div>
         <input id="spam" v-model="reportReason" type="radio" value="spam">
-        <label pl-2 for="spam" font-bold>It's spam</label>
+        <label pl-2 for="spam" font-bold>{{ $t('report.spam') }}</label>
         <p pl-6>
-          Malicious links, fake engagement, or repetitive replies
+          {{ $t('report.spam_desc') }}
         </p>
       </div>
 
       <div v-if="serverRules.length > 0">
         <input id="violation" v-model="reportReason" type="radio" value="violation">
-        <label pl-2 for="violation" font-bold>It violates one or more of the server rules</label>
+        <label pl-2 for="violation" font-bold>{{ $t('report.violation') }}</label>
         <p v-if="reportReason === 'violation'" pl-6 pt-2 text-primary font-bold>
-          Select all that apply:
+          {{ $t('report.select_many') }}
         </p>
         <ul pl-6>
           <li v-for="rule in serverRules" :key="rule.id" pt-2>
@@ -145,26 +148,26 @@ function resetModal() {
 
       <div>
         <input id="other" v-model="reportReason" type="radio" value="other">
-        <label pl-2 for="other" font-bold>It's something else</label>
+        <label pl-2 for="other" font-bold>{{ $t('report.other') }}</label>
         <p pl-6>
-          The issue does not fit into other categories
+          {{ $t('report.other_desc') }}
         </p>
       </div>
 
       <div v-if="reportReason && reportReason !== 'dontlike'">
         <h3 mt-8 mb-4 font-bold>
-          Is there anything else you think we should know?
+          {{ $t('report.anything_else') }}
         </h3>
-        <textarea v-model="additionalComments" w-full h-20 p-3 border placeholder="Additional Comments" />
+        <textarea v-model="additionalComments" w-full h-20 p-3 border :placeholder="$t('report.additional_comments')" />
         <div v-if="getServerName(account) && getServerName(account) !== currentServer">
           <h3 mt-8 mb-2 font-bold>
-            The user you're reporting is from another server
+            {{ $t('report.another_server') }}
           </h3>
           <p pb-1>
-            Do you want to send an anonymized copy of this report to that server as well?
+            {{ $t('report.forward_question') }}
           </p>
           <input id="forward" v-model="forwardReport" type="checkbox" value="rule.id">
-          <label pl-2 for="forward"><b>Yes, forward this report to {{ getServerName(account) }}</b></label>
+          <label pl-2 for="forward"><b>{{ $t('report.forward', [getServerName(account)]) }}</b></label>
         </div>
       </div>
 
@@ -173,16 +176,16 @@ function resetModal() {
         :disabled="!reportReason || (reportReason === 'violation' && selectedRuleIds.length < 1)"
         @click="categoryChosen()"
       >
-        Next
+        {{ $t('action.next') }}
       </button>
     </template>
 
     <template v-else-if="step === 'selectStatuses'">
       <h1 mxa text-4xl mb4>
-        Are there any {{ status ? "other" : "" }} posts that back up this report?
+        {{ status ? $t('report.select_posts_other') : $t('report.select_posts') }}
       </h1>
       <p text-primary font-bold>
-        Select all that apply:
+        {{ $t('report.select_many') }}
       </p>
       <table>
         <tr v-for="availableStatus in availableStatuses" :key="availableStatus.id">
@@ -205,38 +208,44 @@ function resetModal() {
         btn-solid mxa mt-5
         @click="submitReport()"
       >
-        Submit Report
+        {{ $t('report.submit') }}
       </button>
     </template>
 
     <template v-else-if="step === 'furtherActions'">
       <h1 mxa text-4xl mb4>
-        {{ reportReason === 'dontlike' ? "Don't want to see this?" : "Thanks for reporting, we'll look into this." }}
+        {{ reportReason === 'dontlike' ? $t('report.further_actions.limit.title') : $t('report.further_actions.report.title') }}
       </h1>
       <p text-xl>
-        {{ reportReason === 'dontlike' ? "Here are your options for controlling what you see:" : "While we review this, here are the actions you can take:" }}
+        {{ reportReason === 'dontlike' ? $t('report.further_actions.limit.description') : $t('report.further_actions.report.description') }}
       </p>
 
       <div v-if="useRelationship(account).value?.following">
         <button btn-outline mxa mt-4 mb-2 @click="unfollow()">
-          Unfollow <b>@{{ account.acct }}</b>
+          <i18n-t keypath="menu.unfollow_account">
+            <b>@{{ account.acct }}</b>
+          </i18n-t>
         </button><br>
-        You will no longer see posts from this user in your home feed. You may still see posts from them elsewhere.
+        {{ $t('report.unfollow_desc') }}
       </div>
       <div v-if="!useRelationship(account).value?.muting">
         <button btn-outline mxa mt-4 mb-2 @click="mute()">
-          Mute <b>@{{ account.acct }}</b>
+          <i18n-t keypath="menu.mute_account">
+            <b>@{{ account.acct }}</b>
+          </i18n-t>
         </button><br>
-        You will no longer see any posts from this user. They can still follow you and see your posts. They will not know that they are muted.
+        {{ $t('report.mute_desc') }}
       </div>
       <div v-if="!useRelationship(account).value?.blocking">
         <button btn-outline mxa mt-4 mb-2 @click="block()">
-          Block <b>@{{ account.acct }}</b>
+          <i18n-t keypath="menu.block_account">
+            <b>@{{ account.acct }}</b>
+          </i18n-t>
         </button><br>
-        You will no longer see any posts from this user. They will not be able to see your posts or follow you. They will be able to tell that they are blocked.
+        {{ $t('report.block_desc') }}
       </div>
       <button btn-solid mxa mt-10 @click="emit('close')">
-        Done
+        {{ $t('action.done') }}
       </button>
     </template>
   </div>

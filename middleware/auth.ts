69ea2068d30ab6b6b0fd0faf29716ba5a7a8a53f
userquin <userquin@gmail.com>
2023-07-07 12:47:07 +0200

    fix: installed pwa auth shortcuts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -14,12 +14,27 @@ export default defineNuxtRouteMiddleware((to) => {
 })
 
 function handleAuth(to: RouteLocationNormalized) {
+  if (to.path === '/') {
+    // Installed PWA shortcut to notifications
+    if (to.query['notifications-pwa-shortcut'] !== undefined) {
+      if (currentUser.value)
+        return navigateTo('/notifications')
+      else
+        return navigateTo(`/${currentServer.value}/public/local`)
+    }
+
+    // Installed PWA shortcut to local
+    if (to.query['local-pwa-shortcut'] !== undefined)
+      return navigateTo(`/${currentServer.value}/public/local`)
+  }
+
   if (!currentUser.value) {
     if (to.path === '/home' && to.query['share-target'] !== undefined)
       return navigateTo('/share-target')
     else
       return navigateTo(`/${currentServer.value}/public/local`)
   }
+
   if (to.path === '/')
     return navigateTo('/home')
 }

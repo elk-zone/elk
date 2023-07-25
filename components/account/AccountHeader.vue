552d6a5e93d68931068130103a8163e0c8666fb9
Florian Martinez <fm29830@gmail.com>
2023-07-25 17:12:09 +0200

    upd: change name of the role component

--- components/account/AccountHeader.vue
+++ components/account/AccountHeader.vue
@@ -167,7 +167,7 @@ const personalNoteMaxLength = 2000
         <div flex="~ col gap1" pt2>
           <div flex gap2 items-center flex-wrap>
             <AccountDisplayName :account="account" font-bold sm:text-2xl text-xl />
-            <AccountRoleIndicator :account="account" />
+            <AccountRolesIndicator :account="account" />
             <AccountLockIndicator v-if="account.locked" show-label />
             <AccountBotIndicator v-if="account.bot" show-label />
           </div>

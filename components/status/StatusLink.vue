48e8ddfa8e2a93b42cf59d86ccf2cff98ea64527
Alex Liu <dsa1314@gmail.com>
2023-07-29 20:38:10 +0800

    refactor: remove extra top and botton margin on conversations

--- components/status/StatusLink.vue
+++ components/status/StatusLink.vue
@@ -40,7 +40,7 @@ function go(evt: MouseEvent | KeyboardEvent) {
     focus:outline-none focus-visible:ring="2 primary"
     cursor-pointer
     transition-100
-    class="dark:media-mouse-hover-bg-white/3 media-mouse-hover-bg-black/3"
+    class="dark:media-mouse-hover-bg-white/2 media-mouse-hover-bg-black/2"
     aria-roledescription="status-card"
     :lang="status.language ?? undefined"
     @click="onclick"

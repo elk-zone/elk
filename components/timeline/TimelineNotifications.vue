1542409ba1487dda5d0f4299dd4e90819b059b5f
Ayo <ramon.aycojr@gmail.com>
2023-08-31 23:13:12 +0200

    refactor: notifications filter page to support other types

--- components/timeline/TimelineNotifications.vue
+++ components/timeline/TimelineNotifications.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+export type NotificationType = 'mention' | 'reblog' | 'favourite' | 'follow'
+const { filter } = defineProps<{
+  filter?: NotificationType
+}>()
+
+const options = { limit: 30, types: filter ? [filter] : [] }
+
 // Default limit is 20 notifications, and servers are normally caped to 30
-const paginator = useMastoClient().v1.notifications.list({ limit: 30 })
+const paginator = useMastoClient().v1.notifications.list(options)
 const stream = useStreaming(client => client.v1.stream.streamUser())
 
 const { clearNotifications } = useNotifications()
---
+++ pages/notifications/[filter].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { NotificationType } from 'components/timeline/TimelineNotifications.vue'
+
+const { t } = useI18n()
+useHydratedHead({
+  title: () => `${t('tab.notifications_all')} | ${t('nav.notifications')}`,
+})
+
+const route = useRoute()
+const rawFilter = route.params.filter as string
+const filter = ['mention', 'reblog', 'favourite', 'follow'].includes(rawFilter)
+  ? rawFilter as NotificationType
+  : undefined
+</script>
+
+<template>
+  <TimelineNotifications v-if="isHydrated" :filter="filter" />
+</template>

c2772d34f0ffe733c7986f86f1575e8c8ae4f6ad
Ayo <ramon.aycojr@gmail.com>
2023-09-30 17:02:56 +0200

    feat: initial overlay

--- components/status/StatusEmbeddedMedia.vue
+++ components/status/StatusEmbeddedMedia.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { mastodon } from 'masto'
-import { h } from 'vue'
 
 const { status } = defineProps<{
   status: mastodon.v1.Status
@@ -12,10 +11,48 @@ const vnode = $computed(() => {
   const node = sanitizeEmbeddedIframe(status.card?.html)?.children[0]
   return node ? nodeToVNode(node) : null
 })
+const toggle = ref(true)
+const card = ref(status.card)
 </script>
 
 <template>
-  <component :is="vnode" v-if="vnode" h-70 />
+  <div v-if="toggle">
+    <div
+      w-full h-full
+    >
+      <div
+        rounded-full
+        absolute
+        bg-primary
+        opacity-80
+        cursor-pointer
+        @click="() => toggle = !toggle"
+      >
+        <span
+          text-center
+          justify-center
+          items-center
+          text-white
+          hover:text-primary
+          flex flex-col
+          gap-3 h-30 w-30
+          pointer-events-none
+          i-ri:play-circle-line
+        />
+      </div>
+
+      <CommonBlurhash
+        v-if="card?.image"
+        :blurhash="card.blurhash"
+        :src="card.image"
+        w-full
+        max-h-70 object-cover rounded-lg
+      />
+    </div>
+  </div>
+  <div v-else>
+    <component :is="vnode" v-if="vnode" rounded-lg h-70 />
+  </div>
 </template>
 
 <style>

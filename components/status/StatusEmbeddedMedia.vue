2b12f5608efa222b939056ce0126c370e58c8c1d
Ayo <ramon.aycojr@gmail.com>
2023-09-30 22:19:24 +0200

    refactor: check for card if null

--- components/status/StatusEmbeddedMedia.vue
+++ components/status/StatusEmbeddedMedia.vue
@@ -16,78 +16,80 @@ const card = ref(status.card)
 </script>
 
 <template>
-  <div
-    v-if="overlayToggle"
-    h-80
-    cursor-pointer
-    relative
-  >
+  <div v-if="card">
     <div
-      p-3
-      absolute
-      w-full
-      h-full
-      z-100
-      rounded-lg
-      style="background: linear-gradient(black, rgba(0,0,0,0.5), transparent, transparent, rgba(0,0,0,0.20))"
+      v-if="overlayToggle"
+      h-80
+      cursor-pointer
+      relative
     >
-      <NuxtLink flex flex-col gap-1 hover:underline text-xs text-light font-light target="_blank" :href="card?.url">
-        <div flex gap-0.5>
-          <p flex-row line-clamp-1>
-            {{ card?.providerName }} • {{ card?.authorName }}
-          </p>
-          <span
-            flex-row
-            w-4 h-4
-            pointer-events-none
-            i-ri:arrow-right-up-line
-          />
-        </div>
-        <p font-bold line-clamp-1 text-size-base>
-          {{ card?.title }}
-        </p>
-        <p line-clamp-1>
-          {{ $t('status.embedded_media.warning') }}
-        </p>
-      </NuxtLink>
       <div
-        flex
-        h-50
-        mt-1
-        justify-center
-        flex-items-center
+        p-3
+        absolute
+        w-full
+        h-full
+        z-100
+        rounded-lg
+        style="background: linear-gradient(black, rgba(0,0,0,0.5), transparent, transparent, rgba(0,0,0,0.20))"
       >
-        <button
-          absolute
-          bg-primary
-          rounded-full
-          hover:bg-primary-active
-          @click.stop.prevent="() => overlayToggle = !overlayToggle"
+        <NuxtLink flex flex-col gap-1 hover:underline text-xs text-light font-light target="_blank" :href="card?.url">
+          <div flex gap-0.5>
+            <p flex-row line-clamp-1>
+              {{ card?.providerName }}<span v-if="card?.authorName"> • {{ card?.authorName }}</span>
+            </p>
+            <span
+              flex-row
+              w-4 h-4
+              pointer-events-none
+              i-ri:arrow-right-up-line
+            />
+          </div>
+          <p font-bold line-clamp-1 text-size-base>
+            {{ card?.title }}
+          </p>
+          <p line-clamp-1>
+            {{ $t('status.embedded_warning') }}
+          </p>
+        </NuxtLink>
+        <div
+          flex
+          h-50
+          mt-1
+          justify-center
+          flex-items-center
         >
-          <span
-            text-light
-            flex flex-col
-            gap-3
-            w-27 h-27
-            pointer-events-none
-            i-ri:play-circle-line
-          />
-        </button>
+          <button
+            absolute
+            bg-primary
+            rounded-full
+            hover:bg-primary-active
+            @click.stop.prevent="() => overlayToggle = !overlayToggle"
+          >
+            <span
+              text-light
+              flex flex-col
+              gap-3
+              w-27 h-27
+              pointer-events-none
+              i-ri:play-circle-line
+            />
+          </button>
+        </div>
       </div>
+      <CommonBlurhash
+        v-if="card?.image"
+        :blurhash="card.blurhash"
+        :src="card.image"
+        w-full
+        h-full
+        object-cover
+        rounded-lg
+      />
     </div>
-    <CommonBlurhash
-      v-if="card?.image"
-      :blurhash="card.blurhash"
-      :src="card.image"
-      w-full
-      h-full
-      object-cover
-      rounded-lg
-    />
-  </div>
-  <div v-else>
-    <!-- this inserts the iframe -->
-    <component :is="vnode" v-if="vnode" rounded-lg h-80 />
+    <div v-else>
+      <!-- this inserts the iframe -->
+      <component :is="vnode" v-if="vnode" rounded-lg h-80 />
+    </div>
   </div>
 </template>
 

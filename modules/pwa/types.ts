4291721296196fecc6eb1ba3acdd37d25eacc58d
userquin <userquin@gmail.com>
2023-11-08 21:40:41 +0100

    chore(types): fix `$pwa` module augmentation types for Nuxt and Vue

--- modules/pwa/types.ts
+++ modules/pwa/types.ts
@@ -1,6 +1,31 @@
 import type { VitePWAOptions } from 'vite-plugin-pwa'
+import { type Ref, type UnwrapNestedRefs } from 'vue'
 
 export interface VitePWANuxtOptions extends Partial<VitePWAOptions> {}
+
+export interface PwaInjection {
+  isInstalled: boolean
+  showInstallPrompt: Ref<boolean>
+  cancelInstall: () => void
+  install: () => Promise<void>
+  swActivated: Ref<boolean>
+  registrationError: Ref<boolean>
+  needRefresh: Ref<boolean>
+  updateServiceWorker: (reloadPage?: boolean | undefined) => Promise<void>
+  close: () => Promise<void>
+}
+
+declare module '#app/nuxt' {
+  interface NuxtApp {
+    $pwa: UnwrapNestedRefs<PwaInjection>
+  }
+}
+
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    $pwa: UnwrapNestedRefs<PwaInjection>
+  }
+}
 
 declare module '@nuxt/schema' {
   interface NuxtConfig {

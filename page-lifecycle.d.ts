2edbf58cd4564993929b057e7178d16499935eae
userquin <userquin@gmail.com>
2024-01-04 00:34:12 +0100

    chore: fix lint

--- page-lifecycle.d.ts
+++ page-lifecycle.d.ts
@@ -2,15 +2,15 @@ declare module 'page-lifecycle/dist/lifecycle.mjs' {
     type PageLifecycleState = 'active' | 'passive' | 'hidden' | 'frozen' | 'terminated'
 
     interface PageLifecycleEvent extends Event {
-        newState: PageLifecycleState
-        oldState: PageLifecycleState
+      newState: PageLifecycleState
+      oldState: PageLifecycleState
     }
     interface PageLifecycle extends EventTarget {
-        get state(): PageLifecycleState
-        get pageWasDiscarded(): boolean
-        addUnsavedChanges: (id: Symbol | any) => void
-        removeUnsavedChanges: (id: Symbol | any) => void
-        addEventListener: (type: string, listener: (evt: PageLifecycleEvent) => void) => void
+      get state(): PageLifecycleState
+      get pageWasDiscarded(): boolean
+      addUnsavedChanges: (id: symbol | any) => void
+      removeUnsavedChanges: (id: symbol | any) => void
+      addEventListener: (type: string, listener: (evt: PageLifecycleEvent) => void) => void
     }
     const lifecycle: PageLifecycle
     export default lifecycle

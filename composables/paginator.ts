96fa8232a23f314ff1fa004d51403a07c5e7d67e
zenghawtin <hawtinzeng@gmail.com>
2024-01-06 13:53:39 +0800

    fix: can't refresh statuses after delete a status in account/index page, #2546

--- composables/paginator.ts
+++ composables/paginator.ts
@@ -12,6 +12,7 @@ export function usePaginator<T, P, U = T>(
   // called `next` method will mutate the internal state of the variable,
   // and we need its initial state after HMR
   // so clone it
+
   const paginator = _paginator.clone()
 
   const state = ref<PaginatorState>(isHydrated.value ? 'idle' : 'loading')
@@ -57,6 +58,11 @@ export function usePaginator<T, P, U = T>(
         removeCachedStatus(id)
 
         const data = items.value as mastodon.v1.Status[]
+
+        const prevItemsIndex = prevItems.value.findIndex((i: any) => i.id === id)
+        if (prevItemsIndex >= 0)
+          prevItems.value.splice(prevItemsIndex, 1)
+
         const index = data.findIndex(s => s.id === id)
         if (index >= 0)
           data.splice(index, 1)

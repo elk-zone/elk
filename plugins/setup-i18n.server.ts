35bd896d69ec6fef796cb27b4dcf51d7c0fa61cd
userquin <userquin@gmail.com>
2024-02-23 22:29:35 +0100

    chore: use setup i18n plugin for client and server

--- plugins/setup-i18n.server.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin((nuxt) => {
-  const t = nuxt.vueApp.config.globalProperties.$t
-  const d = nuxt.vueApp.config.globalProperties.$d
-  const n = nuxt.vueApp.config.globalProperties.$n
-
-  nuxt.vueApp.config.globalProperties.$t = wrapI18n(t)
-  nuxt.vueApp.config.globalProperties.$d = wrapI18n(d)
-  nuxt.vueApp.config.globalProperties.$n = wrapI18n(n)
-})

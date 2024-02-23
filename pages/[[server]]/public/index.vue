2c37b1459176557c97d2faf347b95276810c21cf
userquin <userquin@gmail.com>
2024-02-23 23:18:23 +0100

    chore: use `$t` in public/index.vue page

--- pages/[[server]]/public/index.vue
+++ pages/[[server]]/public/index.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-
-const { t } = useI18n()
-
 useHydratedHead({
   title: () => t('title.federated_timeline'),
 })
@@ -12,7 +9,7 @@ useHydratedHead({
     <template #title>
       <NuxtLink to="/public" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
         <div i-ri:earth-line />
-        <span>{{ t('title.federated_timeline') }}</span>
+        <span>{{ $t('title.federated_timeline') }}</span>
       </NuxtLink>
     </template>
 

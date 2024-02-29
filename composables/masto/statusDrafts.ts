9cdfc1602e6d501d20f3c86dde51100506a16c51
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-01 03:35:20 +0900

    feat: support `scheduledAt` param in `getDefaultDraft()` function

--- composables/masto/statusDrafts.ts
+++ composables/masto/statusDrafts.ts
@@ -37,6 +37,7 @@ export function getDefaultDraftItem(options: Partial<Mutable<mastodon.rest.v1.Cr
     language,
     mentions,
     poll,
+    scheduledAt,
   } = options
 
   return {
@@ -45,6 +46,7 @@ export function getDefaultDraftItem(options: Partial<Mutable<mastodon.rest.v1.Cr
     params: {
       status: status || '',
       poll,
+      scheduledAt,
       inReplyToId,
       visibility: getDefaultVisibility(visibility || 'public'),
       sensitive: sensitive ?? false,

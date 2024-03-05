fae79149cafb7d38b00a3e420e89d9ebcd97c007
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-05 18:58:34 +0900

    TMP: create ScheduledStatusCard draft

--- pages/scheduled-posts.vue
+++ pages/scheduled-posts.vue
@@ -1,33 +1,31 @@
 <script setup lang="ts">
+import ScheduledStatusCard from '~/components/status/ScheduledStatusCard.vue'
+
+definePageMeta({
+  middleware: 'auth',
+})
+
+const { t } = useI18n()
+
+useHydratedHead({
+  title: () => t('account.scheduled_posts'),
+})
+
 const client = useMastoClient()
 const scheduledPosts = await client.v1.scheduledStatuses.list()
-
-function cancelSchedule(id: string) {
-  client.v1.scheduledStatuses.$select(id).remove()
-}
 </script>
 
 <template>
-  <div v-for="scheduledPost in scheduledPosts" :key="scheduledPost.id" p4>
-    <div flex items-center p2>
-      <div i-ri:calendar-schedule-line me-2 />
-      Scheduled: {{ new Date(scheduledPost.scheduledAt).toLocaleString() }}
+  <MainContent>
+    <template #title>
+      <NuxtLink to="/public/pinned" timeline-title-style flex items-center gap-2 @click="$scrollToTop">
+        <div i-ri:calendar-schedule-line />
+        <span>{{ t('account.scheduled_posts') }}</span>
+      </NuxtLink>
+    </template>
+
+    <div v-for="scheduledPost in scheduledPosts" :key="scheduledPost.id" p4>
+      <ScheduledStatusCard :scheduled-status="scheduledPost" />
     </div>
-    <p p2>
-      {{ scheduledPost.params.text }}
-    </p>
-    <p>
-      <button
-        px3 py1 border-3 rounded-3 bg-primary text-white
-        @click="cancelSchedule(scheduledPost.id)"
-      >
-        Cancel schedule
-      </button>
-    </p>
-    <details>
-      <blockquote border-1 p2>
-        <code>{{ scheduledPost }}</code>
-      </blockquote>
-    </details>
-  </div>
+  </MainContent>
 </template>

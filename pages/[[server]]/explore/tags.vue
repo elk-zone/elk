0f583ece28ffce0f07021e15dc20b074edd67d8a
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-07 23:33:25 +0900

    feat: remember last accessed explore tab (#2658)

--- pages/[[server]]/explore/tags.vue
+++ pages/[[server]]/explore/tags.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
-import { STORAGE_KEY_HIDE_EXPLORE_TAGS_TIPS } from '~~/constants'
+import { STORAGE_KEY_HIDE_EXPLORE_TAGS_TIPS, STORAGE_KEY_LAST_ACCESSED_EXPLORE_ROUTE } from '~~/constants'
 
 const { t } = useI18n()
-
+const route = useRoute()
 const { client } = useMasto()
+
 const paginator = client.value.v1.trends.tags.list({
   limit: 20,
 })
@@ -12,6 +13,13 @@ const hideTagsTips = useLocalStorage(STORAGE_KEY_HIDE_EXPLORE_TAGS_TIPS, false)
 
 useHydratedHead({
   title: () => `${t('tab.hashtags')} | ${t('nav.explore')}`,
+})
+
+const lastAccessedExploreRoute = useLocalStorage(STORAGE_KEY_LAST_ACCESSED_EXPLORE_ROUTE, '')
+lastAccessedExploreRoute.value = route.path.replace(/(.*\/explore\/?)/, '')
+
+onActivated(() => {
+  lastAccessedExploreRoute.value = route.path.replace(/(.*\/explore\/?)/, '')
 })
 </script>
 

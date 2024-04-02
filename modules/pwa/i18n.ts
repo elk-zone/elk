83f2750c1357e7e1cca0de4eb581dd0008f1c446
Klein Petr <petr@kleinpetr.com>
2024-04-02 21:06:36 +0200

    feat: enforce monochrome pwa icon to be used with android themed icons

--- modules/pwa/i18n.ts
+++ modules/pwa/i18n.ts
@@ -89,6 +89,12 @@ export async function createI18n(): Promise<LocalizedWebManifest> {
         type: 'image/png',
       },
       {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'monochrome',
+      },
+      {
         src: 'pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png',

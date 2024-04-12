b11fd227cbec358a63665b7a2e287a53c26ba6cd
userquin <userquin@gmail.com>
2024-04-12 12:56:55 +0200

    fix(a11y): add aria-* entries to interface settings

--- pages/settings/interface/index.vue
+++ pages/settings/interface/index.vue
@@ -15,26 +15,26 @@ useHydratedHead({
     </template>
     <div p6 flex="~ col gap6">
       <label space-y-2>
-        <p font-medium>{{ $t('settings.interface.font_size') }}</p>
+        <p id="interface-fs" font-medium>{{ $t('settings.interface.font_size') }}</p>
         <SettingsFontSize select-settings />
       </label>
       <div space-y-2>
-        <p font-medium>
+        <p id="interface-cm" font-medium>
           {{ $t('settings.interface.color_mode') }}
         </p>
         <SettingsColorMode />
       </div>
       <div space-y-2>
-        <p font-medium>
+        <p id="interface-tc" font-medium>
           {{ $t('settings.interface.theme_color') }}
         </p>
         <SettingsThemeColors />
       </div>
       <div space-y-2>
-        <p font-medium>
+        <p id="interface-bn" font-medium>
           {{ $t('settings.interface.bottom_nav') }}
         </p>
-        <p>
+        <p id="interface-bn-desc">
           {{ $t('settings.interface.bottom_nav_instructions') }}
         </p>
         <SettingsBottomNav />

f2e5e6eb151830bab3480115bb0e1ac1fa71b299
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-04-13 00:15:54 +0900

    refactor: use v-bind css function for `gridColumnNumber`

--- components/status/StatusMedia.vue
+++ components/status/StatusMedia.vue
@@ -19,7 +19,7 @@ const gridColumnNumber = computed(() => {
 </script>
 
 <template>
-  <div class="status-media-container" :class="`status-media-container-${gridColumnNumber}`">
+  <div class="status-media-container">
     <template v-for="attachment of status.mediaAttachments" :key="attachment.id">
       <StatusAttachment
         :attachment="attachment"
@@ -35,21 +35,12 @@ const gridColumnNumber = computed(() => {
 
 <style lang="postcss">
 .status-media-container {
+  --grid-cols: v-bind(gridColumnNumber);
+  display: grid;
+  grid-template-columns: repeat(var(--grid-cols, 1), 1fr);
   --at-apply: gap-2;
   position: relative;
   width: 100%;
   overflow: hidden;
-}
-.status-media-container-1 {
-  display: grid;
-  grid-template-columns: 1fr;
-}
-.status-media-container-2 {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-}
-.status-media-container-3 {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
 }
 </style>

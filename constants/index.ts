12e74cf6727f61dd339e983c2f6f300f4a1462c3
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-05-28 12:31:12 +0900

    chore: fix lint error in `constants/index.ts`
      29:55  error  '\d' is already included in '\w'                                                                                                                                    regexp/no-dupe-characters-character-class
      29:75  error  '\d' is already included in '\w'                                                                                                                                    regexp/no-dupe-characters-character-class
      29:75  error  Invalid character class range. A character set cannot be the minimum or maximum of a character class range. Either escape the `-` or fix the character class range  regexp/strict
      29:78  error  Unnecessary escape character: \.                                                                                                                                    regexp/no-useless-escape

--- constants/index.ts
+++ constants/index.ts
@@ -26,7 +26,7 @@ export const STORAGE_KEY_LAST_ACCESSED_NOTIFICATION_ROUTE = 'elk-last-accessed-n
 export const STORAGE_KEY_LAST_ACCESSED_EXPLORE_ROUTE = 'elk-last-accessed-explore-route'
 export const STORAGE_KEY_BOTTOM_NAV_BUTTONS = 'elk-bottom-nav-buttons'
 
-export const HANDLED_MASTO_URLS = /^(https?:\/\/)?([\w\d-]+\.)+\w+\/(@[@\w\d-\.]+)(\/objects)?(\/\d+)?$/
+export const HANDLED_MASTO_URLS = /^(https?:\/\/)?([\w\-]+\.)+\w+\/(@[@\w\-.]+)(\/objects)?(\/\d+)?$/
 
 export const NOTIFICATION_FILTER_TYPES: mastodon.v1.NotificationType[] = ['status', 'reblog', 'follow', 'follow_request', 'favourite', 'poll', 'update', 'admin.sign_up', 'admin.report']
 

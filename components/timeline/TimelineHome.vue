4caa63e84fd3fee4a8f3b4491a05d222fc3be3e5
Shinigami <chrissi92@hotmail.de>
2024-09-07 11:27:01 +0200

    perf: reduce timeline limit on slow connection (#2945)

--- components/timeline/TimelineHome.vue
+++ components/timeline/TimelineHome.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import type { mastodon } from 'masto'
 
-const paginator = useMastoClient().v1.timelines.home.list({ limit: 30 })
+const { isSupported, effectiveType } = useNetwork()
+const isSlow = computed(() => isSupported.value && effectiveType.value && ['slow-2g', '2g', '3g'].includes(effectiveType.value))
+const limit = computed(() => isSlow.value ? 10 : 30)
+
+const paginator = useMastoClient().v1.timelines.home.list({ limit: limit.value })
 const stream = useStreaming(client => client.user.subscribe())
 function reorderAndFilter(items: mastodon.v1.Status[]) {
   return reorderedTimeline(items, 'home')

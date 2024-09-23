4d95c17e86376c0d025d2b99281650aa7048e29d
Joaquín Sánchez <userquin@gmail.com>
2024-09-23 14:45:46 +0200

    fix: add new service worker (#2971)

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// DON'T REMOVE THIS FILE: IT IS THE OLD sw.js
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+    self.registration.unregister()
+        .then(() => self.clients.matchAll())
+        .then((clients) => {
+            clients.forEach((client) => {
+                if (client instanceof WindowClient)
+                    client.navigate(client.url);
+            });
+            return Promise.resolve();
+        })
+        .then(() => {
+            self.caches.keys().then((cacheNames) => {
+                Promise.all(
+                    cacheNames.map((cacheName) => {
+                        return self.caches.delete(cacheName);
+                    })
+                );
+            })
+        });
+});

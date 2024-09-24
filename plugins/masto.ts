5da9cb6747ef8b660b1fc0e24adc72e6ecf31bea
userquin <userquin@gmail.com>
2024-09-24 16:23:13 +0200

    fix: don't call masto on server side

--- plugins/masto.ts
+++ plugins/masto.ts
@@ -1,22 +1,27 @@
-export default defineNuxtPlugin(() => {
-  const { params, query } = useRoute()
+export default defineNuxtPlugin({
+  enforce: 'pre',
+  parallel: import.meta.server,
+  setup() {
+    const { params, query } = useRoute()
 
-  publicServer.value = params.server as string || useRuntimeConfig().public.defaultServer
+    publicServer.value = params.server as string || useRuntimeConfig().public.defaultServer
 
-  const masto = createMasto()
-  const user = (typeof query.server === 'string' && typeof query.token === 'string')
-    ? {
-        server: query.server,
-        token: query.token,
-        vapidKey: typeof query.vapid_key === 'string' ? query.vapid_key : undefined,
-      }
-    : (currentUser.value || { server: publicServer.value })
+    const masto = createMasto()
+    const user = (typeof query.server === 'string' && typeof query.token === 'string')
+      ? {
+          server: query.server,
+          token: query.token,
+          vapidKey: typeof query.vapid_key === 'string' ? query.vapid_key : undefined,
+        }
+      : (currentUser.value || { server: publicServer.value })
 
-  loginTo(masto, user)
+    if (import.meta.client)
+      loginTo(masto, user)
 
-  return {
-    provide: {
-      masto,
-    },
-  }
+    return {
+      provide: {
+        masto,
+      },
+    }
+  },
 })

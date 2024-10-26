41346e318b32b843ad1a5a00cb60878cbda186f8
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-10-26 15:13:21 +0900

    feat: add credits to creators in link previews using `fediverse:creator` (#3012)

---
+++ components/status/StatusPreviewCardMoreFromAuthor.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { mastodon } from 'masto'
+
+defineProps<{
+  account: mastodon.v1.Account
+}>()
+</script>
+
+<template>
+  <div
+    max-h-2xl
+    flex gap-2
+    my-auto
+    bg-gray-300
+  >
+    <span z-0>More from</span>
+    <AccountInlineInfo :account="account" hover:bg-gray-300 />
+  </div>
+</template>

672e057f83fb0ac57dcd21440f891d863e02cb12
Joaquín Sánchez <userquin@gmail.com>
2024-11-30 15:42:17 +0100

    docs: add primary light theme to fix color contrast (#3070)

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,8 +1,17 @@
 import { defineTheme } from 'pinceau'
 import { getColors } from 'theme-colors'
 
+const light = getColors('#995e1b')
+const primary = Object
+  .entries(getColors('#d98018'))
+  .reduce((acc, [key, value]) => {
+    acc[key] = {
+      initial: light[key]!,
+      dark: value,
+    }
+    return acc
+  }, {} as Record<string | number, { initial: string, dark: string }>)
+
 export default defineTheme({
-  color: {
-    primary: getColors('#d98018'),
-  },
+  color: { primary },
 })

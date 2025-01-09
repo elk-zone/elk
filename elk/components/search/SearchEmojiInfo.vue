c6ce115911c13d80d58d1ecc02195676ea63b657
Clovis <clovis@synapse-medicine.com>
2025-01-09 21:32:42 +0100

    fix: broken image icon on emoji selector

--- elk/components/search/SearchEmojiInfo.vue
+++ elk/components/search/SearchEmojiInfo.vue
@@ -13,12 +13,16 @@ defineProps<{
 <template>
   <div flex="~ gap3" items-center text-base>
     <img
+      v-if="emoji.src"
       width="20"
       height="20"
       :alt="emoji.alt"
       :src="emoji.src"
       loading="lazy"
     >
+    <span v-else class="text-[20px] leading-[20px]">
+      {{ emoji.alt }}
+    </span>
     <span shrink overflow-hidden leading-none text-base><span text-secondary>:</span>{{ emoji.title }}<span text-secondary>:</span></span>
   </div>
 </template>

c4a1f0a5a6414fee25d2e0bf42a6470316833882
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-01-24 03:37:12 +0900

    [WIP] feat: bluesky-style follow notification

--- components/notification/NotificationGroupedFollow.vue
+++ components/notification/NotificationGroupedFollow.vue
@@ -5,7 +5,16 @@ const { items } = defineProps<{
   items: GroupedNotifications
 }>()
 
+// DEBUG from
+const n = 7
+const follow = items.items[0]
+// eslint-disable-next-line vue/no-mutating-props
+items.items = Array.from({ length: n }).fill(0).map(_ => follow)
+// DEBUG end
+
+const visibleFollows = computed(() => items.items.slice(0, 5))
 const count = computed(() => items.items.length)
+const countPlus = computed(() => Math.max(items.items.length - 5 - 1, 0))
 const isExpanded = ref(false)
 const lang = computed(() => {
   return (count.value > 1 || count.value === 0) ? undefined : items.items[0].status?.language
@@ -17,15 +26,25 @@ const lang = computed(() => {
     <div flex items-center top-0 left-2 pt-2 px-3>
       <div :class="count > 1 ? 'i-ri-group-line' : 'i-ri-user-3-line'" me-3 color-blue text-xl aria-hidden="true" />
       <template v-if="count > 1">
+        <AccountHoverWrapper
+          :account="visibleFollows[0].account"
+        >
+          <NuxtLink :to="getAccountRoute(visibleFollows[0].account)">
+            <AccountDisplayName :account="items.items[0].account" font-bold hover:underline />
+          </NuxtLink>
+        </AccountHoverWrapper>
+        &nbsp;{{ $t('notification.and') }}&nbsp;
         <CommonLocalizedNumber
-          keypath="notification.followed_you_count"
-          :count="count"
+          keypath="notification.others"
+          :count="count - 1"
+          font-bold
         />
+        &nbsp;{{ $t('notification.followed_you') }}
       </template>
       <template v-else-if="count === 1">
         <NuxtLink :to="getAccountRoute(items.items[0].account)">
           <AccountDisplayName
-            :account="items.items[0].account"
+            :account="visibleFollows[0].account"
             text-primary me-1 font-bold line-clamp-1 ws-pre-wrap break-all
           />
         </NuxtLink>
@@ -35,17 +54,13 @@ const lang = computed(() => {
       </template>
     </div>
     <div pb-2 ps8>
-      <div v-if="isExpanded">
-        <AccountCard
-          v-for="item in items.items"
-          :key="item.id"
-          :account="item.account"
-          p3
-        />
-      </div>
-      <div v-else flex="~ wrap gap-1.75" p4>
+      <div
+        v-if="!isExpanded"
+        flex="~ wrap gap-1.75" p4 items-center cursor-pointer
+        @click="isExpanded = !isExpanded"
+      >
         <AccountHoverWrapper
-          v-for="item in items.items"
+          v-for="item in visibleFollows"
           :key="item.id"
           :account="item.account"
         >
@@ -53,6 +68,22 @@ const lang = computed(() => {
             <AccountAvatar :account="item.account" w-12 h-12 />
           </NuxtLink>
         </AccountHoverWrapper>
+        <div flex="~ 1">
+          <span v-if="countPlus > 0" ps-2 text="base lg">+{{ countPlus }}</span>
+          <div i-ri:arrow-down-s-line mx-1 text-secondary text-xl aria-hidden="true" />
+        </div>
+      </div>
+      <div v-else>
+        <AccountInfo
+          v-for="item in items.items"
+          :key="item.id"
+          :account="item.account"
+          p3
+        />
+        <div flex p-1 cursor-pointer @click="isExpanded = !isExpanded">
+          <div i-ri:arrow-up-s-line mx-1 text-secondary text-xl aria-hidden="true" />
+          <span ps-2 text-base>Hide</span>
+        </div>
       </div>
     </div>
   </article>

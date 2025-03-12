f185bf73c2d27dc7c83f65e2dcad32e32b5db60b
Clovis <clovis@bdx.town>
2025-03-12 21:44:10 +0100

    perf: reduce layer number

--- elk/components/common/AnimateNumber.vue
+++ elk/components/common/AnimateNumber.vue
@@ -1,14 +1,59 @@
 <script setup lang="ts">
-defineProps<{
+const { increased } = defineProps<{
   increased?: boolean
 }>()
+
+function getAnimation(newValue: boolean, oldValue: boolean) {
+  if (newValue && !oldValue) {
+    return 'rotate'
+  }
+  else if (!newValue && oldValue) {
+    return 'rotate-inverse'
+  }
+  return ''
+}
+
+const animation = ref(getAnimation(increased, increased))
+
+watch(() => increased, (newValue, oldValue) => {
+  animation.value = getAnimation(newValue, oldValue)
+})
 </script>
 
 <template>
   <div of-hidden h="1.25rem">
-    <div flex="~ col" transition-transform duration-300 :class="increased ? 'translate-y--1/2' : 'translate-y-0'">
+    <div flex="~ col" :class="animation">
       <slot />
       <slot name="next" />
     </div>
   </div>
 </template>
+
+<style scoped>
+@keyframes rotate {
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(-50%);
+  }
+}
+@keyframes rotate-inverse {
+  100% {
+    transform: translateY(0);
+  }
+
+  0% {
+    transform: translateY(-50%);
+  }
+}
+
+.rotate {
+  animation: 300ms ease both rotate;
+}
+
+.rotate-inverse {
+  animation: 300ms ease both rotate-inverse;
+}
+</style>

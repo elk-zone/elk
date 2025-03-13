4846660434531542898890147d4376531ea67b86
Clovis <clovis@bdx.town>
2025-03-13 20:45:04 +0100

    fix: do not smooth scroll on page change

--- elk/plugins/1.scroll-to-top.ts
+++ elk/plugins/1.scroll-to-top.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin(() => {
   return {
     provide: {
       scrollToTop: () => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+        window.scrollTo({ top: 0, left: 0 })
       },
     },
   }

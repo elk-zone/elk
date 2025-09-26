f1fdaffde1dd3bb107d57e7dbe834f5b85258b4c
Daniel Roe <daniel@roe.dev>
2025-09-26 17:38:35 +0300

    docs: update to use docus v4

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus',
+  extends: ['docus'],
+
+  css: ['~/assets/css/main.css'],
+
+  site: {
+    name: 'Elk',
+    url: 'https://docs.elk.zone',
+  },
 
   vite: {
     optimizeDeps: {

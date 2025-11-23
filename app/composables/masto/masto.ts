cbce6f095be26103bfec1e8f87cbbfa5b417b0ca
Ayo <ayo@ayco.io>
2025-11-23 11:38:11 +0100

    feat: don't initialize streaming client without session

--- app/composables/masto/masto.ts
+++ app/composables/masto/masto.ts
@@ -26,54 +26,59 @@ export function mastoLogin(masto: ElkMasto, user: Pick<UserLogin, 'server' | 'to
   const instance: ElkInstance = reactive(getInstanceCache(server) || { uri: server, accountDomain: server })
   const accessToken = user.token
 
-  const createStreamingClient = (streamingApiUrl: string | undefined) => {
-    return streamingApiUrl ? createStreamingAPIClient({ streamingApiUrl, accessToken, implementation: globalThis.WebSocket }) : undefined
-  }
+  let createStreamingClient: (streamingApiUrl: string | undefined) => mastodon.streaming.Client | undefined
 
-  const streamingApiUrl = instance?.configuration?.urls?.streaming
   masto.client.value = createRestAPIClient({ url, accessToken })
-  masto.streamingClient.value = createStreamingClient(streamingApiUrl)
 
-  // Refetch instance info in the background on login
-  masto.client.value.v2.instance.fetch().catch(error => new Promise<mastodon.v2.Instance>((resolve, reject) => {
-    if (error instanceof MastoHttpError && error.statusCode === 404) {
-      return masto.client.value.v1.instance.fetch().then((newInstance) => {
-        console.warn(`Instance ${server} on version ${newInstance.version} does not support "GET /api/v2/instance" API, try converting to v2 instance... expect some errors`)
-        const v2Instance = {
-          ...newInstance,
-          domain: newInstance.uri,
-          sourceUrl: '',
-          usage: {
-            users: {
-              activeMonth: 0,
-            },
-          },
-          icon: [],
-          apiVersions: {
-            mastodon: newInstance.version,
-          },
-          contact: {
-            email: newInstance.email,
-          },
-          configuration: {
-            ...(newInstance.configuration ?? {}),
-            urls: {
-              streaming: newInstance.urls.streamingApi,
-            },
-          },
-        } as unknown as mastodon.v2.Instance
-        return resolve(v2Instance)
-      }).catch(reject)
+  if (currentUser.value !== undefined) {
+    createStreamingClient = (streamingApiUrl: string | undefined) => {
+      return streamingApiUrl ? createStreamingAPIClient({ streamingApiUrl, accessToken, implementation: globalThis.WebSocket }) : undefined
     }
 
-    return reject(error)
-  })).then((newInstance) => {
-    Object.assign(instance, newInstance)
-    if (newInstance.configuration.urls.streaming !== streamingApiUrl)
-      masto.streamingClient.value = createStreamingClient(newInstance.configuration.urls.streaming)
+    const streamingApiUrl = instance?.configuration?.urls?.streaming
+    masto.streamingClient.value = createStreamingClient(streamingApiUrl)
 
-    instanceStorage.value[server] = newInstance
-  })
+    // Refetch instance info in the background on login
+    masto.client.value.v2.instance.fetch().catch(error => new Promise<mastodon.v2.Instance>((resolve, reject) => {
+      if (error instanceof MastoHttpError && error.statusCode === 404) {
+        return masto.client.value.v1.instance.fetch().then((newInstance) => {
+          console.warn(`Instance ${server} on version ${newInstance.version} does not support "GET /api/v2/instance" API, try converting to v2 instance... expect some errors`)
+          const v2Instance = {
+            ...newInstance,
+            domain: newInstance.uri,
+            sourceUrl: '',
+            usage: {
+              users: {
+                activeMonth: 0,
+              },
+            },
+            icon: [],
+            apiVersions: {
+              mastodon: newInstance.version,
+            },
+            contact: {
+              email: newInstance.email,
+            },
+            configuration: {
+              ...(newInstance.configuration ?? {}),
+              urls: {
+                streaming: newInstance.urls.streamingApi,
+              },
+            },
+          } as unknown as mastodon.v2.Instance
+          return resolve(v2Instance)
+        }).catch(reject)
+      }
+
+      return reject(error)
+    })).then((newInstance) => {
+      Object.assign(instance, newInstance)
+      if (newInstance.configuration.urls.streaming !== streamingApiUrl)
+        masto.streamingClient.value = createStreamingClient(newInstance.configuration.urls.streaming)
+
+      instanceStorage.value[server] = newInstance
+    })
+  }
 
   return instance
 }

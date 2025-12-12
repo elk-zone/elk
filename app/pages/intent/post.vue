723a7311237e482e0983c66c70ae5d7c55585019
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-12-12 14:46:38 +0900

    chore: remove wrong param

--- app/pages/intent/post.vue
+++ app/pages/intent/post.vue
@@ -12,7 +12,6 @@ onMounted(async () => {
     spoilerText: route.query.spoiler_text as string,
     visibility: route.query.visibility as mastodon.v1.StatusVisibility,
     language: route.query.language as string,
-    inReplyToId: route.query.query as string,
     quotedStatusId: route.query.quote as string,
   }), true)
   // TODO: need a better idea 👀

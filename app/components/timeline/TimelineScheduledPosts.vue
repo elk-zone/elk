dfc28a69b7f000c7e4eeb179bc50226c4061cecd
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-12-12 16:21:09 +0900

    feat: add scheduled posts timeline page

---
+++ app/components/timeline/TimelineScheduledPosts.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const paginator = useMastoClient().v1.scheduledStatuses.list()
+</script>
+
+<template>
+  <CommonPaginator end-message="common.no_scheduled_posts" :paginator="paginator" />
+</template>

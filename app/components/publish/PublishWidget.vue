elk-zone/elk

commit f44025430dfdfa2d72d71dc543ecc4400192ddc1

feat: handle fetch error

app/components/publish/PublishWidget.vue
@@ -175,17 +175,24 @@ const postLanguageDisplay = computed(() => languagesNameList.find(i => i.code ==
 const isDM = computed(() => draft.value.params.visibility === 'direct')
 
 const hasQuote = computed(() => !!draft.value.params.quotedStatusId)
-
 const quotedStatus = ref<mastodon.v1.Status | null>(null)
-
+const quoteFetchError = ref<string | null>(null)
 watchEffect(async () => {
-  if (hasQuote.value)
-    quotedStatus.value = await fetchStatus(draft.value.params.quotedStatusId!)
+  if (hasQuote.value) {
+    try {
+      quotedStatus.value = await fetchStatus(draft.value.params.quotedStatusId!)
+    }
+    catch (err) {
+      console.error(err)
+      quoteFetchError.value = (err as Error).message
+    }
+  }
 })
 
 function removeQuote() {
   draft.value.params.quotedStatusId = undefined
   quotedStatus.value = null
+  quoteFetchError.value = null
 }
 
 async function handlePaste(evt: ClipboardEvent) {
@@ -466,6 +473,9 @@ const detectLanguage = useDebounceFn(async () => {
               </button>
             </div>
             <StatusQuote v-if="quotedStatus" :status="quotedStatus" />
+            <div v-if="quoteFetchError" text-danger b="base 1" rounded-lg hover:bg-active my-3 p-3>
+              {{ $t('error.quote_fetch_error') }} ({{ quoteFetchError }})
+            </div>
             <StatusCardSkeleton v-else b="base 1" rounded-lg hover:bg-active my-3 />
           </template>
 

commit ad08cd03f184bab7b28b10ec6a8b52aaff5b7f3d

fix: fix if-logic error introduced by f4402543

app/components/publish/PublishWidget.vue
@@ -473,7 +473,7 @@ const detectLanguage = useDebounceFn(async () => {
               </button>
             </div>
             <StatusQuote v-if="quotedStatus" :status="quotedStatus" />
-            <div v-if="quoteFetchError" text-danger b="base 1" rounded-lg hover:bg-active my-3 p-3>
+            <div v-else-if="quoteFetchError" text-danger b="base 1" rounded-lg hover:bg-active my-3 p-3>
               {{ $t('error.quote_fetch_error') }} ({{ quoteFetchError }})
             </div>
             <StatusCardSkeleton v-else b="base 1" rounded-lg hover:bg-active my-3 />
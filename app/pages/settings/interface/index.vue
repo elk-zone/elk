7b6a8523750799909e58eb7c5b3bae7b44f5f6a9
Florens Verschelde <243601+fvsch@users.noreply.github.com>
2025-12-27 07:03:17 +0100

    fix(ui): back-on-small-screen logic in settings (#3328)

--- app/pages/settings/interface/index.vue
+++ app/pages/settings/interface/index.vue
@@ -7,7 +7,7 @@ useHydratedHead({
 </script>
 
 <template>
-  <MainContent back-on-small-screen>
+  <MainContent back="small-only">
     <template #title>
       <div text-lg font-bold flex items-center gap-2 @click="$scrollToTop">
         <span>{{ $t('settings.interface.label') }}</span>

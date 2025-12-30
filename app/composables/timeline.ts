77be05837ac25bda6605ca4370d7ae4d61124c9c
TAKAHASHI Shuuji <shuuji3@gmail.com>
2025-12-30 14:15:46 +0900

    test: split timeline filters into three functions

--- app/composables/timeline.ts
+++ app/composables/timeline.ts
@@ -1,5 +1,4 @@
 import type { mastodon } from 'masto'
-import { getPreferences } from '~/composables/settings'
 
 const maxDistance = 10
 const maxSteps = 1000
@@ -10,7 +9,7 @@ function areStatusesConsecutive(a: mastodon.v1.Status, b: mastodon.v1.Status) {
   return !!inReplyToId && (inReplyToId === a.reblog?.id || inReplyToId === a.id)
 }
 
-function removeFilteredItems(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext): mastodon.v1.Status[] {
+export function removeFilteredItems(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext): mastodon.v1.Status[] {
   const isStrict = (filter: mastodon.v1.FilterResult) => filter.filter.filterAction === 'hide' && filter.filter.context.includes(context)
   const isFiltered = (item: mastodon.v1.Status) => (item.account.id === currentUser.value?.account.id) || !item.filtered?.find(isStrict)
   const isReblogFiltered = (item: mastodon.v1.Status) => !item.reblog?.filtered?.find(isStrict)
@@ -18,7 +17,7 @@ function removeFilteredItems(items: mastodon.v1.Status[], context: mastodon.v1.F
   return [...items].filter(isFiltered).filter(isReblogFiltered)
 }
 
-function removeUserPreferenceItems(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext): mastodon.v1.Status[] {
+export function removeUserPreferenceItems(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext): mastodon.v1.Status[] {
   // Only apply to home and public timelines
   if (context !== 'home' && context !== 'public')
     return items
@@ -47,45 +46,47 @@ function removeUserPreferenceItems(items: mastodon.v1.Status[], context: mastodo
   })
 }
 
-export function reorderedTimeline(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext = 'public') {
+export function reorderTimeline(items: mastodon.v1.Status[]) {
   let steps = 0
-
-  let newItems = removeFilteredItems(items, context)
-  newItems = removeUserPreferenceItems(newItems, context)
-
-  for (let i = newItems.length - 1; i > 0; i--) {
+  for (let i = items.length - 1; i > 0; i--) {
     for (let k = 1; k <= maxDistance && i - k >= 0; k++) {
       // Prevent infinite loops
       steps++
       if (steps > maxSteps)
-        return newItems
+        return items
 
       // Check if the [i-k] item is a reply to the [i] item
       // This means that they are in the wrong order
 
-      if (areStatusesConsecutive(newItems[i], newItems[i - k])) {
-        const item = newItems.splice(i, 1)[0]
-        newItems.splice(i - k, 0, item) // insert older item before the newer one
+      if (areStatusesConsecutive(items[i], items[i - k])) {
+        const item = items.splice(i, 1)[0]
+        items.splice(i - k, 0, item) // insert older item before the newer one
         k = 0
       }
       else if (k > 1) {
         // Check if the [i] item is a reply to the [i-k] item
         // This means that they are in the correct order but there are posts between them
-        if (areStatusesConsecutive(newItems[i - k], newItems[i])) {
+        if (areStatusesConsecutive(items[i - k], items[i])) {
           // If the next statuses are already ordered, move them all
           let j = i
           for (; j < items.length - 1; j++) {
-            if (!areStatusesConsecutive(newItems[j], newItems[j + 1]))
+            if (!areStatusesConsecutive(items[j], items[j + 1]))
               break
           }
           const orderedCount = j - i + 1
-          const itemsToMove = newItems.splice(i, orderedCount)
+          const itemsToMove = items.splice(i, orderedCount)
           // insert older item after the newer one
-          newItems.splice(i - k + 1, 0, ...itemsToMove)
+          items.splice(i - k + 1, 0, ...itemsToMove)
           k = 0
         }
       }
     }
   }
-  return newItems
+  return items
+}
+
+export function filterAndReorderTimeline(items: mastodon.v1.Status[], context: mastodon.v1.FilterContext = 'public') {
+  const itemsWithoutFiltered = removeFilteredItems(items, context)
+  const itemsWithUserPreference = removeUserPreferenceItems(itemsWithoutFiltered, context)
+  return reorderTimeline(itemsWithUserPreference)
 }

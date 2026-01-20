5394bcae3e3be1e6b0a4054f6e3b695822dd6a2c
TAKAHASHI Shuuji <shuuji3@gmail.com>
2026-01-20 15:21:25 +0900

    fix: trim trailing whitespace in content parsing to remove unnecessary `<p></p>` (#3515)

--- app/composables/content-parse.ts
+++ app/composables/content-parse.ts
@@ -3,7 +3,14 @@ import type { mastodon } from 'masto'
 import type { Node } from 'ultrahtml'
 import { findAndReplaceEmojisInText } from '@iconify/utils'
 import { decode } from 'tiny-decode'
-import { DOCUMENT_NODE, ELEMENT_NODE, h, parse, render, TEXT_NODE } from 'ultrahtml'
+import {
+  DOCUMENT_NODE,
+  ELEMENT_NODE,
+  h,
+  parse,
+  render,
+  TEXT_NODE,
+} from 'ultrahtml'
 import { emojiRegEx, getEmojiAttributes } from '~~/config/emojis'
 
 export interface ContentParseOptions {
@@ -19,7 +26,9 @@ export interface ContentParseOptions {
   inReplyToStatus?: mastodon.v1.Status
 }
 
-const sanitizerBasicClasses = filterClasses(/^h-\S*|p-\S*|u-\S*|dt-\S*|e-\S*|mention|hashtag|ellipsis|invisible$/u)
+const sanitizerBasicClasses = filterClasses(
+  /^h-\S*|p-\S*|u-\S*|dt-\S*|e-\S*|mention|hashtag|ellipsis|invisible$/u,
+)
 const sanitizer = sanitize({
   // Allow basic elements as seen in https://github.com/mastodon/mastodon/blob/17f79082b098e05b68d6f0d38fabb3ac121879a9/lib/sanitize_ext/sanitize_config.rb
   br: {},
@@ -86,6 +95,7 @@ export function parseMastodonHTML(
   html: string,
   options: ContentParseOptions = {},
 ) {
+  html = html.trim()
   const {
     markdown = true,
     replaceUnicodeEmoji = true,
@@ -106,25 +116,27 @@ export function parseMastodonHTML(
     // Handle code blocks
     html = html
       /* eslint-disable regexp/no-super-linear-backtracking, regexp/no-misleading-capturing-group */
-      .replace(/>(```|~~~)(\w*)([\s\S]+?)\1/g, (_1, _2, lang: string, raw: string) => {
-        const code = htmlToText(raw)
-          .replace(/</g, '&lt;')
-          .replace(/>/g, '&gt;')
-          .replace(/`/g, '&#96;')
-          .replace(/\*/g, '&ast;')
-        const classes = lang ? ` class="language-${lang}"` : ''
-        return `><pre><code${classes}>${code}</code></pre>`
-      })
+      .replace(
+        />(```|~~~)(\w*)([\s\S]+?)\1/g,
+        (_1, _2, lang: string, raw: string) => {
+          const code = htmlToText(raw)
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/`/g, '&#96;')
+            .replace(/\*/g, '&ast;')
+          const classes = lang ? ` class="language-${lang}"` : ''
+          return `><pre><code${classes}>${code}</code></pre>`
+        },
+      )
       .replace(/`([^`\n]*)`/g, (_1, raw) => {
-        return raw ? `<code>${htmlToText(raw).replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\*/g, '&ast;')}</code>` : ''
+        return raw
+          ? `<code>${htmlToText(raw).replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\*/g, '&ast;')}</code>`
+          : ''
       })
   }
 
   // Always sanitize the raw HTML data *after* it has been modified
-  const transforms: Transform[] = [
-    sanitizer,
-    ...options.astTransforms || [],
-  ]
+  const transforms: Transform[] = [sanitizer, ...(options.astTransforms || [])]
 
   if (hideEmojis) {
     transforms.push(removeUnicodeEmoji)
@@ -159,7 +171,10 @@ export function parseMastodonHTML(
  * @param html The content to parse
  * @param customEmojis The custom emojis to use
  */
-export function convertMastodonHTML(html: string, customEmojis: Record<string, mastodon.v1.CustomEmoji> = {}) {
+export function convertMastodonHTML(
+  html: string,
+  customEmojis: Record<string, mastodon.v1.CustomEmoji> = {},
+) {
   const tree = parseMastodonHTML(html, {
     emojis: customEmojis,
     markdown: true,
@@ -190,7 +205,10 @@ export function sanitizeEmbeddedIframe(html: string): Node {
 export function htmlToText(html: string) {
   try {
     const tree = parse(html)
-    return (tree.children as Node[]).map(n => treeToText(n)).join('').trim()
+    return (tree.children as Node[])
+      .map(n => treeToText(n))
+      .join('')
+      .trim()
   }
   catch (err) {
     console.error(err)
@@ -201,8 +219,7 @@ export function htmlToText(html: string) {
 export function recursiveTreeToText(input: Node): string {
   if (input && input.children && input.children.length > 0)
     return input.children.map((n: Node) => recursiveTreeToText(n)).join('')
-  else
-    return treeToText(input)
+  else return treeToText(input)
 }
 
 const emojiIdNeedsWrappingRE = /^([\w\-])+$/
@@ -257,7 +274,11 @@ export function treeToText(input: Node): string {
 
   if (input.name === 'img' || input.name === 'picture') {
     if (input.attributes.class?.includes('custom-emoji')) {
-      const id = input.attributes['data-emoji-id'] ?? input.attributes.alt ?? input.attributes.title ?? 'unknown'
+      const id
+        = input.attributes['data-emoji-id']
+          ?? input.attributes.alt
+          ?? input.attributes.title
+          ?? 'unknown'
       return id.match(emojiIdNeedsWrappingRE) ? `:${id}:` : id
     }
     if (input.attributes.class?.includes('iconify-emoji'))
@@ -273,7 +294,10 @@ export function treeToText(input: Node): string {
 // Strings get converted to text nodes.
 // The input node's children have been transformed before the node itself
 // gets transformed.
-type Transform = (node: Node, root: Node) => (Node | string)[] | Node | string | null
+type Transform = (
+  node: Node,
+  root: Node,
+) => (Node | string)[] | Node | string | null
 
 // Helpers for transforming (filtering, modifying, ...) a parsed HTML tree
 // by running the given chain of transform functions one-by-one.
@@ -285,7 +309,6 @@ function transformSync(doc: Node, transforms: Transform[]) {
         const result = visit(node.children[i], transform, root)
         if (Array.isArray(result))
           children.push(...result)
-
         else if (result)
           children.push(result)
       }
@@ -300,14 +323,16 @@ function transformSync(doc: Node, transforms: Transform[]) {
     return transform(node, root)
   }
 
-  for (const transform of transforms)
-    doc = visit(doc, transform, doc) as Node
+  for (const transform of transforms) doc = visit(doc, transform, doc) as Node
 
   return doc
 }
 
 // A tree transform for sanitizing elements & their attributes.
-type AttrSanitizers = Record<string, (value: string | undefined) => string | undefined>
+type AttrSanitizers = Record<
+  string,
+  (value: string | undefined) => string | undefined
+>
 function sanitize(allowedElements: Record<string, AttrSanitizers>): Transform {
   return (node) => {
     if (node.type !== ELEMENT_NODE)
@@ -333,7 +358,10 @@ function filterClasses(allowed: RegExp) {
     if (!c)
       return undefined
 
-    return c.split(/\s/g).filter(cls => allowed.test(cls)).join(' ')
+    return c
+      .split(/\s/g)
+      .filter(cls => allowed.test(cls))
+      .join(' ')
   }
 }
 
@@ -415,7 +443,9 @@ function transformUnicodeEmoji(node: Node) {
   findAndReplaceEmojisInText(emojiRegEx, node.value, (match, result) => {
     const attrs = getEmojiAttributes(match)
     matches.push(result.slice(start))
-    matches.push(h('img', { src: attrs.src, alt: attrs.alt, class: attrs.class }))
+    matches.push(
+      h('img', { src: attrs.src, alt: attrs.alt, class: attrs.class }),
+    )
     start = result.length + match.match.length
     return undefined
   })
@@ -426,7 +456,9 @@ function transformUnicodeEmoji(node: Node) {
   return matches.filter(Boolean)
 }
 
-function removeCustomEmoji(customEmojis: Record<string, mastodon.v1.CustomEmoji>): Transform {
+function removeCustomEmoji(
+  customEmojis: Record<string, mastodon.v1.CustomEmoji>,
+): Transform {
   return (node) => {
     if (node.type !== TEXT_NODE)
       return node
@@ -435,20 +467,24 @@ function removeCustomEmoji(customEmojis: Record<string, mastodon.v1.CustomEmoji>
     if (split.length === 1)
       return node
 
-    return split.map((name, i) => {
-      if (i % 2 === 0)
-        return name
+    return split
+      .map((name, i) => {
+        if (i % 2 === 0)
+          return name
 
-      const emoji = customEmojis[name] as mastodon.v1.CustomEmoji
-      if (!emoji)
-        return `:${name}:`
+        const emoji = customEmojis[name] as mastodon.v1.CustomEmoji
+        if (!emoji)
+          return `:${name}:`
 
-      return ''
-    }).filter(Boolean)
+        return ''
+      })
+      .filter(Boolean)
   }
 }
 
-function replaceCustomEmoji(customEmojis: Record<string, mastodon.v1.CustomEmoji>): Transform {
+function replaceCustomEmoji(
+  customEmojis: Record<string, mastodon.v1.CustomEmoji>,
+): Transform {
   return (node) => {
     if (node.type !== TEXT_NODE)
       return node
@@ -457,39 +493,35 @@ function replaceCustomEmoji(customEmojis: Record<string, mastodon.v1.CustomEmoji
     if (split.length === 1)
       return node
 
-    return split.map((name, i) => {
-      if (i % 2 === 0)
-        return name
+    return split
+      .map((name, i) => {
+        if (i % 2 === 0)
+          return name
 
-      const emoji = customEmojis[name] as mastodon.v1.CustomEmoji
-      if (!emoji)
-        return `:${name}:`
+        const emoji = customEmojis[name] as mastodon.v1.CustomEmoji
+        if (!emoji)
+          return `:${name}:`
 
-      return h(
-        'picture',
-        {
-          'alt': `:${name}:`,
-          'class': 'custom-emoji',
-          'data-emoji-id': name,
-        },
-        [
-          h(
-            'source',
-            {
+        return h(
+          'picture',
+          {
+            'alt': `:${name}:`,
+            'class': 'custom-emoji',
+            'data-emoji-id': name,
+          },
+          [
+            h('source', {
               srcset: emoji.staticUrl,
               media: '(prefers-reduced-motion: reduce)',
-            },
-          ),
-          h(
-            'img',
-            {
+            }),
+            h('img', {
               src: emoji.url,
               alt: `:${name}:`,
-            },
-          ),
-        ],
-      )
-    }).filter(Boolean)
+            }),
+          ],
+        )
+      })
+      .filter(Boolean)
   }
 }
 
@@ -500,7 +532,20 @@ const _markdownReplacements: [RegExp, (c: (string | Node)[]) => Node][] = [
   [/~~(.*?)~~/g, c => h('del', null, c)],
   [/`([^`]+)`/g, c => h('code', null, c)],
   // transform @username@twitter.com as links
-  [/\B@(\w+)@twitter\.com\b/gi, c => h('a', { href: `https://twitter.com/${c}`, target: '_blank', rel: 'nofollow noopener noreferrer', class: 'mention external' }, `@${c}@twitter.com`)],
+  [
+    /\B@(\w+)@twitter\.com\b/gi,
+    c =>
+      h(
+        'a',
+        {
+          href: `https://twitter.com/${c}`,
+          target: '_blank',
+          rel: 'nofollow noopener noreferrer',
+          class: 'mention external',
+        },
+        `@${c}@twitter.com`,
+      ),
+  ],
 ]
 
 function _markdownProcess(value: string) {
@@ -508,10 +553,12 @@ function _markdownProcess(value: string) {
 
   let start = 0
   while (true) {
-    let found: {
-      match: RegExpMatchArray
-      replacer: (c: (string | Node)[]) => Node
-    } | undefined
+    let found:
+      | {
+        match: RegExpMatchArray
+        replacer: (c: (string | Node)[]) => Node
+      }
+      | undefined
 
     for (const [re, replacer] of _markdownReplacements) {
       re.lastIndex = start
@@ -542,8 +589,14 @@ function transformMarkdown(node: Node) {
 }
 
 function addBdiParagraphs(node: Node) {
-  if (node.name === 'p' && !('dir' in node.attributes) && node.children?.length && node.children.length > 1)
+  if (
+    node.name === 'p'
+    && !('dir' in node.attributes)
+    && node.children?.length
+    && node.children.length > 1
+  ) {
     node.attributes.dir = 'auto'
+  }
 
   return node
 }
@@ -553,15 +606,22 @@ function transformParagraphs(node: Node): Node | Node[] {
   addBdiParagraphs(node)
 
   // For top level paragraphs, inject an empty <p> to preserve status paragraphs in our editor (except for the last one)
-  if (node.parent?.type === DOCUMENT_NODE && node.name === 'p' && node.parent.children.at(-1) !== node)
+  if (
+    node.parent?.type === DOCUMENT_NODE
+    && node.name === 'p'
+    && node.parent.children.at(-1) !== node
+  ) {
     return [node, h('p')]
+  }
 
   return node
 }
 
 function isMention(node: Node) {
   const child = node.children?.length === 1 ? node.children[0] : null
-  return Boolean(child?.name === 'a' && child.attributes.class?.includes('mention'))
+  return Boolean(
+    child?.name === 'a' && child.attributes.class?.includes('mention'),
+  )
 }
 
 function isSpacing(node: Node) {
@@ -570,10 +630,16 @@ function isSpacing(node: Node) {
 
 // Extract the username from a known mention node
 function getMentionHandle(node: Node): string | undefined {
-  return hrefToHandle(node.children?.[0].attributes.href) ?? node.children?.[0]?.children?.[0]?.attributes?.['data-id']
+  return (
+    hrefToHandle(node.children?.[0].attributes.href)
+    ?? node.children?.[0]?.children?.[0]?.attributes?.['data-id']
+  )
 }
 
-function transformCollapseMentions(status?: mastodon.v1.Status, inReplyToStatus?: mastodon.v1.Status): Transform {
+function transformCollapseMentions(
+  status?: mastodon.v1.Status,
+  inReplyToStatus?: mastodon.v1.Status,
+): Transform {
   let processed = false
 
   return (node: Node, root: Node): Node | Node[] => {
@@ -625,7 +691,10 @@ function transformCollapseMentions(status?: mastodon.v1.Status, inReplyToStatus?
         mentionsCount++
         if (inReplyToStatus) {
           const mentionHandle = getMentionHandle(mention)
-          if (inReplyToStatus.account.acct === mentionHandle || inReplyToStatus.mentions.some(m => m.acct === mentionHandle)) {
+          if (
+            inReplyToStatus.account.acct === mentionHandle
+            || inReplyToStatus.mentions.some(m => m.acct === mentionHandle)
+          ) {
             removeNextSpacing = true
             return false
           }
@@ -639,13 +708,20 @@ function transformCollapseMentions(status?: mastodon.v1.Status, inReplyToStatus?
     // We already have the replying to badge in this case or the status is connected to the previous one.
     // This is needed because the status doesn't include the in Reply to handle, only the account id.
     // But this covers the majority of cases.
-    const showMentions = !(contextualMentionsCount === 0 || (mentionsCount === 1 && status?.inReplyToAccountId))
+    const showMentions = !(
+      contextualMentionsCount === 0
+      || (mentionsCount === 1 && status?.inReplyToAccountId)
+    )
     const grouped = contextualMentionsCount > 2
     if (!showMentions || grouped)
       trimContentStart?.()
 
     const contextualChildren = children.slice(mentions.length)
-    const mentionNodes = showMentions ? (grouped ? [h('mention-group', null, ...contextualMentions)] : contextualMentions) : []
+    const mentionNodes = showMentions
+      ? grouped
+        ? [h('mention-group', null, ...contextualMentions)]
+        : contextualMentions
+      : []
     return {
       ...node,
       children: [...mentionNodes, ...contextualChildren],
@@ -661,7 +737,9 @@ function hrefToHandle(href: string): string | undefined {
   }
 }
 
-function transformMentionLink(node: Node): string | Node | (string | Node)[] | null {
+function transformMentionLink(
+  node: Node,
+): string | Node | (string | Node)[] | null {
   if (node.name === 'a' && node.attributes.class?.includes('mention')) {
     const href = node.attributes.href
     if (href) {
@@ -682,7 +760,13 @@ function createTransformNamedMentions(mentions: mastodon.v1.StatusMention[]) {
       const mention = href && mentions.find(m => m.url === href)
       if (mention) {
         node.attributes.href = `/${currentServer.value}/@${mention.acct}`
-        node.children = [h('span', { 'data-type': 'mention', 'data-id': mention.acct }, `@${mention.username}`)]
+        node.children = [
+          h(
+            'span',
+            { 'data-type': 'mention', 'data-id': mention.acct },
+            `@${mention.username}`,
+          ),
+        ]
         return node
       }
     }

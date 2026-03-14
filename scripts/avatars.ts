8309ec5fdeae12fe669633d7337c5ad94aa81cfc
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-03-14 13:01:42 +0900

    chore: remove unused deps, replace fs-extra and lint-staged to native fs and nano-staged

--- scripts/avatars.ts
+++ scripts/avatars.ts
@@ -1,5 +1,4 @@
 import { writeFile } from 'node:fs/promises'
-import fs from 'fs-extra'
 import { ofetch } from 'ofetch'
 import { join, resolve } from 'pathe'
 import { elkTeamMembers } from '../app/composables/about.ts'
@@ -20,7 +19,7 @@ async function download(url: string, fileName: string) {
 }
 
 async function fetchAvatars() {
-  await fs.ensureDir(avatarsDir)
+  await mkdir(avatarsDir, { recursive: true })
 
   await Promise.all(elkTeamMembers.reduce((acc, { github }) => {
     acc.push(...sizes.map(s => download(`https://github.com/${github}.png?size=${s}`, join(avatarsDir, `${github}-${s}x${s}.png`))))

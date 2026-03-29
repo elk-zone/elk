98e06a5502c47494827358b64d64a2831d074275
userquin <userquin@gmail.com>
2026-03-29 05:03:22 +0200

    chore: add known pages and avoid hitting the server for /settings

--- service-worker/precache.ts
+++ service-worker/precache.ts
@@ -1,3 +1,4 @@
+import type { RouteHandlerCallbackOptions } from 'workbox-core/src/types'
 import type { RouteHandlerCallback } from 'workbox-core/types'
 import type { PrecacheEntry, PrecacheRouteOptions } from 'workbox-precaching'
 import { WorkboxError } from 'workbox-core/_private/WorkboxError'
@@ -7,26 +8,94 @@ import { NetworkOnly } from 'workbox-strategies'
 
 let precacheController: PrecacheController | undefined
 
+const pages = [
+  '/blocks',
+  '/bookmarks',
+  '/compose',
+  '/conversations',
+  '/domain_blocks',
+  '/explore',
+  '/explore/links',
+  '/explore/tags',
+  '/favourites',
+  '/hashtags',
+  '/home',
+  '/intent/post',
+  '/lists',
+  '/mutes',
+  '/notifications',
+  '/notifications/mentions',
+  '/pinned',
+  '/public',
+  '/public/local',
+  '/scheduled-post',
+  '/scheduled-posts',
+  '/search',
+  '/shared-target',
+] as const
+
+async function getCacheResponse(
+  controller: PrecacheController,
+  url: string,
+  options: RouteHandlerCallbackOptions,
+): Promise<Response | undefined> {
+  const pathname = new URL(url).pathname
+  // since we have "nitro.prerender.crawlLinks = true" we need to include here
+  // any page not being prerendered
+  if (pathname === '/settings') {
+    return undefined
+  }
+  for (const page of pages) {
+    if (pathname.endsWith(page)) {
+      const idx = url.indexOf(page)
+      if (idx > -1) {
+        const newUrl = new URL(url.slice(idx), url)
+        const cacheKey = controller.getCacheKeyForURL(newUrl.href)
+        if (!cacheKey) {
+          return undefined
+        }
+        options.request = new Request(newUrl.pathname)
+        options.params = { cacheKey, ...options.params }
+        return await controller.strategy.handle(options)
+      }
+      return undefined
+    }
+  }
+
+  return undefined
+}
+
 class CustomPrecacheController extends PrecacheController {
   override createHandlerBoundToURL(url: string): RouteHandlerCallback {
     const cacheKey = this.getCacheKeyForURL(url)
     if (!cacheKey) {
       throw new WorkboxError('non-precached-url', { url })
     }
+
     const networkOnlyHandler = new NetworkOnly()
     return async (options) => {
-      // check if present at precache before using network only
-      if (options.request.mode !== 'navigate' || this.getCacheKeyForURL(options.request.url)) {
+      // if present in the cache: this will resolve /public or /public/local for example
+      if (this.getCacheKeyForURL(options.request.url)) {
         options.request = new Request(url)
         options.params = { cacheKey, ...options.params }
         return await this.strategy.handle(options)
       }
 
+      // check if present at precache before using network only
+      // <host>/<page> => check for /<page> at cache = if present get it
+      const response = await getCacheResponse(
+        this,
+        options.request.url,
+        options,
+      )
+      if (response)
+        return response
+
       try {
         return await networkOnlyHandler.handle(options)
       }
       catch {
-        // fallback with 404
+        // fallback
         options.request = new Request(url)
         options.params = { cacheKey, ...options.params }
         return await this.strategy.handle(options)
@@ -36,9 +105,10 @@ class CustomPrecacheController extends PrecacheController {
 }
 function getOrCreatePrecacheController(): PrecacheController {
   if (!precacheController) {
-    precacheController = new CustomPrecacheController({
-      fallbackToNetwork: false,
-    })
+    precacheController
+      = import.meta.env.DEV
+        ? new PrecacheController()
+        : new CustomPrecacheController()
   }
   return precacheController
 }

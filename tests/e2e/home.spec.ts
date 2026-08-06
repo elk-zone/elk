fd901fc933703526c9699cc5a67af59c01e83150
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-08-06 23:44:18 +0900

    test: relax title assertion
    Signed-off-by: TAKAHASHI Shuuji <id@shuuji3.xyz>

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -4,6 +4,6 @@ test('home page loads', async ({ page, goto }) => {
   const res = await goto('/', { waitUntil: 'hydration' })
 
   expect(res?.ok()).toBe(true)
-  expect(await page.title()).toBe('Local Timeline | Elk')
+  expect(await page.title()).toContain('Elk')
   expect(page.getByRole('heading')).toHaveText('Elk')
 })
